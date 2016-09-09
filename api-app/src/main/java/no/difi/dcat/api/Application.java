33c49c3299b3257e0fa14501a3e2d0ae481bd35f
David Skogan <david.skogan@acando.no>
2016-09-09 08:54:44 +0200

    nye modeler

---
+++ api-app/src/main/java/no/difi/dcat/api/Application.java
@@ -0,0 +1,76 @@
+package no.difi.dcat.api;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import no.difi.dcat.api.web.Key;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.concurrent.TimeUnit;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+
+	@Bean
+	public LoadingCache<Key, String> getDcatCache() {
+		return CacheBuilder.newBuilder()
+				.maximumSize(10)
+				.expireAfterWrite(24, TimeUnit.HOURS)
+				.build(
+						new CacheLoader<Key, String>() {
+
+
+							private final Logger logger = LoggerFactory.getLogger(CacheLoader.class);
+
+
+							public String load(Key key) throws IOException {
+
+								logger.error("CACHE MISS!!!");
+								CloseableHttpClient httpclient = HttpClients.createDefault();
+
+								HttpGet httpGet = new HttpGet(key.getUrl());
+
+								httpGet.setHeader("Accept", key.getContentType());
+
+								CloseableHttpResponse response1 = httpclient.execute(httpGet);
+
+
+								HttpEntity entity = response1.getEntity();
+
+								return EntityUtils.toString(entity, Charset.forName("UTF-8"));
+
+
+							}
+						});
+
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Application.class, args);
+	}
+
+
+}
---
+++ api-app/src/main/java/no/difi/dcat/api/web/DcatRestController.java
@@ -0,0 +1,111 @@
+package no.difi.dcat.api.web;
+
+import com.google.common.cache.LoadingCache;
+import no.difi.dcat.api.settings.FusekiSettings;
+import no.difi.dcat.datastore.DcatDataStore;
+import no.difi.dcat.datastore.Fuseki;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+
+@RestController
+@CrossOrigin(origins = "*")
+public class DcatRestController {
+
+	@Autowired
+	private FusekiSettings fusekiSettings;
+
+	@Autowired
+	private LoadingCache<Key, String> dcatCache;
+
+	private final Logger logger = LoggerFactory.getLogger(DcatRestController.class);
+	private DcatDataStore dcatDataStore;
+
+
+	@PostConstruct
+	public void initialize() {
+		dcatDataStore = new DcatDataStore(new Fuseki(fusekiSettings.getDcatServiceUri()));
+	}
+
+	/**
+	 * Supported urls:
+	 * - /api/dcat
+	 * - /api/dcat?format=jsonld
+	 * - /api/dcat?format=rdf/xml
+	 *
+	 * @param format
+	 * @return
+	 */
+	@RequestMapping(value = "/api/dcat")
+	public ResponseEntity getDcat(@Valid @RequestParam(value = "format", required = false) String format) {
+
+		SupportedFormat supportedFormat = SupportedFormat.parseFormat(format);
+
+		try {
+
+			Key key = new Key(fusekiSettings.getDcatServiceUri() + "/get?graph=urn:x-arq:UnionGraph", supportedFormat.getMimetype().toString());
+
+			final HttpHeaders httpHeaders= new HttpHeaders();
+			httpHeaders.setContentType(supportedFormat.getMimetype());
+
+			return new ResponseEntity<String>(dcatCache.get(key),httpHeaders, HttpStatus.OK);
+
+
+		} catch (ExecutionException e) {
+			logger.error("Error getting DCAT from Fuseki: " + e.getMessage());
+			return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
+
+		}
+	}
+
+
+	@RequestMapping(value = "/api/invalidateCache", method = RequestMethod.POST)
+	public ResponseEntity invalidateCache() {
+		dcatCache.invalidateAll();
+		return new ResponseEntity<String>(HttpStatus.OK);
+
+	}
+
+
+	@RequestMapping(value = "/api/refreshCache", method = RequestMethod.POST)
+	public ResponseEntity refreshCache() {
+
+		List<Key> keyset = new ArrayList<>(dcatCache.asMap().keySet());
+		keyset.forEach(dcatCache::refresh);
+
+		return new ResponseEntity<String>(HttpStatus.OK);
+
+	}
+
+
+// This compression filter is quite slow, it's better to set up compression in tomcat.
+//
+//	@Bean
+//	public CompressingFilter compressingFilter() {
+//		CompressingFilter compressingFilter = new CompressingFilter();
+//		return compressingFilter;
+	/*
+		<dependency>
+			<groupId>net.sourceforge.pjl-comp-filter</groupId>
+			<artifactId>pjl-comp-filter</artifactId>
+			<version>1.7</version>
+		</dependency>
+	 */
+//	}
+
+
+
+
+}
+
