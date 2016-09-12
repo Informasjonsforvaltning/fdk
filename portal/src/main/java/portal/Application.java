bbb49f4ab43b955deb8bc5d93862765569e47603
David Skogan <david.skogan@acando.no>
2016-09-12 09:26:23 +0200

    Fixed problem with portal dummy implementation

--- portal/src/main/java/portal/Application.java
+++ portal/src/main/java/portal/Application.java
@@ -1,21 +1,24 @@
 package portal;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+        import java.util.Arrays;
+
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-@RestController
 public class Application {
 
-    @RequestMapping("/")
-    public String home() {
-        return "Datakatalog";
-    }
-
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
     }
 
 }
