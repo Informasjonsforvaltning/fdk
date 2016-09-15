17d7c86589c1c9b1c6b2e28d7234619f9578bd5c
David Skogan <david.skogan@acando.no>
2016-09-15 13:15:53 +0200

    Dagens struggle

--- admin-webapp/src/main/java/no/difi/dcat/admin/Application.java
+++ admin-webapp/src/main/java/no/difi/dcat/admin/Application.java
@@ -2,6 +2,7 @@ package no.difi.dcat.admin;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.ErrorAttributes;
 import org.springframework.boot.builder.SpringApplicationBuilder;
