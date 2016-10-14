2556536bcb6118fe2fc3421beb212d70d3e23fa5
David Skogan <david.skogan@acando.no>
2016-10-14 15:56:25 +0200

    FDK-83 La til versjonsinformasjon i portal.webapp

--- portal/webapp/src/main/java/no/dcat/portal/webapp/MvcConfig.java
+++ portal/webapp/src/main/java/no/dcat/portal/webapp/MvcConfig.java
@@ -3,7 +3,9 @@ package no.dcat.portal.webapp;
 /**
  * Created by nodavsko on 28.09.2016.
  */
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -17,5 +19,11 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
         registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/login").setViewName("login");
     }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
 
 }
