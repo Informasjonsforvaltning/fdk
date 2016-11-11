b344ddee17a8f3bc3ea41d6359d80deef48b23b0
Bjørn Grøva <bjorn.grova@brreg.no>
2016-11-11 08:15:36 +0100

    Formatted code to make it slightly more readable

--- datastore/src/main/java/no/difi/dcat/datastore/domain/dcat/Dataset.java
+++ datastore/src/main/java/no/difi/dcat/datastore/domain/dcat/Dataset.java
@@ -96,118 +96,106 @@ public class Dataset {
 
 	public List<String> getTheme() { return theme; }
 	public void setTheme(List<String> theme) { this.theme = theme; }
+
 	public String getId() {
 		return id;
 	}
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public List<Distribution> getDistributions() {
 		return distributions;
 	}
 	public void setDistributions(List<Distribution> distributions) {
 		this.distributions = distributions;
 	}
+
 	public Catalog getCatalog() {
 		return catalog;
 	}
 	public void setCatalog(Catalog catalog) {
 		this.catalog = catalog;
 	}
+
 	public Map<String, String> getTitle() {
 		return title;
 	}
 	public void setTitle(Map<String, String> title) {
 		this.title = title;
 	}
+
 	public Map<String, String> getDescription() {
 		return description;
 	}
 	public void setDescription(Map<String, String> description) {
 		this.description = description;
 	}
+
 	public Contact getContactPoint() {
 		return contactPoint;
 	}
 	public void setContactPoint(Contact contactPoint) {
 		this.contactPoint = contactPoint;
 	}
+
 	public Map<String, List<String>> getKeyword() {
 		return keyword;
 	}
 	public void setKeyword(Map<String, List<String>> keyword) {
 		this.keyword = keyword;
 	}
+
 	public Publisher getPublisher() {
 		return publisher;
 	}
 	public void setPublisher(Publisher publisher) {
 		this.publisher = publisher;
 	}
+
 	public Date getIssued() {
 		return issued;
 	}
 	public void setIssued(Date issued) {
 		this.issued = issued;
 	}
+
 	public Date getModified() {
 		return modified;
 	}
 	public void setModified(Date modified) {
 		this.modified = modified;
 	}
+
 	public String getLanguage() {
 		return language;
 	}
 	public void setLanguage(String language) {
 		this.language = language;
 	}
+
 	public String getLandingPage() {
 		return landingPage;
 	}
 	public void setLandingPage(String landingPage) {
 		this.landingPage = landingPage;
 	}
+
 	public List<String> getConformsTo() { return conformsTo; }
 	public void setConformsTo(List<String> conformsTo)  this.conformsTo = conformsTo; }
 
-	public List<String> getTemporal() {
-		return temporal;
-	}
+	public List<String> getTemporal() { return temporal; }
+	public void setTemporal(List<String> temporal) { this.temporal = temporal; }
 
-	public void setTemporal(List<String> temporal) {
-		this.temporal = temporal;
-	}
+	public List<String> getSpatial() { return spatial; }
+	public void setSpatial(List<String> spatial) { this.spatial = spatial; }
 
-	public List<String> getSpatial() {
-		return spatial;
-	}
+	public String getAccessRights() { return accessRights; }
+	public void setAccessRights(String accessRights) { this.accessRights = accessRights; }
 
-	public void setSpatial(List<String> spatial) {
-		this.spatial = spatial;
-	}
+	public List<String> getAccessRightsComment() { return accessRightsComment; }
+	public void setAccessRightsComment(List<String> accessRightsComment) { this.accessRightsComment = accessRightsComment; }
 
-	public String getAccessRights() {
-		return accessRights;
-	}
-
-	public void setAccessRights(String accessRights) {
-		this.accessRights = accessRights;
-	}
-
-	public List<String> getAccessRightsComment() {
-		return accessRightsComment;
-	}
-
-	public void setAccessRightsComment(List<String> accessRightsComment) {
-		this.accessRightsComment = accessRightsComment;
-	}
-
-	public List<String> getReferences() {
-		return references;
-	}
-
-	public void setReferences(List<String> references) {
-		this.references = references;
-	}
+	public List<String> getReferences() { return references; }
+	public void setReferences(List<String> references) { this.references = references; }
 }
