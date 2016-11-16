c3b77e83163442d77769587991e2714ed3ae1252
Bjørn Grøva <bjorn.grova@brreg.no>
2016-11-16 10:33:19 +0100

    FDK-116 prepared api-app config files for openshift

--- api-app/src/main/java/no/difi/dcat/api/settings/FusekiSettings.java
+++ api-app/src/main/java/no/difi/dcat/api/settings/FusekiSettings.java
@@ -3,6 +3,11 @@ package no.difi.dcat.api.settings;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 @EnableConfigurationProperties
@@ -24,6 +29,16 @@ public class FusekiSettings {
 	public void setAdminServiceUri(String adminServiceUri) {
 		this.adminServiceUri = adminServiceUri;
 	}
-	
+
+	//Det må lages en egen PropertySourcePlaceholderConfigurer siden @PropertySource fortsatt ikke støtter yaml format.
+	@Bean
+	@Profile("default") //Skal kun brukes når spring_active_profiles inneholder default
+	public static PropertySourcesPlaceholderConfigurer properties() {
+		PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+		YamlPropertiesFactoryBean yaml = new YamlPropertiesFactoryBean();
+		yaml.setResources(new ClassPathResource("src/test-local/resources/properties/local-properties.yml")); //Path til propertiesfiler som skal brukes for JUnit og kjøring på lokal maskin
+		propertySourcesPlaceholderConfigurer.setProperties(yaml.getObject());
+		return propertySourcesPlaceholderConfigurer;
+	}
 	
 }
