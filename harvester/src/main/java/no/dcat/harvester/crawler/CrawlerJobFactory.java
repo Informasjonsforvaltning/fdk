80ddf721b388154de83f6bef01646cfd1f2207d7
Bjørn Grøva <bjorn.grova@brreg.no>
2016-12-06 15:52:39 +0100

    FDK-116 everything now works on Openshift!

--- harvester/src/main/java/no/dcat/harvester/crawler/CrawlerJobFactory.java
+++ harvester/src/main/java/no/dcat/harvester/crawler/CrawlerJobFactory.java
@@ -35,9 +35,6 @@ public class CrawlerJobFactory {
 	private FusekiResultHandler fusekiResultHandler;
 	private ElasticSearchResultHandler elasticSearchResultHandler;
 
-	//TODO: gjør om til property
-	//private String elasticSearchClusterName = "fellesdatakatalog";
-
 	private final Logger logger = LoggerFactory.getLogger(CrawlerJobFactory.class);
 
 	@PostConstruct
