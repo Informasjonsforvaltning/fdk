5f62468a17c43a7150feac561ee803834029bb67
Bjørn Grøva <bjorn.grova@brreg.no>
2016-12-06 13:18:57 +0100

    FDK-116 added cluster name setting + additional logging

--- harvester/src/main/java/no/dcat/harvester/crawler/handlers/ElasticSearchResultHandler.java
+++ harvester/src/main/java/no/dcat/harvester/crawler/handlers/ElasticSearchResultHandler.java
@@ -30,18 +30,22 @@ public class ElasticSearchResultHandler implements CrawlerResultHandler {
 
     String hostename;
     int port;
+    String clustername;
 
-    public ElasticSearchResultHandler(String hostname, int port) {
+    public ElasticSearchResultHandler(String hostname, int port, String clustername) {
         this.hostename = hostname;
         this.port = port;
+        this.clustername = clustername;
+
+        logger.debug("ES clustername: " + this.clustername);
     }
 
 
     @Override
     public void process(DcatSource dcatSource, Model model) {
-        logger.trace("Processing results Elasticsearch");
+        logger.debug("Processing results Elasticsearch: " + this.hostename +":" + this.port + " cluster: "+ this.clustername);
 
-        try (Elasticsearch elasticsearch = new Elasticsearch(hostename, port)) {
+        try (Elasticsearch elasticsearch = new Elasticsearch(hostename, port, clustername)) {
             logger.trace("Start indexing");
             indexWithElasticsearch(dcatSource, model, elasticsearch);
         } catch (Exception e) {
