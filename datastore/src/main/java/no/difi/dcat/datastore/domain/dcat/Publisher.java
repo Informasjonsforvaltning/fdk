a89313aa29b4e3a079a89d54b180d0cde80c429b
Marcus Gustafson <extmargus@brreg.no>
2016-12-06 16:03:54 +0100

    FDK-140 Loading of publisher index.

--- datastore/src/main/java/no/difi/dcat/datastore/domain/dcat/Publisher.java
+++ datastore/src/main/java/no/difi/dcat/datastore/domain/dcat/Publisher.java
@@ -2,22 +2,41 @@ package no.difi.dcat.datastore.domain.dcat;
 
 public class Publisher {
 
-	private String id;
-	private String name;
-	
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	
-	
+    private String id;
+    private String name;
+    private String overordnetEnhet;
+    private String organisasjonsform;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public void setOverordnetEnhet(String overordnetEnhet) {
+        this.overordnetEnhet = overordnetEnhet;
+    }
+
+    public String getOverordnetEnhet() {
+        return overordnetEnhet;
+    }
+
+    public void setOrganisasjonsform(String organisasjonsform) {
+        this.organisasjonsform = organisasjonsform;
+    }
+
+    public String getOrganisasjonsform() {
+        return organisasjonsform;
+    }
 }
---
+++ harvester/src/test/java/no/dcat/harvester/crawler/handlers/ElasticSearchResultPubHandlerTest.java
@@ -0,0 +1,47 @@
+package no.dcat.harvester.crawler.handlers;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import no.dcat.harvester.dcat.domain.theme.builders.vocabulary.EnhetsregisteretRDF;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.sparql.vocabulary.FOAF;
+import org.elasticsearch.action.index.IndexRequest;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Class for testing ElasticSearchResultPubHandler.
+ */
+public class ElasticSearchResultPubHandlerTest {
+    private static String result = "index {[dcat][publisher][http://data.brreg.no/enhetsregisteret/enhet/983887457], source[{\n" +
+            "  \"id\": \"http://data.brreg.no/enhetsregisteret/enhet/983887457\",\n" +
+            "  \"name\": \"BR\",\n" +
+            "  \"overordnetEnhet\": \"814716872\",\n" +
+            "  \"organisasjonsform\": \"ORGL\"\n" +
+            "}]}";
+    @Test
+    public void test() {
+        Gson gson = new GsonBuilder().setPrettyPrinting().setDateFormat("yyyy-MM-dd'T'HH:mm:ssX").create();
+        ElasticSearchResultPubHandler handler = new ElasticSearchResultPubHandler(null, 0);
+
+        String pudlisherUri = "http://data.brreg.no/enhetsregisteret/enhet/983887457";
+        String organisasjonsform    = "ORGL";
+        String overordnetEnhet     = "814716872";
+        String name     = "BR";
+
+        Model model = ModelFactory.createDefaultModel();
+
+        Resource publisher = model.createResource(pudlisherUri);
+
+        publisher.addProperty(EnhetsregisteretRDF.organisasjonsform, organisasjonsform);
+        publisher.addProperty(EnhetsregisteretRDF.overordnetEnhet, overordnetEnhet);
+        publisher.addProperty(FOAF.name, name);
+
+        IndexRequest index = handler.extractAndCreatePublisher(gson, publisher);
+
+        assertEquals(result, index.toString());
+    }
+}
