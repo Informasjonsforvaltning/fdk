aef108749fd2393cbef3eeb73242fdca9ce23f00
Bjørn Grøva <bjorn.grova@brreg.no>
2016-12-15 08:50:35 +0100

    elasticsearch host, port and cluster name can now be specified when calling loadDatasetFromFile. I these are not specified, old defaults are used so as to preserve compatibility

--- harvester/src/main/java/no/dcat/harvester/crawler/Loader.java
+++ harvester/src/main/java/no/dcat/harvester/crawler/Loader.java
@@ -8,7 +8,8 @@ import no.dcat.harvester.crawler.handlers.ElasticSearchResultPubHandler;
 import no.difi.dcat.datastore.AdminDataStore;
 import no.difi.dcat.datastore.DcatDataStore;
 import no.difi.dcat.datastore.domain.DcatSource;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
@@ -21,6 +22,12 @@ import java.util.List;
  */
 
 public class Loader {
+
+    private final String DEFAULT_ELASTICSEARCH_HOST = "localhost";
+    private final int DEFAULT_ELASTICSEARCH_PORT = 9300;
+    private final String DEFAULT_ELASTICSEARCH_CLUSTER = "elasticsearch";
+
+    private static Logger logger = LoggerFactory.getLogger(Loader.class);
 
     public static void main(String[] args) {
 
@@ -35,16 +42,45 @@ public class Loader {
         loader.loadDatasetFromFile(file);
     }
 
+
+    /**
+     * Load dataset from file into elasticsearch instance on localhost
+     *
+     * @param filename filename to be loaded. Must be a valid DCAT file
+     * @return list of strings containing validation result for DCAT file
+     */
     public List<String> loadDatasetFromFile(String filename) {
+        //Kompatibilitetsmetode - sikrer kompatibiltet med opprinnelig metodesignator
+
+        return loadDatasetFromFile(filename, DEFAULT_ELASTICSEARCH_HOST, DEFAULT_ELASTICSEARCH_PORT, DEFAULT_ELASTICSEARCH_CLUSTER);
+    }
+
+
+    /**
+     * Load dataset from file into specified elasticsearch instance
+     *
+     * @param filename file to be loaded into elasticsearch. Must be a valid DCAT file
+     * @param elasticSearchHost hostname of elasticsearch server
+     * @param elasticSearchPort port where elasticsearch cluster is reached. Usually 9300
+     * @param elasticSearchCluster name of elasticsearch cluster.
+     * @return list of strings containing validation result for DCAT file
+     */
+    public List<String> loadDatasetFromFile(String filename, String elasticSearchHost, int elasticSearchPort, String elasticSearchCluster) {
         URL url;
         try {
-             url = new URL(filename);
+
+            logger.debug("loadDatasetFromFile: filename: " + filename);
+            logger.debug("loadDatasetFromFile: elasticsearch host: " + elasticSearchHost);
+            logger.debug("loadDatasetFromFile: elasticsearch port: " + elasticSearchPort);
+            logger.debug("loadDatasetFromFile: elasticsearch cluster: " +elasticSearchCluster);
+
+            url = new URL(filename);
             DcatSource dcatSource = new DcatSource("http//dcat.no/test", "Test", url.toString(), "admin_user", "123456789");
 
 
             //FusekiResultHandler fshandler = new FusekiResultHandler(dcatDataStore, null);
-            CrawlerResultHandler esHandler = new ElasticSearchResultHandler("localhost",9300, "elasticsearch");
-            CrawlerResultHandler publisherHandler = new ElasticSearchResultPubHandler("localhost",9300, "elasticsearch");
+            CrawlerResultHandler esHandler = new ElasticSearchResultHandler(elasticSearchHost,elasticSearchPort, elasticSearchCluster);
+            CrawlerResultHandler publisherHandler = new ElasticSearchResultPubHandler(elasticSearchHost,elasticSearchPort, elasticSearchCluster);
 
             LoadingCache<URL, String> brregCach = Application.getBrregCache();
             CrawlerJob job = new CrawlerJob(dcatSource, null, brregCach, esHandler, publisherHandler);
