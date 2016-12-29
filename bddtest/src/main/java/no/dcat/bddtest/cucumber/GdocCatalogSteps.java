bf8561549f1c41aaee4eba2b38bd8002c68e5a27
dskogan <david.skogan@acando.no>
2016-12-29 17:14:25 +0100

    Added cucumber test to gdoc. But docker is not running anymore

--- bddtest/src/main/java/no/dcat/bddtest/cucumber/GdocCatalogSteps.java
+++ bddtest/src/main/java/no/dcat/bddtest/cucumber/GdocCatalogSteps.java
@@ -5,11 +5,15 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import no.dcat.bddtest.WebDriverFactory;
+import no.dcat.bddtest.elasticsearch.client.DeleteIndex;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 import static java.lang.Thread.sleep;
 import static com.thoughtworks.selenium.SeleneseTestBase.assertTrue;
@@ -17,16 +21,34 @@ import static com.thoughtworks.selenium.SeleneseTestBase.assertTrue;
 /**
  * Created by dask on 16.12.2016.
  */
+@Configuration
+@ConfigurationProperties(prefix = "application")
 public class GdocCatalogSteps {
 
     private final WebDriver driver = WebDriverFactory.createWebDriver();
 
     private static Logger logger = LoggerFactory.getLogger(GdocCatalogSteps.class);
 
-    public static String PORTAL_URL = "http://fdk-por-fellesdatakatalog-ut1.ose-npc.brreg.no/"; //"http://portal-fdk.tt1.brreg.no";
-    public static String ADMIN_URL = "http://fdk-adm-fellesdatakatalog-ut1.ose-npc.brreg.no/"; //"http://admin-fdk.tt1.brreg.no";
-    public static String GDOC_CONVERT_URL  = "http://192.168.99.100:8084/convert";
-    public static String GDOC_LATEST_URL  = "http://192.168.99.100:8084/versions/latest";
+    @Value("${portal}")
+    public static String PORTAL_URL = "http://192.168.99.100:8080"; // = "http://fdk-por-fellesdatakatalog-ut1.ose-npc.brreg.no/"; //"http://portal-fdk.tt1.brreg.no";
+
+    @Value("${admin")
+    public static String ADMIN_URL = "http://192.168.99.100:8082"; // = "http://fdk-adm-fellesdatakatalog-ut1.ose-npc.brreg.no/"; //"http://admin-fdk.tt1.brreg.no";
+
+    @Value("${gdoc}")
+    public static String GDOC_CONVERT_URL = "http://192.168.99.100:8084"; //  = "http://192.168.99.100:8084/convert";
+
+    @Value("${elasticsearch.host")
+    private static String elasticsearch_host = "http://192.168.99.100"; // = "http://elasticsearch-fellesdatakatalog-ut1.ose-npc.brreg.no/";
+
+    @Value("${elasticsearch.port")
+    private static int    elasticsearch_port  = 9200;
+
+    @Given("^Databasen er tom\\.$")
+    public void cleanElasticSearch() throws Throwable {
+
+        new DeleteIndex(elasticsearch_host, elasticsearch_port).deleteIndex("dcat");
+    }
 
 
     @Given("^brukeren har lagt til nytt datasett med tittel \"([^\"]*)\" i google dokumentet$")
@@ -60,6 +82,7 @@ public class GdocCatalogSteps {
 
         WebElement row = driver.findElement(By.xpath("//tr[td[contains(text(),'gdoc')]]"));
         WebElement harvest = row.findElement(By.xpath("td/a[contains(@href,'admin/harvestDcatSource?')]"));
+        harvest.click();
 
         logger.info("harvest-url " + harvest.getAttribute("href"));
 
@@ -73,8 +96,8 @@ public class GdocCatalogSteps {
 
     }
 
-    @Then("^datasettet med tittel \"([^\"]*)\" skal finnes$")
-    public void datasettMedTittelSkalFinnes(String title) throws Throwable {
+    @Then("^søker etter \"([^\"]*)\"\\.$")
+    public void søkerEtter(String title) throws Throwable {
 
         WebElement element = driver.findElement(By.id("search"));
         element.sendKeys(title);
@@ -82,6 +105,9 @@ public class GdocCatalogSteps {
 
         sleep(2000);
 
+    }
+    @Then("^skal datasettet med tittel \"([^\"]*)\" finnes øverst på resultatsiden$")
+    public void datasettMedTittelSkalFinneØverst(String title) throws Throwable {
         WebElement results = driver.findElement(By.id("datasets"));
         WebElement heading = results.findElement(By.tagName("h4"));
         String actual = heading.getText();
--- bddtest/src/main/resources/feature/gdocKatalog.feature
+++ bddtest/src/main/resources/feature/gdocKatalog.feature
@@ -1,10 +1,12 @@
 Feature: Innføringsteamets testdata i google sheet er tilgjengelig som en DCAT-katalog
 
   Background: Endrer, konverterer og høster.
-    Given brukeren har lagt til nytt datasett med tittel "Test datasett" i google dokumentet
-    And brukeren starter gdok-konverteringen
-    And brukeren går inn i admin-grensesnittet og velger harvest 'gdok-katalog'
 
   Scenario: Det nye datasettet er tilgjengelig etter harvest
+    Given Databasen er tom.
+    And brukeren har lagt til nytt datasett med tittel "Test datasett" i google dokumentet
+    And brukeren starter gdok-konverteringen
+    And brukeren går inn i admin-grensesnittet og velger harvest 'gdok-katalog'
     When brukeren åpner portalen
-    Then datasettet med tittel "Test datasett" skal finnes
+    And søker etter "Test datasett".
+    Then skal datasettet med tittel "Test datasett" finnes øverst på resultatsiden
