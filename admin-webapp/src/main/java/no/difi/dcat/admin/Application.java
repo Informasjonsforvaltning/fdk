bfc27d2aab12b6e4f61cf76b98b82b70d5e3a73f
dskogan <david.skogan@acando.no>
2016-12-30 14:57:06 +0100

    Updated admin-webapp to run as embedded tomcat spring boot application

--- admin-webapp/src/main/java/no/difi/dcat/admin/Application.java
+++ admin-webapp/src/main/java/no/difi/dcat/admin/Application.java
@@ -1,61 +1,44 @@
 package no.difi.dcat.admin;
 
+import no.difi.dcat.admin.error.AppErrorController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.ErrorAttributes;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import no.difi.dcat.admin.error.AppErrorController;
 
-import java.util.Arrays;
-
-//@Configuration
-//@ComponentScan
-//@EnableAutoConfiguration
 @SpringBootApplication
 @EnableScheduling
 public class Application extends SpringBootServletInitializer {
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		PasswordEncoder encoder = new BCryptPasswordEncoder();
-		return encoder;
-	}
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder;
+    }
 
-	@Autowired
-	private ErrorAttributes errorAttributes;
+    @Autowired
+    private ErrorAttributes errorAttributes;
 
-	@Bean
-	public AppErrorController appErrorController() {
-		return new AppErrorController(errorAttributes);
-	}
+    @Bean
+    public AppErrorController appErrorController() {
+        return new AppErrorController(errorAttributes);
+    }
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Application.class);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		ApplicationContext ctx = SpringApplication.run(Application.class, args);
-/*
-		System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-		String[] beanNames = ctx.getBeanDefinitionNames();
-		Arrays.sort(beanNames);
-		for (String beanName : beanNames) {
-			System.out.println(beanName);
-		}*/
-	}
+        SpringApplication.run(Application.class, args);
+    }
 
 }
