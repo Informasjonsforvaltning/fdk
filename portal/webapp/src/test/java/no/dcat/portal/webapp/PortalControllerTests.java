63c79d8b4882d503d5aa36b5584be1f2a1a6eb49
dskogan <david.skogan@acando.no>
2017-01-03 15:16:20 +0100

    Startet working with test coverage

--- portal/webapp/src/test/java/no/dcat/portal/webapp/PortalControllerTests.java
+++ portal/webapp/src/test/java/no/dcat/portal/webapp/PortalControllerTests.java
@@ -1,19 +1,22 @@
 package no.dcat.portal.webapp;
 
-import org.junit.Assert;
+import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.servlet.ModelAndView;
 
+
 /**
  * Created by nodavsko on 18.10.2016.
  */
 @RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PortalControllerTests {
     private static Logger logger = LoggerFactory.getLogger(PortalControllerTests.class);
 
@@ -21,12 +24,23 @@ public class PortalControllerTests {
     PortalConfiguration config;
 
     @Test
-    public void testIndex () throws Exception {
+    public void testResultsReturnsModelAndView () throws Exception {
         PortalController controller = new PortalController(config);
         MockHttpSession session = new MockHttpSession();
         ModelAndView actual = controller.result(session, "", "", "");
 
-        Assert.assertNotEquals("result == home", null, actual);
+        assertEquals("", actual.getModel().get("themes"));
+        assertEquals("", actual.getModel().get("publisher"));
+        assertEquals("", actual.getModel().get("q"));
+    }
+
+    @Test
+    public void testDetailsAPI() throws Exception {
+        PortalController controller = new PortalController(config);
+        ModelAndView actual = controller.detail("anyIdHere");
+
+        assertNotEquals("OK", null, actual);
+
     }
 
 
