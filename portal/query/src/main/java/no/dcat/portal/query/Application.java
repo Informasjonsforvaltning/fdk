4d15e08e065d59a7d3705fb0a074bebef2e7b508
dskogan <david.skogan@acando.no>
2017-01-10 16:29:25 +0100

    sonar critical attemt portal

--- portal/query/src/main/java/no/dcat/portal/query/Application.java
+++ portal/query/src/main/java/no/dcat/portal/query/Application.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
@@ -12,7 +11,7 @@ public class Application {
 
     public static void main(String[] args) {
 
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
