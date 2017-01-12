948bfae3f0a0338483bf73cd0e7adbc8d0209c5b
Bjørn Grøva <bjorn.grova@brreg.no>
2017-01-12 15:39:00 +0100

    Renamed Harvest Application class to more appropriate name

--- harvester/src/main/java/no/dcat/harvester/HarvesterApplication.java
+++ harvester/src/main/java/no/dcat/harvester/HarvesterApplication.java
@@ -3,27 +3,20 @@ package no.dcat.harvester;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
-import no.dcat.harvester.crawler.CrawlerJob;
-import no.dcat.harvester.crawler.Loader;
-import no.dcat.harvester.crawler.handlers.ElasticSearchResultHandler;
-import no.difi.dcat.datastore.domain.DcatSource;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 @SpringBootApplication
-public class Application {
-    static Logger logger = LoggerFactory.getLogger(Application.class);
+public class HarvesterApplication {
+    static Logger logger = LoggerFactory.getLogger(HarvesterApplication.class);
 
 
     @Bean
@@ -48,7 +41,7 @@ public class Application {
 
         logger.debug("STARTING HARVESTER APPLICATION");
 
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        ApplicationContext ctx = SpringApplication.run(HarvesterApplication.class, args);
 
         logger.debug("HARVESTER APPLICATION STARTED");
 
