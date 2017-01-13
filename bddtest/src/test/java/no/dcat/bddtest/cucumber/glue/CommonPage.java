d7ca8f512c17e7c29da13426e78fb49ff76d238d
Mads Sørhaug <mgs@brreg.no>
2017-01-13 15:25:43 +0100

    FDK-200  - Cucumber test for hits pr theme

--- bddtest/src/test/java/no/dcat/bddtest/cucumber/glue/CommonPage.java
+++ bddtest/src/test/java/no/dcat/bddtest/cucumber/glue/CommonPage.java
@@ -1,22 +1,49 @@
 package no.dcat.bddtest.cucumber.glue;
 
+import com.google.common.base.Predicate;
 import io.github.bonigarcia.wdm.PhantomJsDriverManager;
 import no.dcat.bddtest.cucumber.SpringIntegrationTestConfig;
 import org.apache.commons.lang3.StringUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Common class for glue-code for pagetesting.
  */
 public abstract class CommonPage extends SpringIntegrationTestConfig {
+    private final Logger logger = LoggerFactory.getLogger(CommonPage.class);
     WebDriver driver = null;
 
     public void openPage(String page) {
         String hostname = "localhost";
         int port = 8080;
 
-        driver.navigate().to(String.format("http://%s:%d/%s", hostname, port, page));
+        driver.get(String.format("http://%s:%d/%s", hostname, port, page));
+    }
+
+    public boolean openPageWaitRetry(String page, String idToFind, int waitTimes) {
+        return openPageWaitRetry(page, d -> driver.findElement(By.id(idToFind)).isDisplayed(), waitTimes);
+    }
+
+    public boolean openPageWaitRetry(String page, Predicate<WebDriver> waitCondition, int waitTimes) {
+        logger.info(String.format("Waiting for page %s %d times", page, waitTimes));
+        openPage(page);
+        if (waitTimes <= 0) {
+            return false;
+        }
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, 10);
+            wait.until(waitCondition);
+            return true;
+        } catch (TimeoutException toe) {
+            openPageWaitRetry(page, waitCondition, --waitTimes);
+        }
+        return false;
     }
 
     protected String getEnv(String env) {
--- bddtest/src/test/java/no/dcat/bddtest/cucumber/glue/ThemePage.java
+++ bddtest/src/test/java/no/dcat/bddtest/cucumber/glue/ThemePage.java
@@ -1,14 +1,20 @@
 package no.dcat.bddtest.cucumber.glue;
 
+import cucumber.api.DataTable;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
+import java.util.List;
 
 import static com.thoughtworks.selenium.SeleneseTestBase.assertTrue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 /**
  *
  */
@@ -38,21 +44,33 @@ public class ThemePage extends CommonPage {
     }
 
     @Then("^link '(.+)'$ should exist$")
-    public void ThemeBoS_eng(String theme) {
+    public void themeBoS_eng(String theme) {
         assertTrue(driver.findElement(By.id(theme)).isEnabled());
     }
-
-
-
 
     @When("^jeg åpner temasiden i fellesdatakatalog")
     public void goToFDK() {
         driver.get("http://" + portalHostname + ":" + portalPort +"/");
     }
 
-    @Then("^vises alle '(.+)'$")
-    public void ThemeBoS(String theme) {
-        assertTrue(driver.findElement(By.id(theme)).isEnabled());
+    @Then("vises følgende tema og antall:$")
+    public void ThemeBoS(DataTable themes) {
+
+        for (List<String> themeAndAntall : themes.raw()) {
+
+            String theme = themeAndAntall.get(0);
+            String antall = themeAndAntall.get(1);
+
+            WebElement themeElement = driver.findElement(By.id(theme));
+            assertTrue(themeElement.isEnabled());
+
+            WebElement themeCount = themeElement.findElement(By.name("hits"));
+            String count = themeCount.getAttribute("innerHTML");
+
+            assertThat( String.format("Antall stemmer ikke med forventet antall for %s", theme), antall, is(count));
+        }
+
+
     }
 
 }
