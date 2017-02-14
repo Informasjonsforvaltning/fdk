c33fdc02dcc8960531a6c284dbc1a173a06b1228
Bjørn Grøva <bjorn.grova@brreg.no>
2017-02-14 16:26:05 +0100

    FDK-297 exported feature from Jira, added glue code template file

---
+++ bddtest/src/test/java/no/dcat/bddtest/cucumber/glue/ThemeFilteringSteps.java
@@ -0,0 +1,24 @@
+package no.dcat.bddtest.cucumber.glue;
+
+/**
+ * Created by bjg on 14.02.2017.
+ */
+public class ThemeFilteringSteps extends CommonPage {
+    @Given("^I see all the existing themes$")
+    public void i_see_all_the_existing_themes() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions    throw new PendingException();
+    }
+
+    @When("^I click on theme \"([^\"]*)\"$")
+    public void i_click_on_theme(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^the result list should show (\\d+) datasets$")
+    public void the_result_list_should_show_datasets(int arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+}
