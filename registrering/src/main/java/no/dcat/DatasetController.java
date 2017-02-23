1fb1f523b146b5ff4131221c59b1d26bfee4ea7e
Bjørn Grøva <bjorn.grova@brreg.no>
2017-02-23 14:49:38 +0100

    FDK-355 added method to return list of all datasets

--- registrering/src/main/java/no/dcat/DatasetController.java
+++ registrering/src/main/java/no/dcat/DatasetController.java
@@ -3,8 +3,15 @@ package no.dcat;
 import no.dcat.factory.DatasetFactory;
 import no.dcat.model.Dataset;
 import no.dcat.service.DatasetRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedResources;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,9 +21,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/dataset")
 public class DatasetController {
+
+    private static Logger logger = LoggerFactory.getLogger(DatasetController.class);
 
     @Autowired
     private DatasetRepository datasetRepository;
@@ -24,9 +35,10 @@ public class DatasetController {
     @Autowired
     private DatasetFactory datasetFactory;
 
-
     @Autowired
     private ElasticsearchTemplate elasticsearchTemplate;
+
+
 
 
     /**
@@ -57,6 +69,24 @@ public class DatasetController {
         dataset.setDescription(description);
         Dataset savedDataset = datasetRepository.save(dataset);
         return new ResponseEntity<>(savedDataset, HttpStatus.OK);
+    }
+
+
+    /**
+     * Return list of all datasets in catalog.
+     * Without parameters, the first 20 datasets are returned
+     * The returned data contains paging hyperlinks.
+     * <p>
+     * @param page number of pages. First page = 0
+     * @param size number of datasets returned
+     * @return List of data sets, with hyperlinks to other pages in search result
+     */
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public HttpEntity<PagedResources<Dataset>> listDatasets(Pageable pageable,
+                                                            PagedResourcesAssembler assembler) {
+
+        Page<Dataset> datasets = datasetRepository.findAll(new PageRequest(0,20));
+        return new ResponseEntity<>(assembler.toResource(datasets), HttpStatus.OK);
     }
 
 }
