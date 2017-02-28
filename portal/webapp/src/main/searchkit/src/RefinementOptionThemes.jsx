73a28675145995419ff27a57b2a57de44f57a9cb
Frédéric Heitmann-André <frederic.andre@gmail.com>
2017-02-28 17:10:48 +0100

    Added language support, language placeholders, general styling and templating of the searchkit application

---
+++ portal/webapp/src/main/searchkit/src/RefinementOptionThemes.jsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+
+export class RefinementOptionThemes extends React.Component {
+		render() {
+			let props = this.props;
+			let themeLabel = '';
+		  const {
+		    bemBlocks, onClick, active, disabled, style, itemKey,
+		    label, count, showCount, showCheckbox} = props;
+			if(window.themes.length > 0) {
+				themeLabel =  _.find(window.themes, props.label.substr(-4))[props.label.substr(-4)];
+			}
+			const block = bemBlocks.option;
+		  const className = block()
+		    .state({ active, disabled })
+		    .mix(bemBlocks.container("item"))
+      console.log('props.selected is ', props.active);
+			return (
+						<a onClick={props.onClick} data="ENVI" href="#" className={props.bemBlocks.option().state({active:props.active}) + ' list-group-item fdk-label fdk-label-default'}>{themeLabel}
+						 <span className="fdk-badge">(<span className="fdk-count">{props.count}</span>)</span>
+						 </a>
+			);
+		}
+}
--- portal/webapp/src/main/searchkit/src/getText.js
+++ portal/webapp/src/main/searchkit/src/getText.js
@@ -1,8 +1,7 @@
 const qs = require('qs');
 export function getText(langTextCode) {
   let queryObj = qs.parse(window.location.search.substr(1));
-  console.log('queryObj is ', queryObj);
-  let langCode = queryObj.lang || 'nb';
+  let langCode = typeof queryObj.lang === 'object' ? queryObj.lang[0] : queryObj.lang || 'nb';  
   var langStrings = window.langStrings || {
     en: {
       'Datakatalog': 'data catalog'
