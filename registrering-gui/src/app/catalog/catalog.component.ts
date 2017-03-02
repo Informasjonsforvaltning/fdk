5bf424e7f0c057ee616f22769468389bf02d318b
Mads Sørhaug <mgs@brreg.no>
2017-03-02 16:44:51 +0100

    FDK-355  - Navigation and binding on catalog

--- registrering-gui/src/app/catalog/catalog.component.ts
+++ registrering-gui/src/app/catalog/catalog.component.ts
@@ -1,10 +1,9 @@
 import {Component, OnInit} from "@angular/core";
-import {ActivatedRoute, Router, Params} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {CatalogService} from "./catalog.service";
 import "rxjs/add/operator/switchMap";
 import {Catalog} from "./catalog";
 
-const language = 'nb';
 
 @Component({
   selector: 'app-catalog',
@@ -16,6 +15,7 @@ export class CatalogComponent implements OnInit {
   catalog: Catalog;
   title: string;
   description: string;
+  language: string;
 
 
   constructor(
@@ -25,16 +25,10 @@ export class CatalogComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.language = 'nb';
     // snapshot alternative
-    // let id = this.route.snapshot.params['id'];
-    // this.service.get(id).then((catalog: Catalog) => this.catalog = catalog);
-
-    this.route.params
-      .switchMap((params: Params) => this.service.get(params['id']))
-      .subscribe((catalog: Catalog) => this.catalog = catalog);
-
-    this.title = this.catalog.title[language];
-    this.description = this.catalog.description[language];
+    let id = this.route.snapshot.params['id'];
+    this.service.get(id).then((catalog: Catalog) => this.catalog = catalog);
 
   }
 
