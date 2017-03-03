b6a78ca59ba1a6dfc127edba79174125705d27ea
Bjørn Grøva <bjorn.grova@brreg.no>
2017-03-03 15:20:58 +0100

    FDK-358 feature and test data for cucumber test for creating dataset

---
+++ bddtest/src/test/java/no/dcat/bddtest/cucumber/glue/RestCreateDatasetSteps.java
@@ -0,0 +1,46 @@
+package no.dcat.bddtest.cucumber.glue;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+/**
+ * Created by bjg on 03.03.2017.
+ */
+public class RestCreateDatasetSteps {
+    @Given("^a catalog exists$")
+    public void a_catalog_exists() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+
+    @Given("^user has access to register in the catalog$")
+    public void user_has_access_to_register_in_the_catalog() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+
+    @When("^a the user chooses to create a new dataset$")
+    public void a_the_user_chooses_to_create_a_new_dataset() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+
+    @Then("^status code HTTP (\\d+) Created is returned$")
+    public void status_code_HTTP_Created_is_returned(int arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+
+    @Then("^Json containing the dataset id is returned in http body$")
+    public void json_containing_the_dataset_id_is_returned_in_http_body() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+}
