c0a5cf8f3756eace64c68a3c26e0fbb8b4f862f6
David Skogan <david.skogan@gmail.com>
2017-03-20 16:18:27 +0100

    Changed name of application to prefix Gdoc

--- support/gdoc-import/src/main/java/no/dcat/gdoc/GdocApplication.java
+++ support/gdoc-import/src/main/java/no/dcat/gdoc/GdocApplication.java
@@ -9,9 +9,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  */
 @SpringBootApplication
 @EnableScheduling
-public class Application {
+public class GdocApplication {
     public static void main(String[] args) {
 
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(GdocApplication.class, args);
     }
 }
