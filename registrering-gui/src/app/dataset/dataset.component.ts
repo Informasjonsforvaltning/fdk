c1efb52a332f0a013ebb77e9ac565c8b60e54e55
Frédéric Heitmann-André <frederic.andre@gmail.com>
2017-03-21 16:55:33 +0100

    added support for labels in input field for themes, fetched from elastic

--- registrering-gui/src/app/dataset/dataset.component.ts
+++ registrering-gui/src/app/dataset/dataset.component.ts
@@ -1,14 +1,19 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit, ViewChild} from "@angular/core";
 import {ActivatedRoute, Router} from "@angular/router";
+import {FormGroup, FormControl} from "@angular/forms";
 import {DatasetService} from "./dataset.service";
 import {CatalogService} from "../catalog/catalog.service";
 import {Dataset} from "./dataset";
+import { Observable } from 'rxjs';
+import { Http, Response } from '@angular/http';
+import {NgModule} from '@angular/core';
 
 @Component({
   selector: 'app-dataset',
   templateUrl: './dataset.component.html',
   styleUrls: ['./dataset.component.css', '../../assets/css/designsystem.css', '../../assets/css/registrering.css']
-})
+})// class Select
+
 export class DatasetComponent implements OnInit {
   title = 'Registrer datasett';
   dataset: Dataset;
@@ -17,14 +22,20 @@ export class DatasetComponent implements OnInit {
   language: string;
   saved: boolean;
   catId: string;
+  tags:any;
 
+  form: FormGroup;
+
+  themes: Array<any>;
+  selection: Array<string>;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private service: DatasetService,
-    private catalogService: CatalogService
-  ) { }
+    private catalogService: CatalogService,
+    private http: Http
+  ) {  }
 
   ngOnInit() {
     this.language = 'nb';
@@ -32,6 +43,18 @@ export class DatasetComponent implements OnInit {
     this.catId = this.route.snapshot.params['cat_id'];
     let datasetId = this.route.snapshot.params['dataset_id'];
     this.service.get(this.catId, datasetId).then((dataset: Dataset) => this.dataset = dataset);
+
+    this.http
+        .get(`http://localhost:8083/themes`)
+        .map(data => data.json().hits.hits.map(item => {
+          return {
+            value: item._source.code,
+            label: item._source.title[this.language]
+          }
+        })).toPromise().then((data)=>this.themes = data);
+
+    this.form = new FormGroup({});
+    this.form.addControl('selectMultiple', new FormControl(''));
   }
 
   save(): void {
