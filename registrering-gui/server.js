d51c9657bc7503efb176481461d77b97f3a7fc35
Bjørn Grøva <bjorn.grova@brreg.no>
2017-03-22 14:36:23 +0100

    changed to libraries for server.js

--- registrering-gui/server.js
+++ registrering-gui/server.js
@@ -2,15 +2,12 @@
 //var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 
-var sys = require("sys"),
-  my_http = require("http");
-my_http.createServer(function(request,response){
-  sys.puts("I got kicked");
-  response.writeHeader(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(8080);
-sys.puts("Server Running on 8080");
+var connect = require('connect');
+var serveStatic = require('serve-static');
+
+connect().use(serveStatic(__dirname)).listen(8080, function(){
+  console.log('Server running on 8080...');
+});
 
 //server.listen(server_port, server_ip_address, function () {
 //  console.log( "Listening on " + server_ip_address + ", port " + server_port )
