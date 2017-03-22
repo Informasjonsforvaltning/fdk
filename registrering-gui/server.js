0eaad2ac514c960fff1015030cea629b983cb28d
Bjørn Grøva <bjorn.grova@brreg.no>
2017-03-22 13:32:51 +0100

    Openshift test with simple code

--- registrering-gui/server.js
+++ registrering-gui/server.js
@@ -1,6 +1,17 @@
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+//var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+//var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
-server.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", port " + server_port )
-});
+
+var sys = require("sys"),
+  my_http = require("http");
+my_http.createServer(function(request,response){
+  sys.puts("I got kicked");
+  response.writeHeader(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}).listen(8080);
+sys.puts("Server Running on 8080");
+
+//server.listen(server_port, server_ip_address, function () {
+//  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+//});
