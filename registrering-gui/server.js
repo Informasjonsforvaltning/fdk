05fc35dcc01eed123e473c44ed170922cb640362
Bjørn Grøva <bjorn.grova@brreg.no>
2017-03-22 14:55:32 +0100

    changed http library

--- registrering-gui/server.js
+++ registrering-gui/server.js
@@ -1,14 +1,32 @@
-//var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-//var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 
-var connect = require('connect');
-var serveStatic = require('serve-static');
+var express = require('express'),
+  fs      = require('fs'),
+  app     = express(),
+  eps     = require('ejs'),
+  morgan  = require('morgan');
 
-connect().use(serveStatic(__dirname)).listen(8080, function(){
-  console.log('Server running on 8080...');
+Object.assign=require('object-assign')
+
+app.engine('html', require('ejs').renderFile);
+app.use(morgan('combined'))
+
+
+app.get('/', function (req, res) {
+  res.sendfile('./src/index.html');
 });
 
-//server.listen(server_port, server_ip_address, function () {
-//  console.log( "Listening on " + server_ip_address + ", port " + server_port )
-//});
+
+// error handling
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500).send('Something bad happened!');
+});
+
+
+app.listen(server_port, server_ip_address);
+console.log('Server running on http://%s:%s', server_ip_address, server_port);
+
+module.exports = app ;
