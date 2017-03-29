835c8333d7f5cfcc93ee6d7eed9c3ff8c47f4371
Mads Sørhaug <mgs@brreg.no>
2017-03-29 10:29:33 +0200

    FDK-477  - Forenklet pålogging til å bare være en knapp for innlogging og utlogging. Bruker som benyttes er alltid bjg

--- registrering-gui/src/app/app.component.ts
+++ registrering-gui/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component} from "@angular/core";
+import {AuthenticationService} from "./security/authentication.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -6,9 +8,40 @@ import {Component} from "@angular/core";
   styleUrls: ['./app.component.css', '../assets/css/designsystem.css', '../assets/css/registrering.css']
 })
 export class AppComponent {
+  loading = false;
+  error = '';
   title = 'Registrer datakatalog';
+
+  constructor(private router: Router,
+              private authenticationService: AuthenticationService) {
+  }
+
+  isAuthenticated(): boolean{
+    return localStorage.getItem('username') != null && localStorage.getItem('authorization') != null;
+  }
 
   getUsername(): string {
     return localStorage.getItem('username');
   }
+
+  login() {
+    this.loading = true;
+    this.authenticationService.login()
+      .subscribe(result => {
+        if (result === true) {
+          // login successful
+          // this.router.navigate(['/']);
+        } else {
+          // login failed
+          this.error = 'Innlogging feilet';
+          this.loading = false;
+        }
+      });
+  }
+
+  logout(): void {
+    this.authenticationService.logout();
+    this.router.navigate(['/'])
+  }
+
 }
