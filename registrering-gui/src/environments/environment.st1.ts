5d8da2b718cde8bee6d5120c3a849e9886759222
David Skogan <david.skogan@gmail.com>
2017-03-30 14:09:15 +0200

    Removed hardcoded environment files

--- registrering-gui/src/environments/environment.st1.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  envName: 'st1',
-  api: 'http://fdk-apir-fellesdatakatalog-st1.ose-npc.brreg.no'
-};
--- registrering-gui/src/environments/environment.ut1.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  envName: 'ut1',
-  api: 'http://fdk-apir-fellesdatakatalog-ut1.ose-npc.brreg.no'
-};
