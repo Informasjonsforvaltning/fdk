e7cfc8dbe535eb145949e427b5089418dcc5815f
David Skogan <david.skogan@gmail.com>
2017-04-21 08:16:13 +0200

    Refactored and removed difi in package names

---
+++ admin-webapp/src/main/java/no/dcat/admin/settings/ApplicationSettings.java
@@ -0,0 +1,40 @@
+package no.dcat.admin.settings;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties(prefix = "application")
+public class ApplicationSettings {
+
+    private String harvesterUrl;
+    private String kibanaLinkFirstHalf;
+    private String kibanaLinkSecondHalf;
+
+    public String getHarvesterUrl() {
+        return harvesterUrl;
+    }
+
+    public void setHarvesterUrl(String harvesterUrl) {
+        this.harvesterUrl = harvesterUrl;
+    }
+
+    public String getKibanaLinkFirstHalf() {
+        return kibanaLinkFirstHalf;
+    }
+
+    public void setKibanaLinkFirstHalf(String kibanaLinkFirstHalf) {
+        this.kibanaLinkFirstHalf = kibanaLinkFirstHalf;
+    }
+
+    public String getKibanaLinkSecondHalf() {
+        return kibanaLinkSecondHalf;
+    }
+
+    public void setKibanaLinkSecondHalf(String kibanaLinkSecondHalf) {
+        this.kibanaLinkSecondHalf = kibanaLinkSecondHalf;
+    }
+
+}
---
+++ api-app/src/main/java/no/dcat/api/web/SupportedFormat.java
@@ -0,0 +1,40 @@
+package no.dcat.api.web;
+
+import org.springframework.http.MediaType;
+
+public enum SupportedFormat {
+
+	JSONLD("application/ld+json", "JSONLD"),
+	RDFXML("application/rdf+xml", "RDF/XML");
+	
+	private final MediaType mimetype;
+	private final String lang;
+	
+	private SupportedFormat(String mimetype, String lang) {
+		this.mimetype = MediaType.parseMediaType(mimetype);
+		this.lang = lang;
+	}
+	
+	public MediaType getMimetype() {
+		return mimetype;
+	}
+
+	public String getLang() {
+		return lang;
+	}
+
+	public static SupportedFormat parseFormat(String format) {
+		if (format == null) {
+			return JSONLD;
+		}
+		
+		format = format.replace("/", "");
+		
+		if (format.equalsIgnoreCase(RDFXML.toString())) {
+			return RDFXML;
+		} else {
+			return JSONLD;
+		}
+	}
+	
+}
