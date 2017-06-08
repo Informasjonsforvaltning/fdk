44bd1e9b6eecc0f0dbd1db156c70408cc0a0e53c
Bjørn Grøva <bjorn.grova@brreg.no>
2017-06-08 08:30:43 +0200

    FDK-416 population and saving of spatial field

--- registrering-gui/src/app/dataset/spatial/spatial.component.ts
+++ registrering-gui/src/app/dataset/spatial/spatial.component.ts
@@ -19,6 +19,8 @@ export class SpatialComponent implements OnInit {
 
     public spatialForm: FormGroup;
 
+    //model for spatial field: list of uris
+    //Note: spatial objects are saved as SKOS objects on server
     spatialUris: string[];
 
 
@@ -30,18 +32,28 @@ export class SpatialComponent implements OnInit {
         if (!this.dataset.spatials) {
             this.dataset.spatials = [];
         } else {
+            //convert incoming list of SKOS objects to list of uris to display in field
             this.spatialUris = this.dataset.spatials.map(entry => {
                 return entry.uri;
             });
         }
 
         this.spatialForm = this.toFormGroup(this.dataset);
-        this.spatialForm.setValue(this.spatialUris);
 
         this.spatialForm.valueChanges.debounceTime(400).distinctUntilChanged().subscribe(
             geoCoverage => {
                 if (geoCoverage.spatial) {
-                    this.dataset.spatials = geoCoverage.spatial;
+                    //check if spatial uri already is in list of spatial uris
+                    geoCoverage.spatial.forEach(entry =>{
+                        if(this.spatialUris.indexOf(entry) === -1) {
+                            this.spatialUris.push(entry);
+                        }
+                    })
+                    //convert list of spatial uris into SKOS objects
+                    //for now, only the uri is retained, handling of prefLabel to be decided
+                    this.dataset.spatials = this.spatialUris.map(spatialUri => {
+                        return {uri: spatialUri, prefLabel: null}
+                    })
                 }
 
                 this.onSave.emit(true);
@@ -53,7 +65,7 @@ export class SpatialComponent implements OnInit {
 
     private toFormGroup(data: Dataset) {
         return this.fb.group({
-            spatial : [ data.spatials || []]
+            spatial : [this.spatialUris]
         });
     }
 
