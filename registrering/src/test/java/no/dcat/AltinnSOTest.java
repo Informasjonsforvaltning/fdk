ba7eca7d9739f3f896bacb94e6d22f7b85824325
David Skogan <david.skogan@gmail.com>
2017-06-13 08:47:54 +0200

    Added logging to apache http

--- registrering/src/test/java/no/dcat/AltinnSOTest.java
+++ registrering/src/test/java/no/dcat/AltinnSOTest.java
@@ -1,16 +1,16 @@
 package no.dcat;
 
-import org.apache.http.HttpResponse;
+import org.apache.http.HttpHost;
+import org.apache.http.client.AuthCache;
 import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.auth.DigestScheme;
+import org.apache.http.impl.client.BasicAuthCache;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.ssl.SSLContextBuilder;
 import org.apache.http.ssl.SSLContexts;
-import org.hamcrest.Matchers;
-import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,47 +21,94 @@ import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+import javax.crypto.SecretKey;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.security.KeyManagementException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.UnrecoverableKeyException;
-import java.security.cert.CertificateException;
+import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
 
 /**
  * Created by dask on 07.06.2017.
  */
+
+// -Djavax.net.ssl.keyStore=D:/altinn/brreg.jks -Djavax.net.ssl.keyStorePassword=keystore
+
 public class AltinnSOTest {
+    private static final String ksPassword = "xEPtHApswvpiNHTp";
+    private static final String apikey = "7FB6140D-B194-4BF6-B3C8-257094FBF8C4";
+
     private static Logger logger = LoggerFactory.getLogger(AltinnSOTest.class);
 
-    private final static String apikey = "7FB6140D-B194-4BF6-B3C8-257094FBF8C4";
 
     @Test
     public void redirect() throws Throwable {
 
 
-        RestTemplate restTemplate = new RestTemplate(getRequestFactory1());
+        RestTemplate restTemplate = new RestTemplate(getRequestFactory3());
         HttpHeaders headers = new HttpHeaders();
         headers.set("ApiKey", apikey);
         headers.set("Accept", "application/hal+json");
 
         ResponseEntity<String> response = restTemplate.exchange("https://tt02.altinn.no/api/serviceowner/reportees?subject=01116100572&servicecode=3811&serviceedition=201501",
-                HttpMethod.GET,null,
+                HttpMethod.GET, null,
                 String.class, headers);
 
         logger.info("Response {}", response.getStatusCodeValue());
 
     }
 
+    ClientHttpRequestFactory getRequestFactory3() {
+        try {
+            KeyStore keyStore = KeyStore.getInstance("PKCS12");
+
+            keyStore.load(new FileInputStream(new File("D://altinn/Buypass ID-REGISTERENHETEN I BRØNNØYSUND-serienummer4659019343921797777264492-2014-06-06.p12")),
+                    ksPassword.toCharArray());
+
+
+            KeyStore.Entry entry = keyStore.getEntry("registerenheten i brønnøysund", new KeyStore.PasswordProtection(ksPassword.toCharArray()));
+
+            logger.info("Entry: {}", entry.getAttributes());
+
+            AuthCache authCache = new BasicAuthCache();
+            DigestScheme digestAuth = new DigestScheme();
+            authCache.put(new HttpHost("localhost", 8099, "https"), digestAuth);
+
+            
+            TrustStrategy acceptingTrustStrategy = ( chain, authType) -> true;
+
+            SSLContext sslContext = SSLContexts.custom()
+                    .loadKeyMaterial(keyStore, ksPassword.toCharArray())
+                    .loadTrustMaterial(null, acceptingTrustStrategy)
+
+                    .build();
+
+            HttpClient httpClient = HttpClients.custom()
+                    .setSslcontext(sslContext)
+                    .build();
+
+            HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+
+            requestFactory.setHttpClient(httpClient);
+            return requestFactory;
+        } catch (Exception e) {
+            logger.error("Error",e);
+        }
+        return null;
+    }
+
     ClientHttpRequestFactory getRequestFactory2() {
         try {
-            TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+            TrustStrategy acceptingTrustStrategy = ( chain, authType) -> true;
 
             SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom()
                     .loadTrustMaterial(null, acceptingTrustStrategy)
@@ -77,10 +124,10 @@ public class AltinnSOTest {
             return requestFactory;
 
         } catch (Exception e) {
-            logger.error("Exception RF2",e);
+            logger.error("Exception RF2", e);
         }
 
-     return null;
+        return null;
     }
 
     ClientHttpRequestFactory getRequestFactory1() {
@@ -88,10 +135,10 @@ public class AltinnSOTest {
         try {
             KeyStore keyStore = KeyStore.getInstance("PKCS12"); //KeyStore.getDefaultType());
             keyStore.load(new FileInputStream(new File("D://altinn/Buypass ID-REGISTERENHETEN I BRØNNØYSUND-serienummer4659019343921797777264492-2014-06-06.p12")),
-                    "xEPtHApswvpiNHTp".toCharArray());
+                    ksPassword.toCharArray());
 
             SSLContext sslcontext = SSLContexts.custom()
-                    .loadKeyMaterial(keyStore,"xEPtHApswvpiNHTp".toCharArray() )
+                    .loadKeyMaterial(keyStore, ksPassword.toCharArray())
                     .build();
 
             /*
@@ -106,7 +153,7 @@ public class AltinnSOTest {
             SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslcontext);
 
             CloseableHttpClient httpclient = HttpClients.custom()
-                    .setSSLSocketFactory(sslsf). build();
+                    .setSSLSocketFactory(sslsf).build();
 
             ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(
                     httpclient);
@@ -116,6 +163,58 @@ public class AltinnSOTest {
             logger.error("Exception ", e);
         }
         return null;
+    }
+
+    // source: http://vafer.org/blog/20061010073725/
+    ClientHttpRequestFactory getRequestFactory4() {
+        String pKeyPassword = ksPassword;
+        try {
+            KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("SunX509");
+            KeyStore keyStore = KeyStore.getInstance("PKCS12");
+
+            keyStore.load(new FileInputStream(new File("D://altinn/Buypass ID-REGISTERENHETEN I BRØNNØYSUND-serienummer4659019343921797777264492-2014-06-06.p12")),
+                    pKeyPassword.toCharArray());
+
+            keyManagerFactory.init(keyStore, pKeyPassword.toCharArray());
+
+            SSLContext context = SSLContext.getInstance("TLS");
+            context.init(keyManagerFactory.getKeyManagers(), null, new SecureRandom());
+            /*
+            URL url = new URL("https://tt02.altinn.no/api/serviceowner/reportees?subject=01116100572&servicecode=3811&serviceedition=201501");
+            HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+            con.setSSLSocketFactory(context.getSocketFactory());
+
+            String result = readStream(con.getInputStream());
+            logger.info(result);
+*/
+
+            SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(context);
+
+            CloseableHttpClient httpclient = HttpClients.custom()
+                    .setSSLSocketFactory(sslsf).build();
+
+            ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(
+                    httpclient);
+
+            return requestFactory;
+
+        } catch (Exception e) {
+            logger.error("RF4",e);
+        }
+        return null;
+    }
+
+    private static String readStream(InputStream is) throws IOException {
+        final BufferedReader reader = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+        StringBuilder total = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            total.append(line);
+        }
+        if (reader != null) {
+            reader.close();
+        }
+        return total.toString();
     }
 
 }
