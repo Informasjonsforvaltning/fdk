cfa2db68c95de7e27482664f4e456c942e346c5b
Håvard Ottestad <hmottestad@gmail.com>
2017-07-07 14:37:27 +0200

    FDK-626 running app

---
+++ themes/src/main/java/no/dcat/themes/ApplicationThemes.java
@@ -0,0 +1,12 @@
+package no.dcat.themes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationThemes {
+    public static void main(String... args) {
+        SpringApplication.run(ApplicationThemes.class, args);
+    }
+
+}
