b71ea968cdbdcd839bd04bdd7662826b00de1084
Frédéric Heitmann-André <frederic.andre@gmail.com>
2017-08-21 13:41:08 +0200

    working themes checkboxes

--- registrering-gui/src/app/dataset/information/information.component.ts
+++ registrering-gui/src/app/dataset/information/information.component.ts
@@ -23,7 +23,6 @@ export class InformationComponent implements OnInit {
     allThemes: any[];
     themes: any[];
     subjects: any[];
-    availableThemes: any[];
 
     constructor(private fb: FormBuilder,
     private formBuilder: FormBuilder,
@@ -38,88 +37,77 @@ export class InformationComponent implements OnInit {
                 return keyword['nb'];
             });
         }
-        this.fetchThemes();
-        this.availableThemes = [
-          {label:'Transport', selected: true},
-          {label:'Regioner og byer', selected: false},
-          {label:'Befolkning og sammfunn', selected: false},
-          {label:'Jordbruk, fiskeri, skogbruk og mat', selected:false},
-          {label:'Regioner og byer', selected:false}
-        ];
-        // for debug data. Remove!!
-        this.dataset.themes.forEach((theme, i, )=>{
-          if(!theme.title) this.dataset.themes.splice(i-1);
+console.log('1this.allThemes is ', this.allThemes);
+        this.fetchThemes().then(()=> {
+
+                  console.log('2this.allThemes is ', this.allThemes);
+
+  /*                this.themes = this.dataset.themes ? this.dataset.themes.map((tema) => {
+                          return tema.uri
+                      }) : [];
+*/
+
+                  console.log('3this.allThemes is ', this.allThemes);
+                if(this.dataset.themes) {
+                  this.allThemes.forEach((theme, themeIndex, themeArray) => {
+                    console.log('this.dataset is ', this.dataset.themes);
+                    this.dataset.themes.forEach((datasetTheme, datasetThemeIndex, datasetThemeArray)=> {
+                      console.log('typeof datasetTheme.title is ', typeof datasetTheme.title);
+                      if(theme.value === datasetTheme.uri) {
+                        themeArray[themeIndex].selected = true;
+                      }
+                    })
+                  })
+                }
+                        this.informationForm = this.toFormGroup(this.dataset);
+
+
+                                this.informationForm.valueChanges.debounceTime(400).distinctUntilChanged().subscribe(
+                                    (information) => {
+                              console.log('4this.allThemes is ', this.allThemes);
+                                      console.log('information is ', information);
+                                        if (information.keywords.length === 0) {
+                                            this.dataset.keywords = null;
+                                        } else {
+                                            this.dataset.keywords = information.keywords.map(keyword => {
+                                                return {nb: keyword}
+                                            });
+                                        }
+
+                                        this.dataset.themes = [];
+                                        information.themesArray.forEach((checkbox, checkboxIndex)=>{
+                                            this.allThemes.forEach((theme, index)=>{
+                                                if(theme.label) theme.title = {nb: theme.label};
+                                                if(theme.value) theme.uri = theme.value;
+                                                if((index === checkboxIndex) && checkbox) this.dataset.themes.push(theme);
+                                            });
+                                        });
+                                        information.languages = null;
+
+                                        if (information.subjects.length === 0) {
+                                            this.dataset.subjects = null;
+                                        } else {
+                                            this.dataset.subjects = information.subjects;
+                                        }
+
+                                        this.onSave.emit(true);
+                                    }
+                                );
         });
 
-        console.log('this.dataset.themes is ', this.dataset.themes);
-
-        this.themes = this.dataset.themes ? this.dataset.themes.map((tema) => {
-                return tema.uri
-            }) : [];
-        this.allThemes =
-        this.dataset.themes ?
-          this.dataset.themes.map((thema) => {
-                if(thema.title) return {value: thema.uri, label: thema.title['nb']}
-          }) :
-        [];
-
-      if(this.dataset.themes) {
-        this.allThemes.forEach((theme, themeIndex, themeArray) => {
-          this.dataset.themes.forEach((datasetTheme, datasetThemeIndex, datasetThemeArray)=> {
-            console.log('typeof datasetTheme.title is ', typeof datasetTheme.title);
-            if(datasetTheme.title && theme.code === datasetTheme.title.nb) {
-              themeArray[themeIndex].selected = true;
-            }
-          })
-        })
-      }
 
 
-      this.dataset.themes = [];
 
       this.dataset.themes = this.dataset.themes || [];
 
         this.subjects = this.dataset.subjects || [];
 
-        this.informationForm = this.toFormGroup(this.dataset);
-
-        this.informationForm.valueChanges.debounceTime(400).distinctUntilChanged().subscribe(
-            (information) => {
-              console.log('information is ', information);
-                if (information.keywords.length === 0) {
-                    this.dataset.keywords = null;
-                } else {
-                    this.dataset.keywords = information.keywords.map(keyword => {
-                        return {nb: keyword}
-                    });
-                }
-
-                this.dataset.themes = [];
-                information.themesArray.forEach((checkbox, checkboxIndex)=>{
-                    this.availableThemes.forEach((theme, index)=>{
-                        if(theme.label) theme.prefLabel = {nb: theme.label};
-                        if((index === checkboxIndex) && checkbox) this.dataset.themes.push(theme);
-                    });
-                });
-                information.languages = null;
-
-                if (information.subjects.length === 0) {
-                    this.dataset.subjects = null;
-                } else {
-                    this.dataset.subjects = information.subjects;
-                }
-
-                this.onSave.emit(true);
-            }
-        );
     }
 
-
     private toFormGroup(data: Dataset) {
-      //Befolkning og samfunn - Jordbruk, fiskeri, skogbruk og matForvaltning og offentlig sektorRegioner og byerJustis, rettssystem og allmenn sikkerhetTransportMiljøInternasjonale temaerEnergiUtdanning, kultur og sportHelseVitenskap og teknologiØkonomi og finansundefinedundefined
+      console.log('this.allThemes is ', this.allThemes);
         return this.fb.group({
-            themes: [this.themes],
-            themesArray: this.formBuilder.array(this.availableThemes.map(s => {return this.formBuilder.control(s.selected)})),
+            themesArray: this.formBuilder.array(this.allThemes.map(s => {return this.formBuilder.control(s.selected)})),
             subjects: [this.subjects],
             keywords: [this.keywords]
         });
@@ -144,7 +132,7 @@ export class InformationComponent implements OnInit {
     }
 
     fetchThemes() {
-        this.themesService.fetchThemes('nb').then(themes =>
+        return this.themesService.fetchThemes('nb').then(themes =>
             this.allThemes = themes);
     }
 }
