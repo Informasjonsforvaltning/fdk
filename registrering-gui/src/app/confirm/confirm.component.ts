1ac76c7432801e346693055470df315875479a92
Håvard Ottestad <hmottestad@gmail.com>
2017-08-23 12:33:36 +0200

    FDK-652 css import clean up

--- registrering-gui/src/app/confirm/confirm.component.ts
+++ registrering-gui/src/app/confirm/confirm.component.ts
@@ -10,7 +10,7 @@ export interface ConfirmModel {
 }
 @Component({
   selector: 'confirm',
-  styleUrls: ['../../assets/css/designsystem.css', '../../assets/css/registrering.css'],
+  styleUrls: [],
   template: `<div class="modal-dialog">
                 <div class="modal-content">
                    <div class="modal-header">
