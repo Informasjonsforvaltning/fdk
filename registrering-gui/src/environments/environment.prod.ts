31c173ef83eff5a4c6306c9300b34b22e4a53715
Frédéric Heitmann-André <frederic.andre@gmail.com>
2017-08-30 15:43:50 +0200

    changed port number for queryURL

--- registrering-gui/src/environments/environment.prod.ts
+++ registrering-gui/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
- declare var process:any;
+declare var process:any;
 var regApiUrl = window.document.getElementsByTagName('body')[0].getAttribute('data-reg-api-url');
 var queryUrl = window.document.getElementsByTagName('body')[0].getAttribute('data-query-service-url');
 var themesAndCodesUrl = window.document.getElementsByTagName('body')[0].getAttribute('data-themes-and-codes-url');
@@ -6,5 +6,5 @@ export const environment = {
   production: true,
   envName: 'prod',
   api: regApiUrl.indexOf('<') === 0 ? 'http://localhost:8099' : regApiUrl,
-  queryUrl: queryUrl.indexOf('<') === 0 ? 'http://localhost:8083' : queryUrl
+  queryUrl: queryUrl.indexOf('<') === 0 ? 'http://localhost:8100' : queryUrl
 };
