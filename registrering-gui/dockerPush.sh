5faeaabd6693f54a1b0dca3c1aa20919c3672517
Håvard Ottestad <hmottestad@gmail.com>
2017-08-30 12:58:06 +0200

    Tag with git commit id and push

--- registrering-gui/dockerPush.sh
+++ registrering-gui/dockerPush.sh
@@ -16,4 +16,8 @@ if [ "${BRANCH}" = "develop" ] ; then
 
   docker push dcat/reg-gui:latest
 
+  # tag with commit ID and push so that we can easily roll back if needed
+  docker tag dcatno/registration:latest dcat/reg-gui:GIT_${TRAVIS_COMMIT}
+  docker push dcat/reg-gui:GIT_${TRAVIS_COMMIT}
+
 fi
