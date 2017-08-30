2aae1f4183b1bb4e222015571cf5ca12d2174e89
Frédéric Heitmann-André <frederic.andre@gmail.com>
2017-08-30 15:48:45 +0200

    removed old variable

--- registrering-gui/src/environments/environment.prod.ts
+++ registrering-gui/src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 declare var process:any;
 var regApiUrl = window.document.getElementsByTagName('body')[0].getAttribute('data-reg-api-url');
 var queryUrl = window.document.getElementsByTagName('body')[0].getAttribute('data-query-service-url');
-var themesAndCodesUrl = window.document.getElementsByTagName('body')[0].getAttribute('data-themes-and-codes-url');
 export const environment = {
   production: true,
   envName: 'prod',
