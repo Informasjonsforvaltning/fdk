f8e78be10d0bb324bd2ea3ba5f3ac8a0cae8e5ae
David Skogan <david.skogan@gmail.com>
2017-09-08 10:13:51 +0200

    Test now runs locally

--- applications/registration-api/src/test/java/no/dcat/authorization/AltinnAuthorizationIT.java
+++ applications/registration-api/src/test/java/no/dcat/authorization/AltinnAuthorizationIT.java
@@ -1,27 +1,19 @@
 package no.dcat.authorization;
 
-import no.dcat.RegisterApplication;
-import org.assertj.core.api.Assertions;
-import org.hamcrest.core.Is;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.util.ReflectionTestUtils;
-import org.springframework.web.client.HttpClientErrorException;
-import sun.nio.cs.Surrogate;
 
 import java.util.List;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 /**
@@ -47,13 +39,12 @@ public class AltinnAuthorizationIT {
         List<Entity> actualEntities = authorizationService.getAuthorizedEntities("02084902333");
 
         Assert.assertNotNull(actualEntities);
-        //Assertions.assertThat(actualEntities.size(), Is.is(2) );
+        assertThat(actualEntities.size(), is(3) );
 
         logger.info("# of entities {}", actualEntities.size());
         for (Entity entity : actualEntities) {
             logger.info("Entity {}", entity.toString());
         }
-
 
     }
 
