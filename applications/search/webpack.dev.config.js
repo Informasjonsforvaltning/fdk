75fbf18b584d0595c91951de0b476d31a74d1994
Øyvind Isaksen <oyvind.isaksen@acando.no>
2017-09-20 14:51:28 +0200

    endring package.json og webpack.dev.config

--- applications/search/webpack.dev.config.js
+++ applications/search/webpack.dev.config.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   devtool:"eval",
@@ -16,23 +17,25 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
+    new ExtractTextPlugin("styles.css")
   ],
   resolve: {
     alias: {
       react: path.resolve('./node_modules/react')
     },
-    extensions:[".js", ".jsx", ".webpack.js", ".web.js",""]
+    extensions:[".js", ".jsx", ".webpack.js", ".web.js"]
   },
   resolveLoader: {
-    root: path.join(__dirname, "node_modules")
+    //root: path.join(__dirname, "node_modules")
+    modules: [__dirname, 'node_modules']
   },
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
         exclude: /(node_modules)/,
-        loader: 'babel',
+        loader: 'babel-loader',
         query: {
           presets: [
             require.resolve('babel-preset-es2015'),
@@ -42,7 +45,11 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        loaders: ["style", "css", "sass"]
+        //loaders: ["style", "css", "sass"]
+        use: ExtractTextPlugin.extract({  
+          fallback: 'style-loader',  
+          use: ['css-loader', 'sass-loader']  
+        })
       },
       {
         test: /\.(jpg|png|svg)$/,
