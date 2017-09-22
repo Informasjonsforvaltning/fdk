ddfb446a8f6972f42c315216b8ddc52543cda62c
Øyvind Isaksen <oyvind.isaksen@acando.no>
2017-09-22 09:26:32 +0200

    import style og endre search-url

--- applications/search/src/QueryTransport.jsx
+++ applications/search/src/QueryTransport.jsx
@@ -88,7 +88,7 @@ export class QueryTransport extends AxiosESTransport {
     }
 
     return this.axios.get(
-			'/search?q=' +
+      `${this.options.searchUrlPath}?q=` +
 			(query.query ? encodeURIComponent(query.query.simple_query_string.query) : '') +
 			'&from=' +
 			((!query.from) ? '0' : query.from) +
