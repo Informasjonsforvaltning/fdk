6d396b86942acc01ad6435697268e3b714321c73
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2017-09-27 15:10:28 +0200

    Adding TDBInferenceService with Thread Scope

--- applications/reference-data/src/main/java/no/dcat/themes/database/RequiresConnectionReturn.java
+++ applications/reference-data/src/main/java/no/dcat/themes/database/RequiresConnectionReturn.java
@@ -1,5 +1,5 @@
 package no.dcat.themes.database;
 
 public interface RequiresConnectionReturn<R> {
-    R withConnection(TDBService connection);
+    R withConnection(TDBInferenceService connection);
 }
--- applications/reference-data/src/main/java/no/dcat/themes/database/TDBConnection.java
+++ applications/reference-data/src/main/java/no/dcat/themes/database/TDBConnection.java
@@ -8,10 +8,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class TDBConnection {
 
-    TDBService tdbService;
+    private TDBInferenceService tdbService;
 
     @Autowired
-    public TDBConnection(TDBService tdbService) {
+    public TDBConnection(TDBInferenceService tdbService) {
         this.tdbService = tdbService;
     }
 
