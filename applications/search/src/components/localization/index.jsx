0a2455e8e863dae73da2794deadf877692a803c0
Øyvind Isaksen <oyvind.isaksen@acando.no>
2017-10-02 15:03:39 +0200

    språk nn

--- applications/search/src/components/localization/index.jsx
+++ applications/search/src/components/localization/index.jsx
@@ -2,9 +2,10 @@ import LocalizedStrings from 'react-localization';
 
 import en from '../../l10n/en.json';
 import nb from '../../l10n/nb.json';
+import nn from '../../l10n/nn.json';
 
 let localization =  new LocalizedStrings({
-  en, nb
+  en, nb, nn
 });
 
 localization.setLanguage('nb');
