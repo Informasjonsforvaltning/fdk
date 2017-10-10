c417d79a5edfa53b52ab06fdb33f1d3c9248094e
Bjørn Grøva <bjorn.grova@brreg.no>
2017-10-10 10:35:44 +0200

    FDK-708 started setup of new files

---
+++ applications/registration/src/app/dataset/relations/relations.component.ts
@@ -0,0 +1,28 @@
+import {Component, Input, Output, OnInit, EventEmitter} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Dataset} from "../dataset";
+
+import {DatasetComponent} from "../dataset.component";
+
+@Component({
+  selector: 'relations',
+  templateUrl: './relations.component.html',
+  styleUrls: ['./relations.component.css']
+})
+
+export class ContentComponent implements OnInit {
+  @Input('dataset')
+  public dataset: Dataset;
+
+  @Output()
+  onSave = new EventEmitter<boolean>();
+
+  public contentForm: FormGroup;
+
+  relationType;
+  relationUrl;
+
+  constructor(private fb: FormBuilder,
+              private parent: DatasetComponent) {
+  }
+}
