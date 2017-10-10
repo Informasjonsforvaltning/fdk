772d2a0448b6778ab67a3ee0a00fcd54caab0a83
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2017-10-10 11:24:34 +0200

    FDK-712: First stab at tests

--- applications/reference-data/src/test/java/no/dcat/themes/builders/HelpTextControllerTest.java
+++ applications/reference-data/src/test/java/no/dcat/themes/builders/HelpTextControllerTest.java
@@ -1,7 +1,28 @@
 package no.dcat.themes.builders;
 
+import no.dcat.shared.HelpText;
+import no.dcat.themes.Controller;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
 /**
  * Created by extkkj on 10.10.2017.
  */
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class HelpTextControllerTest {
-}
+        @Autowired
+        private Controller controller;
+
+        @Test
+        public void helpTextsExists() throws Throwable {
+            List<HelpText> helpTexts = controller.helpTexts("foo");
+            System.out.println(helpTexts.toString());
+        }
+    }
