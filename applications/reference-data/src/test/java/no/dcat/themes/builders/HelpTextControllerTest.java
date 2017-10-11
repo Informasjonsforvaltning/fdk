8ec6b1687563704666cb72ff84322fb93bd098f2
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2017-10-11 10:09:48 +0200

    FDK-712: Improve tests

--- applications/reference-data/src/test/java/no/dcat/themes/builders/HelpTextControllerTest.java
+++ applications/reference-data/src/test/java/no/dcat/themes/builders/HelpTextControllerTest.java
@@ -2,8 +2,11 @@ package no.dcat.themes.builders;
 
 import no.dcat.shared.HelpText;
 import no.dcat.themes.Controller;
+
 import static org.junit.Assert.*;
-import org.hamcrest.Matchers;
+
+import static org.hamcrest.Matchers.*;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,22 +23,36 @@ import java.util.List;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class HelpTextControllerTest {
-        @Autowired
-        private Controller controller;
+    @Autowired
+    private Controller controller;
 
-        @Test
-        public void helpTextsHasDesc() throws Throwable {
-            List<HelpText> helpTexts = controller.helpTexts("foo");
-            //           helpTexts.stream().anyMatch(helpText -> {helpText.get
-            //                   equals("http://brreg.no/fdk/fields#Dataset_title")});
-            assertThat(helpTexts.get(2).getDescription().get("nb"), Matchers.is(Matchers.startsWith("En setnings-beskrivelse av formålet til datasettet")));
-        }
-
-        @Test
-        public void helpTextsHasAbstract() throws Throwable {
-        List<HelpText> helpTexts = controller.helpTexts("foo");
-            assertEquals(helpTexts.get(2).getAbstract().get("nb"), "Beskrivelsen skal være kortfattet og ikke gjentas i Beskrivelsesfeltet.");
-            assertEquals(helpTexts.get(2).getShortdesc().get("nb"), "Beskrivelsen skal være kortfattet og ikke gjentas i Beskrivelsesfeltet.");
-            System.out.println("OMG" + helpTexts.get(2).getShortdesc().toString());
-        }
+    @Test
+    public void helpTextsSingleIsSingle() throws Throwable {
+        List<HelpText> helpTexts = controller.helpTexts("Dataset_description");
+        assertEquals(helpTexts.size(), 1);
     }
+
+    @Test
+    public void helpTextsMoreisMore() throws Throwable {
+        List<HelpText> helpTexts = controller.helpTexts();
+        assertThat(helpTexts.size(), greaterThan(1));
+    }
+
+    @Test
+    public void helpTextsSingleHasDesc() throws Throwable {
+        List<HelpText> helpTexts = controller.helpTexts("Dataset_description");
+        assertThat(helpTexts.get(0).getDescription(), is(notNullValue()));
+     //   assertThat(helpTexts.get(0).getDescription(), isMapContaining); Want to test the Map<String, String> structure
+        assertThat(helpTexts.get(0).getDescription().get("nb"), is(notNullValue()));
+    }
+
+    @Test
+    public void helpTextsSingleHasAbstract() throws Throwable {
+        List<HelpText> helpTexts = controller.helpTexts("Dataset_description");
+        assertThat(helpTexts.get(0).getShortdesc(), is(notNullValue()));
+        assertThat(helpTexts.get(0).getAbstract(), is(notNullValue()));
+        //   assertThat(helpTexts.get(0).getShortdesc(), isMapContaining); Want to test the Map<String, String> structure
+        assertThat(helpTexts.get(0).getShortdesc().get("nb"), is(notNullValue()));
+
+    }
+}
