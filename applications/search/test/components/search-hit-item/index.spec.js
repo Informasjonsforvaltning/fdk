a42f7033250b3b84715a93c51b135f75a1b0f15c
Øyvind Isaksen <oyvind.isaksen@acando.no>
2017-10-12 09:37:41 +0200

    fix test and linting detailspage

--- applications/search/test/components/search-hit-item/index.spec.js
+++ applications/search/test/components/search-hit-item/index.spec.js
@@ -6,118 +6,108 @@ describe('SearchHitItem', () => {
 
   const props =
     {
-      "_index": "dcat",
-      "_type": "dataset",
-      "_id": "http://data.brreg.no/datakatalog/dataset/961181399/60",
-      "_score": 0.26726124,
-      "_source": {
-        "id": "http://data.brreg.no/datakatalog/dataset/974761076/63",
+      "_index" : "dcat",
+      "_type" : "dataset",
+      "_id" : "http://data.brreg.no/datakatalog/dataset/974761076/80",
+      "_score" : 0.85435724,
+      "_source" : {
+        "id": "3645d5a8-6689-4fc1-8684-d3d6c2d47386",
+        "uri": "http://brreg.no/catalogs/987654321/datasets/3645d5a8-6689-4fc1-8684-d3d6c2d47386",
+        "catalog": "987654321",
         "title": {
-          "nb": "Innrapportert inntekt"
+          "nb": "AAMarkagrensen Oslo Kommune og nærliggende kommuner"
         },
         "description": {
-          "nb": "Informasjon om innrapportert inntekt"
+          "nb": "Datasettet avgrenser område for virkeområdet til lov 6. juni 2009 nr. 35 om naturområder i Oslo og nærliggende kommuner (markaloven) som trådte i kraft 1. september 2009. Markalovens virkeområde er fastsatt i forskrift 4. september 2015 nr. 1032 om justering av markagrensen fastlegger markalovens geografiske virkeområde med tilhørende kart."
         },
-        "contactPoint": {
-          "id": "http://data.brreg.no/datakatalog/kontaktpunkt/a-13",
-          "email": "mailto:brukerstotte.datasamarbeid@skatteetaten.no",
-          "organizationUnit": "Skatteetaten, datasamarbeid"
+        "objective": {
+          "nb": "Datasettes formål er nullam quis rius eget urna mollis ornare vel eu leo. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Cras mattis consectetur purus sit amet fermentum. Vestibulum id ligula porta felis euismod semper."
         },
-        "keyword": {
-          "nb": [
-            "inntekt",
-            "gjeld og fradrag",
-            "grunnlagsdata",
-            "formue"
-          ]
-        },
-        "publisher": {
-          "id": "http://data.brreg.no/enhetsregisteret/enhet/974761076",
-          "name": "SKATTEETATEN",
-          "overordnetEnhet": "972417807",
-          "organisasjonsform": "ORGL",
-          "subPublisher": [],
-          "aggrSubPublisher": []
-        },
-        "language": {
-          "uri": "http://publications.europa.eu/resource/authority/language/NOR",
-          "authorityCode": "NOR",
-          "prefLabel": {
-            "nb": "Norsk",
-            "nn": "Norsk",
-            "no": "Norsk",
-            "en": "Norwegian"
-          }
-        },
-        "theme": [
+        "contactPoint": [
           {
-            "id": "http://publications.europa.eu/resource/authority/data-theme/GOVE",
-            "code": "GOVE",
-            "startUse": "2015-10-01",
-            "title": {
-              "it": "Governo e settore pubblico",
-              "nb": "Forvaltning og offentlig sektor",
-              "en": "Government and public sector",
-              "hr": "Vlada i javni sektor",
-              "es": "Gobierno y sector público",
-              "de": "Regierung und öffentlicher Sektor",
-              "sk": "Vláda a verejný sektor",
-              "ro": "Guvern şi sector public",
-              "bg": "Правителство и публичен сектор",
-              "et": "Valitsus ja avalik sektor",
-              "el": "Κυβέρνηση και δημόσιος τομέας",
-              "pl": "Rząd i sektor publiczny",
-              "cs": "Vláda a veřejný sektor",
-              "ga": "Rialtas agus earnáil phoiblí",
-              "pt": "Governo e setor público",
-              "lt": "Vyriausybė ir viešasis sektorius",
-              "lv": "Valdība un sabiedriskais sektors",
-              "mt": "Gvern u settur pubbliku",
-              "hu": "Kormányzat és közszféra",
-              "da": "Regeringen og den offentlige sektor",
-              "fi": "Valtioneuvosto ja julkinen sektori",
-              "fr": "Gouvernement et secteur public",
-              "sl": "Vlada in javni sektor",
-              "sv": "Regeringen och den offentliga sektorn",
-              "nl": "Overheid en publieke sector"
-            },
-            "conceptSchema": {
-              "id": "http://publications.europa.eu/resource/authority/data-theme",
-              "title": {
-                "en": "Dataset types Named Authority List"
-              },
-              "versioninfo": "20160921-0",
-              "versionnumber": "20160921-0"
+            "id": "3c50088c-489f-4168-95e6-e6f3cd0c67c5",
+            "uri": "http://brreg.no/catalogs/987654321/contacts/null",
+            "email": "digitalisering@kartverket.no",
+            "organizationUnit": "Avdeling for digitalisering",
+            "hasURL": "http://testetaten.no/url",
+            "hasTelephone": "22306022"
+          }
+        ],
+        "keyword": [
+          {
+            "nb": "Bestemmelse"
+          },
+          {
+            "nb": "jord"
+          },
+          {
+            "nb": "regulering"
+          },
+          {
+            "nb": "statlig bestemmelse"
+          }
+        ],
+        "publisher": {
+          "uri": "http://data.brreg.no/enhetsregisteret/enhet/987654321",
+          "id": "987654321",
+          "name": "TESTETATEN"
+        },
+        "issued": 1325376000000,
+        "modified": 1474421403000,
+        "language": [
+          {
+            "uri": "http://publications.europa.eu/resource/authority/language/NOR",
+            "code": "NOR",
+            "prefLabel": {
+              "nb": "Norsk"
             }
           }
         ],
-        "catalog": {
-          "id": "http://data.brreg.no/datakatalog/katalog/974761076/5",
-          "title": {
-            "nb": "Skatteetaten datakatalog"
+        "landingPage": [
+          "http://testetaten.no/landingsside/nr1"
+        ],
+        "theme": [
+          {
+            "uri": "http://publications.europa.eu/resource/authority/data-theme/GOVE",
+            "title": {
+              "nb": "Forvaltning og offentlig støtte"
+            }
           },
-          "description": {
-            "nb": "Katalog over datasett i Skatteetaten"
-          },
-          "publisher": {
-            "id": "http://data.brreg.no/enhetsregisteret/enhet/974761076",
-            "name": "SKATTEETATEN",
-            "overordnetEnhet": "972417807",
-            "organisasjonsform": "ORGL",
-            "subPublisher": [],
-            "aggrSubPublisher": []
-          },
-          "themeTaxonomy": []
-        },
+          {
+            "uri": "http://publications.europa.eu/resource/authority/data-theme/ENVI",
+            "title": {
+              "nb": "Miljø"
+            }
+          }
+        ],
         "distribution": [
           {
-            "id": "http://data.brreg.no/datakatalog/distribusjon/26",
-            "title": {},
+            "id": "96d63bba-e624-4f39-ad26-26b8c6a68880",
+            "uri": "http://brreg.no/catalogs/987654321/datasets/3645d5a8-6689-4fc1-8684-d3d6c2d47386/distributions/96d63bba-e624-4f39-ad26-26b8c6a68880",
             "description": {
-              "nb": "Innrapportert inntekt Skatteetaten"
+              "nb": "Dette er beskrivelsen av distribusjonen. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Vestibulum id ligula porta felis euismod semper con desbit arum. Se dokumentasjon for denne distribusjonen."
             },
-            "accessURL": "https://api.skatteetaten.no/api/innrapportert/inntektsmottaker/personidentifikator/oppgave/inntekt?fraOgMed=YYYY-MM&tilOgMed=YYYY-MM",
-            "format": "application/xml, application/json"
+            "accessURL": [
+              "http://www.detteerenlenke.no/til-nedlasting",
+              "http://www.detteerenannenlenke.no/til-en-annen-nedlasting\n",
+              "http://www.detteerentredjelenke.no/til-en-tredje-nedlasting"
+            ],
+            "license": {
+              "uri": "https://www.kartverket.no/geodataarbeid/standarder/sosi/",
+              "prefLabel": {
+                "nb": "SOSI"
+              },
+              "extraType": "dct:Standard"
+            },
+            "page": {
+              "uri": "http://lenke/til/mer/info",
+              "prefLabel": {
+                "nb": "Dokumentasjon av distribusjonen"
+              }
+            },
+            "format": [
+              "application/json"
+            ]
           }
         ],
         "sample": [
@@ -135,59 +125,191 @@ describe('SearchHitItem', () => {
             ]
           }
         ],
-        "conformsTo": [],
-        "temporal": [],
-        "spatial": [],
+        "conformsTo": [
+          {
+            "uri": "https://www.kartverket.no/geodataarbeid/standarder/sosi/",
+            "prefLabel": {
+              "nb": "SOSI"
+            },
+            "extraType": "dct:Standard"
+          }
+        ],
+        "temporal": [
+          {
+            "startDate": 1483228800000,
+            "endDate": 1514764799000
+          }
+        ],
+        "spatial": [
+          {
+            "uri": "http://www.geonames.org/3162656/asker.html",
+            "prefLabel": {
+              "nb": "Asker"
+            }
+          },
+          {
+            "uri": "http://www.geonames.org/3162212/baerum.html",
+            "prefLabel": {
+              "nb": "Bærum"
+            }
+          },
+          {
+            "uri": "http://www.geonames.org/3151404/hurum.html",
+            "prefLabel": {
+              "nb": "Hurum"
+            }
+          },
+          {
+            "uri": "http://www.geonames.org/3141104/royken.html",
+            "prefLabel": {
+              "nb": "Røyken"
+            }
+          }
+        ],
         "accessRights": {
           "uri": "http://publications.europa.eu/resource/authority/access-right/RESTRICTED",
-          "authorityCode": "RESTRICTED",
+          "code": "RESTRICTED",
           "prefLabel": {
-            "nb": "Begrenset",
-            "nn": "Begrenset",
-            "en": "Restricted"
+            "nb": "Begrenset"
           }
         },
-        "accessRightsComment": [
-          "https://lovdata.no/NL/lov/2016-05-27-14/§3-1"
+        "legalBasisForRestriction": [
+          {
+            "uri": "https://lovdata.no/dokument/NL/lov/1992-12-04-126",
+            "prefLabel": {
+              "nb": "Lov om arkiv [arkivlova]"
+            }
+          },
+          {
+            "uri": "http://lovdata/paragraph/20",
+            "prefLabel": {
+              "nb": "Den spesifikke loven § 20"
+            }
+          },
+          {
+            "uri": "http://lovdata/paragraph/26",
+            "prefLabel": {
+              "nb": "Den mindre spesifikke loven § 26"
+            }
+          }
         ],
-        "references": [],
+        "legalBasisForProcessing": [
+          {
+            "uri": "http://lovdata/paragraph/2",
+            "prefLabel": {
+              "nb": "Den andre loven med lenger tittel § 2"
+            }
+          }
+        ],
+        "legalBasisForAccess": [
+          {
+            "uri": "http://lovdata/paragraph/10",
+            "prefLabel": {
+              "nb": "Den siste loven med den lengste tittelen § 10"
+            }
+          }
+        ],
+        "hasAccuracyAnnotation": {
+          "inDimension": "Accuracy",
+          "hasBody": {
+            "no": "Denne teksten sier noe om nøyaktigheten. Cras mattis consectetur purus sit."
+          }
+        },
+        "hasCompletenessAnnotation": {
+          "inDimension": "Completeness",
+          "hasBody": {
+            "no": "Denne teksten sier noe om komplettheten. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum."
+          }
+        },
+        "hasCurrentnessAnnotation": {
+          "inDimension": "Currentness",
+          "hasBody": {
+            "no": "Denne teksten sier noe om aktualiteten. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+          }
+        },
+        "hasAvailabilityAnnotations": {
+          "inDimension": "Availability",
+          "hasBody": {
+            "no": "Denne teksten sier noe om tilgjengeligheten. Vestibulum id ligula porta felis euismod semper. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum."
+          }
+        },
+        "hasRelevanceAnnotation": {
+          "inDimension": "Relevance",
+          "hasBody": {
+            "no": "Denne teksten sier noe om relevansen. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cum sociis natoque penatibus et magnis dis parturient montes."
+          }
+        },
+        "references": [
+          {
+            "referenceType": {
+              "uri": "http://www.w3.org/2002/07/hasVersion",
+              "code": "hasVersion",
+              "prefLabel": {
+                "nb": "Har versjon"
+              }
+            },
+            "source": {
+              "uri": "http://referenced/dataset",
+              "title": {
+                "nb": "The first one"
+              },
+              "description": {},
+              "contactPoint": [],
+              "legalBasisForRestriction": [],
+              "legalBasisForProcessing": [],
+              "legalBasisForAccess": [],
+              "registrationStatus": "DRAFT"
+            }
+          }
+        ],
+        "provenance": {
+          "uri": "http://data.brreg.no/datakatalog/provenance/vedtak",
+          "code": "vedtak",
+          "prefLabel": {
+            "nb": "Vedtak"
+          }
+        },
         "identifier": [
-          "63"
+          "42"
         ],
-        "page": [],
+        "page": [
+          "http://uri1"
+        ],
         "accrualPeriodicity": {
-          "uri": "http://publications.europa.eu/resource/authority/frequency/CONT",
-          "authorityCode": "CONT",
+          "uri": "http://publications.europa.eu/resource/authority/frequency/ANNUAL",
+          "code": "ANNUAL",
           "prefLabel": {
-            "lt": "nenutrūkstamas",
-            "ga": "leanúnach",
-            "it": "continuo",
-            "es": "continuo",
-            "hu": "folyamatos",
-            "sk": "priebežný",
-            "bg": "постоянен",
-            "fi": "jatkuva",
-            "da": "kontinuerligt",
-            "de": "kontinuierlich",
-            "pl": "ciągły",
-            "lv": "pastāvīgi",
-            "cs": "průběžný",
-            "mt": "kontinwu",
-            "nl": "voortdurend",
-            "fr": "continuel",
-            "ro": "continuu",
-            "en": "continuous",
-            "hr": "stalan",
-            "sl": "nenehen",
-            "el": "συνεχής",
-            "no": "kontinuerlig",
-            "sv": "kontinuerlig",
-            "et": "pidev",
-            "pt": "contínuo"
+            "nb": "årlig"
           }
         },
-        "subject": [],
-        "ADMSIdentifier": []
+        "subject": [
+          {
+            "uri": "https://data-david.github.io/Begrep/begrep/Enhet",
+            "prefLabel": {
+              "no": "enhet"
+            },
+            "definition": {
+              "no": "alt som er registrert med et organisasjonsnummer "
+            },
+            "note": {
+              "no": "Alle hovedenheter, underenheter og organisasjonsledd som er identifisert med et organisasjonsnummer."
+            },
+            "source": "https://jira.brreg.no/browse/BEGREP-208"
+          }
+        ],
+        "informationModel": [
+          {
+            "uri": "https://www.w3.org/2004/02/skos/",
+            "prefLabel": {
+              "nb": "SKOS"
+            }
+          }
+        ],
+        "type": "Kodeliste",
+        "admsIdentifier": [
+          "http://adms.identifier.no/scheme/42"
+        ],
+        "registrationStatus": "DRAFT"
       }
     }
 
@@ -218,7 +340,6 @@ describe('SearchHitItem', () => {
   it ('should render sample', () => {
     expect(wrapper.find('#search-hit-sample')).to.have.length(1);
   });
-
 
 
   /*
