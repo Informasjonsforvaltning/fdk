a3d96f6a9699ba3a69f055dfb9ea87040ccde236
Frédéric Heitmann-André <frederic.andre@gmail.com>
2017-11-03 15:49:47 +0100

    hitstats now works find with back/forward buttons, language change, and more. Attempted to fix textflash

--- applications/search/src/components/search-result-custom-hitstats/index.jsx
+++ applications/search/src/components/search-result-custom-hitstats/index.jsx
@@ -2,19 +2,41 @@ import React from 'react'; // const React = require('react');
 import qs from 'qs'; // const qs = require('qs');
 
 import localization from '../localization';
-
+let state = {}
+let lastupdated = 0;
 const CustomHitsStats = (props) => {
   const { hitsCount, timeTaken } = props;
   const queryObj = qs.parse(window.location.search.substr(1));
-  let requestCompleted = true;
-  if(timeTaken === window.timeTaken) {
-    requestCompleted =  false;
-  }
-  window.timeTaken = timeTaken;
+  const language = localization.getLanguage();
+  const locationHref = window.location.href;
+  const initialCountSummaryShown = requestCompleted && hitsCountInt;
+  const hitsCountHasDecreased = state.hitsCount > hitsCount;
+
   const filteringOrTextSearchPerformed = queryObj ? Object.keys(queryObj).length > 1 || (Object.keys(queryObj).length === 1 && !queryObj.lang) : false;
   const hitsCountInt = hitsCount ? parseInt(hitsCount, 10) : 0;
+  let previousState = '';
+  let requestCompleted = true;
 
-  if (requestCompleted && filteringOrTextSearchPerformed) { // it's a search
+  if(timeTaken === state.timeTaken && language === state.language && locationHref === state.locationHref) {
+    requestCompleted =  false;
+  }
+  if(state.initialCountSummaryShown) {
+    previousState = 'initialCountSummaryShown';
+  } else {
+    previousState = 'searchSummaryShown';
+  }
+  state.language = language;
+  state.timeTaken = timeTaken;
+  state.locationHref = locationHref;
+  state.initialCountSummaryShown = initialCountSummaryShown;
+  state.hitsCount = hitsCount;
+
+  if (
+      requestCompleted &&
+      filteringOrTextSearchPerformed &&
+      previousState !== 'initialCountSummaryShown'
+    ) { // it's a search
+    lastupdated = Date.now();
     return (
       <div className="sk-hits-stats" data-qa="hits-stats">
         <div className="sk-hits-stats__info" data-qa="info">
@@ -24,6 +46,7 @@ const CustomHitsStats = (props) => {
     );
   } else if (requestCompleted && hitsCountInt) {
     return (
+      lastupdated = Date.now();
       <div className="sk-hits-stats" data-qa="hits-stats">
         <div className="sk-hits-stats__info nosearch" data-qa="info">
           <span>{localization.page['nosearch.summary']}</span> <span>{hitsCount}</span> {localization.page['nosearch.descriptions']}
@@ -33,5 +56,7 @@ const CustomHitsStats = (props) => {
   }
   return null;
 };
+/*
+*/
 
 export default CustomHitsStats;
