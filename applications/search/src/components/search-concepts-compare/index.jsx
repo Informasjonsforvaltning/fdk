82068cf3db5e15b3873d870df31fa49923a6c02a
joacimjakobsen <joacim.jakobsen@evry.com>
2017-11-16 08:54:04 +0100

    Added 'x' icon and changed english text for remove buttons in compare terms list.

--- applications/search/src/components/search-concepts-compare/index.jsx
+++ applications/search/src/components/search-concepts-compare/index.jsx
@@ -7,7 +7,11 @@ const CompareTerms = (props) => {
   const { prefLabel, publisher, onDeleteTerm, termIndex, selectedLanguageCode} = props;
   return (
     <div className="fdk-container p-3">
-      <button className="fdk-button fdk-button-cta pull-right" onClick={() => { onDeleteTerm(termIndex)}}>{localization.terms.removeTerms}</button>
+      <button className="fdk-button fdk-button-cta pull-right" onClick={() => { onDeleteTerm(termIndex)}}>
+        <i className="fa fa-times"/>
+        &nbsp;
+        {localization.terms.removeTerms}
+      </button>
       <h4>{getTranslateText(prefLabel, selectedLanguageCode)}</h4>
       <p>{publisher}</p>
     </div>
