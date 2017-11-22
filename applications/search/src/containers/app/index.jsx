5578f7445a1a06463dd6f5e1e9d5404534e14691
Øyvind Isaksen <oyvind.isaksen@acando.no>
2017-11-22 10:40:08 +0100

    styling menu

--- applications/search/src/containers/app/index.jsx
+++ applications/search/src/containers/app/index.jsx
@@ -106,22 +106,19 @@ export default class App extends React.Component {
               </div>
 
               <div className="col-sm-12 col-md-4 fdk-header-flex">
-                <div className="fdk-header-paddingx">
-                    <DropdownButton
-                      tabIndex="0"
-                      id="search-language-dropdown-1"
-                      bsStyle="default"
-                      className="fdk-button-language"
-                      title={localization.lang.chosenLanguage}
-                      onSelect={this.onChangeLanguage}
-                    >
-                      <MenuItem key="1" eventKey="nb">{localization.lang['norwegian-nb']}</MenuItem>
-                      <MenuItem key="2" eventKey="nn">{localization.lang['norwegian-nn']}</MenuItem>
-                      <MenuItem key="3" eventKey="en">{localization.lang['english-en']}</MenuItem>
-                    </DropdownButton>
-                </div>
+                  <DropdownButton
+                    tabIndex="0"
+                    id="search-language-dropdown-1"
+                    bsStyle="default"
+                    className="fdk-button-language"
+                    title={localization.lang.chosenLanguage}
+                    onSelect={this.onChangeLanguage}
+                  >
+                    <MenuItem key="1" eventKey="nb">{localization.lang['norwegian-nb']}</MenuItem>
+                    <MenuItem key="2" eventKey="nn">{localization.lang['norwegian-nn']}</MenuItem>
+                    <MenuItem key="3" eventKey="en">{localization.lang['english-en']}</MenuItem>
+                  </DropdownButton>
 
-                <div className="fdk-float-rightx">
                   <DropdownButton
                     tabIndex="0"
                     id="search-menu-dropdown-1"
@@ -144,7 +141,6 @@ export default class App extends React.Component {
                       {localization.menu.aboutRegistration}
                     </MenuItem>
                   </DropdownButton>
-                </div>
               </div>
             </div>
           </div>
