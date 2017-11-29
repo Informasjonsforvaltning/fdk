788c604ef1e76f65229cde9a2d57a4c8bb555159
joacimjakobsen <joacim.jakobsen@evry.com>
2017-11-29 12:18:05 +0100

    Transform search hits to preferred data structure.

--- applications/search-api/src/main/java/no/dcat/portal/query/PublisherQueryService.java
+++ applications/search-api/src/main/java/no/dcat/portal/query/PublisherQueryService.java
@@ -1,9 +1,11 @@
 package no.dcat.portal.query;
 
+import lombok.Data;
 import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -12,6 +14,10 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class PublisherQueryService extends ElasticsearchService {
@@ -59,38 +65,46 @@ public class PublisherQueryService extends ElasticsearchService {
      * Finds all publisher loaded into elasticsearch.
      * <p/>
      *
-     * @return The complete elasticsearch response on Json-format is returned..
+     * @return orgPath and name of all publisher as Json-format is returned..
      */
     @CrossOrigin
     @RequestMapping(value = QUERY_PUBLISHER_NAMES, produces = "application/json")
-    public ResponseEntity<List<Hit>> publisherNames(@RequestParam(value = "q", defaultValue = "") String query) {
-        logger.info("/publisher query: {}", query);
-
-        ResponseEntity<String> jsonError = initializeElasticsearchTransportClient();
-        if (jsonError != null) return jsonError;
-
+    public ResponseEntity<PublisherHits> publisherNames(@RequestParam(value = "q", defaultValue = "") String query) {
+        logger.info("/publisher-names query: {}", query);
         QueryBuilder search;
 
+        initializeElasticsearchTransportClient();
         search = QueryBuilders.matchAllQuery();
-/*
-        String[] includeFields = new String[] {"orgPath", "name"};
-        String[] excludeFields = new String[] {"_type"};
-        sourceBuilder.fetchSource(includeFields, excludeFields);*/
         SearchRequestBuilder searchQuery = getClient().prepareSearch(INDEX_DCAT).setTypes(TYPE_DATA_PUBLISHER).setQuery(search).addFields("orgPath", "name");
 
-        SearchResponse responsePublisher = searchQuery.setSize(totNrOfPublisher).execute().actionGet();
+        SearchResponse responsePublisher = searchQuery.execute().actionGet();
         logger.debug("Found publisher: {}", responsePublisher);
 
-        ArrayList<Hit> hits = new ArrayList<Hit>();
-        responsePublisher.getHits().forEach(hit -> hits.add( new Hit(hit.fields.orgPath, hit.fields.name)));
-
-        return new ResponseEntity<List<Hit>>(hits, HttpStatus.OK);
+        ArrayList<PublisherHit> publisherHitList = new ArrayList<PublisherHit>();
+        SearchHit[] responseHits = responsePublisher.getHits().getHits();
+        for (SearchHit searchHit : responseHits) {
+            publisherHitList.add(
+                new PublisherHit(searchHit.field("orgPath").getValue().toString(), searchHit.field("name").getValue().toString())
+            );
+        }
+        PublisherHits publisherHits = new PublisherHits(publisherHitList);
+        return new ResponseEntity<PublisherHits>(publisherHits, HttpStatus.OK);
     }
 
-    private class Hit {
+    @Data
+    public class PublisherHits {
+        ArrayList<PublisherHit> hits;
+
+        public PublisherHits(ArrayList<PublisherHit> hits) {
+            this.hits = hits;
+        }
+    }
+
+    @Data
+    public class PublisherHit {
       String orgPath, name;
 
-      public Hit(String orgPath, String name) {
+      public PublisherHit(String orgPath, String name) {
         this.orgPath = orgPath;
         this.name = name;
       }
