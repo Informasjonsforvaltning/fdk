3547ef792bd8eb0188624f187ebb6a45d83898fc
Øyvind Isaksen <oyvind.isaksen@acando.no>
2017-12-12 12:15:30 +0100

    add environment production and optimize build

--- applications/search/webpack.prod.config.js
+++ applications/search/webpack.prod.config.js
@@ -1,38 +1,52 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   context:path.join(__dirname),
   entry: [
+    'babel-polyfill',
     './src/index.jsx'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/static'
+    publicPath: ''
+  },
+  resolve: {
+    extensions:['.js', '.jsx', '.webpack.js', '.web.js']
   },
   plugins: [
-    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    // new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurrenceOrderPlugin(true),
-    new ExtractTextPlugin("styles.css"),
-    new webpack.optimize.UglifyJsPlugin({
-      mangle: true,
-      warnings: false,
-      output: {
-        comments: false
-      },
-      compress: {
-        warnings: false
-      }
-    }),
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify('production')
+        NODE_ENV: JSON.stringify('production')
       }
     }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true
+      },
+      output: {
+        comments: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new webpack.optimize.OccurrenceOrderPlugin(true),
+    new ExtractTextPlugin("styles.css"),
     new CopyWebpackPlugin([
       { from: './src/assets/css/bootstrap*', to: './', flatten: true },
       { from: './src/assets/img/*', to: './img', flatten: true }
@@ -40,41 +54,29 @@ module.exports = {
       copyUnmodified: true
     })
   ],
-  resolve: {
-    alias: {
-      react: path.resolve('./node_modules/react')
-    },
-    extensions:['.js', '.jsx', '.webpack.js', '.web.js']
-  },
-  resolveLoader: {
-    // root: path.join(__dirname, "node_modules")
-    modules: [__dirname, 'node_modules']
-  },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.jsx?$/,
         exclude: /(node_modules)/,
         loader: 'babel-loader',
         query: {
-
           presets: ['env', 'react']
-
         }
       },
       {
         test: /\.scss$/,
-        // loader: ExtractTextPlugin.extract('css!sass')
         use: ExtractTextPlugin.extract({
-
           fallback: 'style-loader',
-
           use: ['css-loader', 'sass-loader']
-
         })
       },
-      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
-      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader"
+      },
       {
         test: /\.(png|jpg)$/,
         use: [{
