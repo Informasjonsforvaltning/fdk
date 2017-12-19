e01568d40e6e2bd0400047d6a7c5f475f741dc96
Bjørn Grøva <bjorn.grova@brreg.no>
2017-12-19 14:05:38 +0100

    chenged deployment order to ST2->ST1

--- deploy.sh
+++ deploy.sh
@@ -118,13 +118,13 @@ if [ "$environment" == "st1" ] ; then
   if [ "$component" == "all" ] ; then
     for i in $components
     do
-        dockerTag ${i} tt1 st1
+        dockerTag ${i} st2 st1
     done
   else
-    dockerTag ${component} tt1 st1
+    dockerTag ${component} st2 st1
   fi
 
-  gitTag ${component} tt1 st1
+  gitTag ${component} st1 st1
 
 
 elif [ "$environment" == "st2" ] ; then
