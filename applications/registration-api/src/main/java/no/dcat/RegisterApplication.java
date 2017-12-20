1a17c9d21fafa98653f108b570974558e9d85911
joacimjakobsen <joacim.jakobsen@evry.com>
2017-12-20 16:15:26 +0100

    Generate swagger api-docs for search-api, reference-data, harvester-api and registration-api. Access by url /api-docs/search-api, /api-docs/reference-data, /api-docs/harvester-api and /api-docs/egistration-api Also routed through nginx-search so that port 8080 works for these url. THis is necessary to get headers with CORS enabled for the api docs.
    Missing bypass of login for registration-api.

--- applications/registration-api/src/main/java/no/dcat/RegisterApplication.java
+++ applications/registration-api/src/main/java/no/dcat/RegisterApplication.java
@@ -1,19 +1,39 @@
 package no.dcat;
 
 import com.github.ulisesbocchio.spring.boot.security.saml.annotation.EnableSAMLSSO;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
 @EnableElasticsearchRepositories
+@PropertySource("classpath:swagger.properties")
+@EnableSwagger2
 public class RegisterApplication {
+    @Value("${springfox.documentation.swagger.v2.path}")
+    private String swagger2Endpoint;
 
 
     public static void main(String... args) {
         SpringApplication.run(RegisterApplication.class, args);
     }
 
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
 
