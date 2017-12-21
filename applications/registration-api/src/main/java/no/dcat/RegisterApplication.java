5dbdb56c4c0eb12eca7e4a0eafe944889256d949
joacimjakobsen <joacim.jakobsen@evry.com>
2017-12-21 12:57:11 +0100

    Removed swagger parts from registration-api for now.

--- applications/registration-api/src/main/java/no/dcat/RegisterApplication.java
+++ applications/registration-api/src/main/java/no/dcat/RegisterApplication.java
@@ -16,24 +16,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableElasticsearchRepositories
-@PropertySource("classpath:swagger.properties")
-@EnableSwagger2
 public class RegisterApplication {
-    @Value("${springfox.documentation.swagger.v2.path}")
-    private String swagger2Endpoint;
-
-
     public static void main(String... args) {
         SpringApplication.run(RegisterApplication.class, args);
-    }
-
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
     }
 }
 
