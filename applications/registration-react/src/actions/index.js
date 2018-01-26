a214034ca19a55a898559794cdd694258432d162
Øyvind Isaksen <oyvind.isaksen@acando.no>
2018-01-26 12:47:47 +0100

    add fetch url

--- applications/registration-react/src/actions/index.js
+++ applications/registration-react/src/actions/index.js
@@ -19,7 +19,7 @@ export function fetchDatasetIfNeeded(datasetURL) {
   return (dispatch, getState) =>
     shouldFetchApi(
       getState().dataset) && dispatch(
-      fetchApi('/catalogs/910244132//datasets/e679b150-e69d-444c-bf7f-874d6999c62d', [actions.DATASET_REQUEST, actions.DATASET_SUCCESS, actions.DATASET_FAILURE])
+      fetchApi(datasetURL, [actions.DATASET_REQUEST, actions.DATASET_SUCCESS, actions.DATASET_FAILURE])
     );
 }
 
