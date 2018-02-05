46144100e609de1d9a1227a686389473759665f0
Øyvind Isaksen <oyvind.isaksen@acando.no>
2018-02-05 15:22:56 +0100

    add route

--- applications/registration-react/src/index.jsx
+++ applications/registration-react/src/index.jsx
@@ -16,7 +16,7 @@ const routes =
     <Switch>
       <Route exact path="/" component={App} />
       <Route path="/react" component={RegDataset} />
-      <Route path="/react/catalogs/(:catalogId)/datasets/(:id)" component={RegDataset} />
+      <Route path="/catalogs/(:catalogId)/datasets/(:id)" component={RegDataset} />
     </Switch>
   );
 
