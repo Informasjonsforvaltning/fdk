004cecbf912a6a489376aa33c261d69f57aeacc6
Bjørn Grøva <bjorn.grova@brreg.no>
2018-02-14 08:56:16 +0100

    created framework for contents schema

---
+++ applications/registration-react/src/components/reg-form-schema-contents/index.jsx
@@ -0,0 +1,120 @@
+import React from 'react';
+import { Field, FieldArray, reduxForm, formValueSelector } from 'redux-form';
+import { connect } from 'react-redux';
+
+import localization from '../../utils/localization';
+import Helptext from '../reg-form-helptext';
+import InputField from '../reg-form-field-input';
+import asyncValidate from '../../utils/asyncValidate';
+import { informationModelType } from '../../schemaTypes';
+
+// skal disse være med her - de validerer jo felt et annet sted?
+const validate = values => {
+  const errors = {}
+  const title = (values.title && values.title.nb) ? values.title.nb : null;
+  const description = (values.description && values.description.nb) ? values.description.nb : null;
+  const objective = (values.objective && values.objective.nb) ? values.objective.nb : null;
+  const landingPage = (values.landingPage && values.landingPage[0]) ? values.landingPage[0] : null;
+  if (!title) {
+    errors.title = {nb: localization.validation.required}
+  } else if (title.length < 2) {
+    errors.title = {nb: localization.validation.minTwoChars}
+  }
+  if (description && description.length < 2) {
+    errors.description = {nb: localization.validation.minTwoChars}
+  }
+  if (objective && objective.length < 2) {
+    errors.objective = {nb: localization.validation.minTwoChars}
+  }
+  if (landingPage && landingPage.length < 2) {
+    errors.landingPage = [localization.validation.minTwoChars]
+  }
+  /*
+   else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+   errors.email = 'Invalid email address'
+   }
+   */
+  return errors
+}
+
+const renderStandardFields = (item, index, fields, props) => (
+  <div className="d-flex mb-5" key={index}>
+    <div className="w-50">
+      <Field
+        name={`${item}.prefLabel.nb`}
+        component={InputField}
+        label={props.titleLabel}
+        showLabel
+      />
+    </div>
+    <div className="w-50">
+      <Field
+        name={`${item}.uri`}
+        component={InputField}
+        label={props.linkLabel}
+        showLabel
+      />
+    </div>
+  </div>
+);
+
+const renderStandard = (props) => {
+  const { fields } = props;
+  return (
+    <div>
+      {fields && fields.map((item, index) =>
+        renderStandard(item, index, fields, props)
+      )}
+    </div>
+  );
+};
+
+let FormContents = (props) => {
+  const { helptextItems, hasInformationModelUri } = props;
+  return (
+    <form>
+      <div className="form-group">
+        {
+          <div className="mt-4">
+            <div className="form-group">
+              <Helptext title="Standard" helptextItems={helptextItems.Dataset_informationModel} />
+              <FieldArray
+                name="informationModel"
+                component={renderStandard}
+                titleLabel={localization.schema.conformsTo.titleLabel}
+                linkLabel={localization.schema.conformsTo.linkLabel}
+                // titleLabel="title label"
+                // linkLabel="link label"
+              />
+            </div>
+          </div>
+        }
+      </div>
+    </form>
+  )
+}
+
+FormContents = reduxForm({
+  form: 'contents',
+  validate,
+  asyncValidate,
+})(FormContents)
+
+// Decorate with connect to read form values
+const selector = formValueSelector('contents')
+FormContents = connect(state => {
+  const hasInformationModelUri = selector(state, 'informationModel.uri') //todo: endre
+  return {
+    hasInformationModelUri,
+  }
+})(FormContents)
+
+const mapStateToProps = ({ dataset }) => (
+  {
+    initialValues: {
+      informationModel: (dataset.result.informationModel && dataset.result.informationModel.length > 0) ? dataset.result.informationModel : [informationModelType],
+    }
+  }
+)
+
+export default connect(mapStateToProps)(FormContents)
