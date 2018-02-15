13fd04aa7614f62abf6b00ddc5ceceb4a958a7ca
Øyvind Isaksen <oyvind.isaksen@acando.no>
2018-02-15 09:49:31 +0100

    ie test2

--- applications/registration-react/src/components/reg-form-field-input-tags-objects/index.jsx
+++ applications/registration-react/src/components/reg-form-field-input-tags-objects/index.jsx
@@ -4,10 +4,15 @@ import TagsInput from 'react-tagsinput';
 import '../reg-form-field-input-tags/index.scss';
 
 const handleChange = (props, tags) => {
+  console.log("InputTagsFieldArray1");
   const { fieldLabel } = props;
+  console.log("InputTagsFieldArray2");
   let updates = [];
+  console.log("InputTagsFieldArray3");
   updates = tags.map((item) => ({[fieldLabel]:item}))
+  console.log("InputTagsFieldArray4");
   props.input.onChange(updates);
+  console.log("InputTagsFieldArray5");
 }
 
 const InputTagsFieldArray  = (props) => {
