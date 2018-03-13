a93b9c2466b44c7501827bcebd8c0dbcceec710e
Bjørn Grøva <bjorn.grova@brreg.no>
2018-03-13 15:43:31 +0100

    FDK-1089 harvester admin username/password as parameters

--- runCreateServiceInOpenshift.sh
+++ runCreateServiceInOpenshift.sh
@@ -362,6 +362,11 @@ then
     then
         profile=prod
         createOpenshiftService harvester
+
+        oc env dc/harvester \
+            harvester_adminUsername=changeme \
+            harvester_adminPassword=changeme
+
         exposeService harvester
     else
         # deploymentmode = onlyDeployImages
