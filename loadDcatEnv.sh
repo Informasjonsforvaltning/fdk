eb9969d0d8297fa3f1ec4268e6e46d8e1c9e0739
David Skogan <david.skogan@gmail.com>
2018-03-27 10:06:14 +0200

    Fixed subjectCount in dataset query, and added ppe handling to scripts

--- loadDcatEnv.sh
+++ loadDcatEnv.sh
@@ -5,6 +5,10 @@ function startLoad {
     echo "Start loading ..."
     environment=$1
     targetElasticUrl=http://elasticsearch-fellesdatakatalog-${environment}.ose-npc.brreg.no
+    if [ "$environment" == "ppe" ]
+    then
+        targetElasticUrl=http://elasticsearch-fellesdatakatalog-${environment}.ose-pc.brreg.no
+    fi
 
     DATETIME=`date "+%Y-%m-%dT%H_%M_%S"`
 
