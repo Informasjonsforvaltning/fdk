14f9e50fb15fdf090053df8aad3f868b6882831b
David Skogan <david.skogan@gmail.com>
2018-04-26 13:11:02 +0200

    Removed unused imports

--- applications/harvester-api/src/main/java/no/dcat/harvester/crawler/web/CrawlerStartHarvest.java
+++ applications/harvester-api/src/main/java/no/dcat/harvester/crawler/web/CrawlerStartHarvest.java
@@ -7,7 +7,6 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 /**
