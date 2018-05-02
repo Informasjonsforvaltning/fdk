89b5063e07310d71c23b238ed87e4a60a7cc12b3
Bjørn Grøva <bjorn.grova@brreg.no>
2018-05-02 14:33:03 +0200

    FDK-1083 more no.dcat.factory tests

--- applications/registration-api/src/test/java/no/dcat/factory/RegistrationFactoryTest.java
+++ applications/registration-api/src/test/java/no/dcat/factory/RegistrationFactoryTest.java
@@ -6,6 +6,7 @@ import no.dcat.shared.Distribution;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 /**
  * Created by bjg on 02.05.2018.
@@ -37,5 +38,20 @@ public class RegistrationFactoryTest {
 
         assertThat("distribution uri is correct", result.getUri(), containsString("http://brreg.no/catalogs/" + catalogId
                 + "/datasets/" + datasetId + "/distributions/"));
+    }
+
+    @Test
+    public void getContactUriIsCorrect() throws Exception {
+        String result = RegistrationFactory.getContactUri("111", "222");
+        assertThat(result, is("http://brreg.no/catalogs/111/contacts/222"));
+    }
+
+    @Test
+    public void getDistributionUriIsCorrect() throws Exception {
+        Dataset dataset = RegistrationFactory.createDataset("111");
+        Distribution distribution = RegistrationFactory.createDistribution("111", "222");
+        String result = RegistrationFactory.getDistributionUri(distribution, dataset);
+        assertThat(result, is("http://brreg.no/catalogs/111/datasets/"
+                + dataset.getId() +"/distributions/" + distribution.getId()));
     }
 }
