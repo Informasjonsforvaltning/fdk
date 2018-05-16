8d2469bf8649b6afb44a8d854a24759f6ad5b27c
Øyvind Isaksen <oyvind.isaksen@acando.no>
2018-05-16 14:36:26 +0200

    refactor schemas

--- applications/registration-react/src/components/reg-form-schema-accessRights/index.jsx
+++ applications/registration-react/src/components/reg-form-schema-accessRights/index.jsx
@@ -1,240 +1,11 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Field, FieldArray, reduxForm, formValueSelector, getFormSyncErrors } from 'redux-form';
+import { reduxForm, formValueSelector, getFormSyncErrors } from 'redux-form';
 import { connect } from 'react-redux';
 import _throttle from 'lodash/throttle';
 
-import localization from '../../utils/localization';
-import Helptext from '../reg-form-helptext';
-import InputField from '../reg-form-field-input';
-import RadioField from '../reg-form-field-radio';
+import Form from './form';
+import validate from './formValidations';
 import asyncValidate from '../../utils/asyncValidate';
 import { accessRights, legalBasisType } from '../../schemaTypes';
-import { validateRequired, validateMinTwoChars, validateURL} from '../../validation/validation';
-
-const validate = values => {
-  let errors = {}
-  const accessRight = (values.accessRights && values.accessRights.uri) ? values.accessRights.uri : null;
-  const { legalBasisForRestriction, legalBasisForProcessing, legalBasisForAccess } = values;
-  let legalBasisForRestrictionNodes = null;
-  let legalBasisForProcessingNodes = null;
-  let legalBasisForAccessNodes = null;
-
-  errors = validateRequired('accessRight', accessRight, errors);
-
-  if (legalBasisForRestriction) {
-    legalBasisForRestrictionNodes = legalBasisForRestriction.map(item => {
-      let itemErrors = {};
-      const legalBasisForRestrictionPrefLabel = (item.prefLabel && item.prefLabel.nb) ? item.prefLabel.nb : null;
-      const legalBasisForRestrictionURI = item.uri ? item.uri : null;
-
-      itemErrors = validateMinTwoChars('prefLabel', legalBasisForRestrictionPrefLabel, itemErrors);
-      itemErrors = validateURL('uri', legalBasisForRestrictionURI, itemErrors);
-
-      return itemErrors;
-    });
-    let showSyncError = false;
-    showSyncError = (legalBasisForRestrictionNodes.filter(item => (item && JSON.stringify(item) !== '{}')).length > 0);
-    if (showSyncError) {
-      errors.legalBasisForRestriction = legalBasisForRestrictionNodes;
-    }
-  }
-
-  if (legalBasisForProcessing) {
-    legalBasisForProcessingNodes = legalBasisForProcessing.map(item => {
-      let itemErrors = {}
-      const legalBasisForProcessingPrefLabel = (item.prefLabel && item.prefLabel.nb) ? item.prefLabel.nb : null;
-      const legalBasisForProcessingURI = item.uri ? item.uri : null;
-
-      itemErrors = validateMinTwoChars('prefLabel', legalBasisForProcessingPrefLabel, itemErrors);
-      itemErrors = validateURL('uri', legalBasisForProcessingURI, itemErrors);
-
-      return itemErrors;
-    });
-    let showSyncError = false;
-    showSyncError = (legalBasisForProcessingNodes.filter(item => (item && JSON.stringify(item) !== '{}')).length > 0);
-    if (showSyncError) {
-      errors.legalBasisForProcessing = legalBasisForProcessingNodes;
-    }
-  }
-
-  if (legalBasisForAccess) {
-    legalBasisForAccessNodes = legalBasisForAccess.map(item => {
-      let itemErrors = {}
-      const legalBasisForAccessPrefLabel = (item.prefLabel && item.prefLabel.nb) ? item.prefLabel.nb : null;
-      const legalBasisForAccessURI = item.uri ? item.uri : null;
-
-      itemErrors = validateMinTwoChars('prefLabel', legalBasisForAccessPrefLabel, itemErrors);
-      itemErrors = validateURL('uri', legalBasisForAccessURI, itemErrors);
-
-      return itemErrors;
-    });
-    let showSyncError = false;
-    showSyncError = (legalBasisForAccessNodes.filter(item => (item && JSON.stringify(item) !== '{}')).length > 0);
-    if (showSyncError) {
-      errors.legalBasisForAccess = legalBasisForAccessNodes;
-    }
-  }
-
-  return errors
-}
-
-/*
- Resets fields when radio button "Offentlig" is chosen.
- */
-const resetFields = (props) => {
-  props.change('legalBasisForRestriction', [legalBasisType]);
-  props.change('legalBasisForProcessing', [legalBasisType]);
-  props.change('legalBasisForAccess', [legalBasisType]);
-}
-
-const renderLegalBasisFields = (item, index, fields, customProps) => (
-  <div className="d-flex mb-2" key={index}>
-    <div className="w-50">
-      <Field
-        name={`${item}.prefLabel.nb`}
-        component={InputField}
-        label={customProps.titleLabel}
-        showLabel
-      />
-    </div>
-    <div className="w-50">
-      <Field
-        name={`${item}.uri`}
-        component={InputField}
-        label={customProps.linkLabel}
-        showLabel
-      />
-    </div>
-    <div className="d-flex align-items-end">
-      <button
-        className="fdk-btn-no-border"
-        type="button"
-        title="Remove temporal"
-        onClick={
-          () => {
-            if (fields.length === 1) {
-              fields.remove(index);
-              fields.push({});
-            }
-
-            if (fields.length > 1) {
-              fields.remove(index);
-            }
-            asyncValidate(fields.getAll(), customProps.dispatch, customProps, `remove_${fields.name}_${index}`);
-          }
-        }
-      >
-        <i className="fa fa-trash mr-2" />
-      </button>
-    </div>
-  </div>
-);
-
-export const renderLegalBasis = (customProps) => {
-  const { fields } = customProps;
-  return (
-    <div>
-      {fields && fields.map((item, index) =>
-        renderLegalBasisFields(item, index, fields, customProps)
-      )}
-      <button className="fdk-btn-no-border" type="button" onClick={() => fields.push({})}>
-        <i className="fa fa-plus mr-2" />
-        Legg til
-      </button>
-    </div>
-  );
-};
-
-export const FormAccessRights = (props) => {
-  const { syncErrors, helptextItems, hasAccessRightsURI } = props;
-  const accessRight = syncErrors ? syncErrors.accessRight : null;
-
-  return (
-    <form>
-      <div className="form-group">
-        <Helptext title="Tilgangsnivå" helptextItems={helptextItems.Dataset_distribution} />
-        <Field
-          name="accessRights.uri"
-          radioId="accessRight-public"
-          component={RadioField}
-          type="radio"
-          value="http://publications.europa.eu/resource/authority/access-right/PUBLIC"
-          label="Offentlig"
-          onChange={() => resetFields(props)}
-        />
-        <Field
-          name="accessRights.uri"
-          radioId="accessRight-restricted"
-          component={RadioField}
-          type="radio"
-          value="http://publications.europa.eu/resource/authority/access-right/RESTRICTED"
-          label="Begrenset offentlighet"
-        />
-        <Field
-          name="accessRights.uri"
-          radioId="accessRight-non_public"
-          component={RadioField}
-          type="radio"
-          value="http://publications.europa.eu/resource/authority/access-right/NON_PUBLIC"
-          label="Unntatt offentlighet"
-        />
-
-        {accessRight &&
-        <div className="alert alert-danger mt-3">{accessRight.nb}</div>
-        }
-
-        {
-          (hasAccessRightsURI === 'http://publications.europa.eu/resource/authority/access-right/RESTRICTED' ||
-            hasAccessRightsURI === 'http://publications.europa.eu/resource/authority/access-right/NON_PUBLIC'
-          ) && (
-            <div className="mt-4">
-              <div className="form-group">
-                <Helptext title={localization.schema.accessRights.legalBasisForRestriction.heading} helptextItems={helptextItems.Dataset_legalBasisForRestriction} />
-                <FieldArray
-                  name="legalBasisForRestriction"
-                  component={renderLegalBasis}
-                  titleLabel={localization.schema.accessRights.legalBasisForRestriction.titleLabel}
-                  linkLabel={localization.schema.accessRights.legalBasisForRestriction.linkLabel}
-                />
-              </div>
-
-              <div className="form-group">
-                <Helptext title={localization.schema.accessRights.legalBasisForProcessing.heading} helptextItems={helptextItems.Dataset_legalBasisForProcessing} />
-                <FieldArray
-                  name="legalBasisForProcessing"
-                  component={renderLegalBasis}
-                  titleLabel={localization.schema.accessRights.legalBasisForProcessing.titleLabel}
-                  linkLabel={localization.schema.accessRights.legalBasisForProcessing.linkLabel}
-                />
-              </div>
-
-              <div className="form-group">
-                <Helptext title={localization.schema.accessRights.legalBasisForAccess.heading} helptextItems={helptextItems.Dataset_legalBasisForAccess} />
-                <FieldArray
-                  name="legalBasisForAccess"
-                  component={renderLegalBasis}
-                  titleLabel={localization.schema.accessRights.legalBasisForAccess.titleLabel}
-                  linkLabel={localization.schema.accessRights.legalBasisForAccess.linkLabel}
-                />
-              </div>
-            </div>
-          )}
-      </div>
-    </form>
-  )
-}
-
-FormAccessRights.defaultProps = {
-  syncErrors: null,
-  hasAccessRightsURI: null
-};
-
-FormAccessRights.propTypes = {
-  syncErrors: PropTypes.object,
-  helptextItems: PropTypes.object.isRequired,
-  hasAccessRightsURI: PropTypes.string
-};
 
 const selector = formValueSelector('accessRights');
 
@@ -248,7 +19,7 @@ const FormAccessRightsSchema = reduxForm({
     hasAccessRightsURI,
     syncErrors: getFormSyncErrors("accessRights")(state)
   }
-})(FormAccessRights));
+})(Form));
 
 
 const mapStateToProps = ({ dataset }) => (
@@ -262,4 +33,7 @@ const mapStateToProps = ({ dataset }) => (
   }
 )
 
-export default connect(mapStateToProps)(FormAccessRightsSchema)
+const mergeProps = (stateProps, dispatchProps, ownProps) =>
+  Object.assign({}, stateProps, dispatchProps, ownProps);
+
+export default connect(mapStateToProps, null, mergeProps)(FormAccessRightsSchema)
