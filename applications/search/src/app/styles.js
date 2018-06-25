51deca5dcb508bc894c3e964ad0a8ce69a18da1d
Øyvind Isaksen <oyvind.isaksen@acando.no>
2018-06-25 13:44:22 +0200

    add helper.scss to styles.js

--- applications/search/src/app/styles.js
+++ applications/search/src/app/styles.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'designsystemet/fdk-designsystem-bootstrap3/scss/helper.scss';
 import 'designsystemet/fdk-designsystem-bootstrap3/scss/portal.scss';
 import 'designsystemet/fdk-designsystem-bootstrap3/scss/typo.scss';
 import 'designsystemet/fdk-designsystem-bootstrap3/scss/common.scss';
