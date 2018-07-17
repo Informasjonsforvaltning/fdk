f183438498f4b0a6d95c5a768925990aaa01ae43
Markko Paas <markko.paas@gmail.com>
2018-07-17 10:19:45 +0200

    Move Java unit tests to build stage

--- buildGroupsEnv.sh
+++ buildGroupsEnv.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export BUILD1_APPS="fuseki harvester harvester-api nginx-search nginx-registration reference-data registration-react registration-api registration-auth registration-validator search search-api"
-export BUILD1_CMD="mvn install -DskipTests -Dmaven.javadoc.skip=true"
+export BUILD1_CMD="mvn install -Dmaven.javadoc.skip=true -B"
