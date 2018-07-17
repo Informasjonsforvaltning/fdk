d34cc822b906d7ed7dc4ef69d4902dcacfb7164b
Markko Paas <markko.paas@gmail.com>
2018-07-17 10:33:13 +0200

    Separate search application to its own build group

---
+++ applications/search/travisBuild.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -e
+
+SECONDS=0 # start timer
+
+echo "Install dependencies"
+npm install
+
+echo "SECONDS"
+echo $SECONDS
+
+echo "Run tests"
+npm test
+
+echo "SECONDS"
+echo $SECONDS
+
+if [ "$COVERALLS_REPO_TOKEN" ]
+then
+    echo "Report coverage"
+    npm run coverage
+fi
+
+echo "SECONDS"
+echo $SECONDS
+
+echo "Start build in docker image";
+docker build -t dcatno/search:latest .
+
+echo "SECONDS"
+echo $SECONDS
--- runAll.sh
+++ runAll.sh
@@ -5,6 +5,7 @@ set -e
 
 # We will build what is needed and download images that are missing
 ./buildOrPullApplicationGroup.sh "$BUILD1_APPS" "$BUILD1_CMD"
+./buildOrPullApplicationGroup.sh "$BUILD2_APPS" "$BUILD2_CMD"
 
 docker-compose down --remove-orphans
 
