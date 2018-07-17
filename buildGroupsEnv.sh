437e9fbb1260c94e4f939d26b482dc2ebf1cf14d
Markko Paas <markko.paas@gmail.com>
2018-07-17 10:33:13 +0200

    Separate search application to its own build group

--- buildGroupsEnv.sh
+++ buildGroupsEnv.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-export BUILD1_APPS="fuseki harvester harvester-api nginx-search nginx-registration reference-data registration-react registration-api registration-auth registration-validator search search-api"
+export BUILD1_APPS="fuseki harvester harvester-api nginx-search nginx-registration reference-data registration-react registration-api registration-auth registration-validator search-api"
 export BUILD1_CMD="mvn install -Dmaven.javadoc.skip=true -B"
+
+export BUILD2_APPS="search"
+export BUILD2_CMD="( cd applications/search && ./travisBuild.sh )"
