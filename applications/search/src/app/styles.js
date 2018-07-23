239b5bcc848277616df33edf9cc6939439986e63
Markko Paas <markko.paas@gmail.com>
2018-07-23 08:49:08 +0200

    Install bootstrap4 (and break all layout and styles)

--- applications/search/src/app/styles.js
+++ applications/search/src/app/styles.js
@@ -1,5 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
 import 'designsystemet/fdk-designsystem-bootstrap3/scss/helper.scss';
 import 'designsystemet/fdk-designsystem-bootstrap3/scss/portal.scss';
 import 'designsystemet/fdk-designsystem-bootstrap3/scss/typo.scss';
