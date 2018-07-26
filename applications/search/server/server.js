2c4cc28ff480c6e44f4199738bef997ddcff26a6
Markko Paas <markko.paas@gmail.com>
2018-07-26 20:52:06 +0200

    (registration-react) (search) Remove commented out production switch from search and registration-react Dockerfile because we now can give the parameter through docker-compose

--- applications/search/server/server.js
+++ applications/search/server/server.js
@@ -53,6 +53,7 @@ module.exports = {
 
     app.listen(app.get('port'), () => {
       console.log('FDK-search lytter på', app.get('port')); // eslint-disable-line no-console
+      console.log('env:', env.production ? 'production' : 'development'); // eslint-disable-line no-console
     });
   }
 };
