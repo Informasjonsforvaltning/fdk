9b7a04404030be0054c743b587b7218cdb70f529
Markko Paas <markko.paas@gmail.com>
2018-07-31 13:58:45 +0200

    (search) Convert publishers-select component to function

--- applications/search/src/pages/datasets-report-page/publishers-select/publishers-select.component.jsx
+++ applications/search/src/pages/datasets-report-page/publishers-select/publishers-select.component.jsx
@@ -1,71 +1,47 @@
 import React from 'react';
 import Select from 'react-select';
 import * as axios from 'axios';
-import defaults from 'lodash/defaults';
+import PropTypes from 'prop-types';
 
 import localization from '../../../lib/localization';
 import './publishers-select.scss';
 
-export class PublishersSelect extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      backspaceRemoves: true,
-      multi: false
-    };
-    this.options = defaults({
-      headers: {},
-      searchUrlPath: '/publisher'
-    });
-    this.axios = axios.create({
-      baseURL: this.host,
-      headers: this.options.headers
-    });
-    this.onChange = this.onChange.bind(this);
+function getPublishers(input) {
+  if (!input) {
+    return Promise.resolve({ options: [] });
   }
 
-  onChange(value) {
-    if (!value) {
-      this.props.onSearch(null, '');
-    } else {
-      this.props.onSearch(value.name, value.orgPath);
-    }
-  }
+  return axios.get(`/publisher?q=${input}`).then(response => {
+    const nodes = response.data.hits.hits.map(item => item._source);
 
-  static getPublishers(input) {
-    if (!input) {
-      return Promise.resolve({ options: [] });
-    }
-
-    return axios.get(`/publisher?q=${input}`).then(response => {
-      const nodes = response.data.hits.hits.map(item => item._source);
-      return { options: nodes };
-    });
-  }
-
-  render() {
-    return (
-      <div className="section fdk-report-search-publishers">
-        <div className="fdk-report-search-publishers__header mb-2">
-          {localization.report.searchPublisher}
-        </div>
-        <Select.Async
-          ignoreAccents={false}
-          placeholder={localization.report.searchPublisherPlaceholder}
-          searchPromptText={localization.report.typeToSearch}
-          multi={this.state.multi}
-          value={this.props.value}
-          onChange={this.props.onChange}
-          valueKey="orgPath"
-          labelKey="name"
-          loadOptions={PublishersSelect.getPublishers}
-          backspaceRemoves={this.state.backspaceRemoves}
-        />
-      </div>
-    );
-  }
+    return { options: nodes };
+  });
 }
 
-PublishersSelect.defaultProps = {};
+export function PublishersSelect(props) {
+  return (
+    <div className="section fdk-report-search-publishers">
+      <div className="fdk-report-search-publishers__header mb-2">
+        {localization.report.searchPublisher}
+      </div>
+      <Select.Async
+        ignoreAccents={false}
+        placeholder={localization.report.searchPublisherPlaceholder}
+        searchPromptText={localization.report.typeToSearch}
+        multi={false}
+        value={props.value}
+        onChange={props.onChange}
+        valueKey="orgPath"
+        labelKey="name"
+        loadOptions={getPublishers}
+        backspaceRemoves={true}
+      />
+    </div>
+  );
+}
 
-PublishersSelect.propTypes = {};
+PublishersSelect.propTypes = {
+  onSearch: PropTypes.func,
+  onChange: PropTypes.func,
+  value: PropTypes.string
+};
