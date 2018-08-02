a382988109e6b238802ba09b747cfc0f7a638a16
Markko Paas <markko.paas@gmail.com>
2018-08-02 14:54:02 +0200

    (search) Use publishers list from redux store in datasets reports publisher select

--- applications/search/src/pages/datasets-report-page/publishers-select/publishers-select.component.jsx
+++ applications/search/src/pages/datasets-report-page/publishers-select/publishers-select.component.jsx
@@ -1,39 +1,30 @@
+import _ from 'lodash';
 import React from 'react';
 import Select from 'react-select';
-import * as axios from 'axios';
 import PropTypes from 'prop-types';
 
 import localization from '../../../lib/localization';
 import './publishers-select.scss';
 
-function getPublishers(input) {
-  if (!input) {
-    return Promise.resolve({ options: [] });
-  }
-
-  return axios.get(`/publisher?q=${input}`).then(response => {
-    const nodes = response.data.hits.hits.map(item => item._source);
-
-    return { options: nodes };
-  });
-}
-
 export function PublishersSelect(props) {
+  const options = _.chain(props.publishers)
+    .values()
+    .sortBy('name')
+    .value();
+
   return (
     <div className="section fdk-report-search-publishers">
       <div className="fdk-report-search-publishers__header mb-2">
         {localization.report.searchPublisher}
       </div>
-      <Select.Async
-        ignoreAccents={false}
+      <Select
         placeholder={localization.report.searchPublisherPlaceholder}
         searchPromptText={localization.report.typeToSearch}
-        multi={false}
         value={props.value}
         onChange={props.onChange}
         valueKey="orgPath"
         labelKey="name"
-        loadOptions={getPublishers}
+        options={options}
         backspaceRemoves
       />
     </div>
@@ -46,6 +37,7 @@ PublishersSelect.defaultProps = {
 };
 
 PublishersSelect.propTypes = {
+  publishers: PropTypes.object.isRequired,
   onChange: PropTypes.func,
   value: PropTypes.object
 };
