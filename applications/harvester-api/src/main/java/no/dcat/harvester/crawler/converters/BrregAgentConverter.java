724a240b80bed8b1183c06d71df18775a27aa884
David Skogan <david.skogan@gmail.com>
2018-08-08 12:19:45 +0200

    FDK-6343 Reworked logic to lookup master data about publishers. Publishers now will have a prefLabel from the foaf:name they provided with the catalog publishers. If successfull lookup in masterdata its official name from the business register will be written to foaf:name. Deleted unused classes. Refactored code and added guard to not process same publisher twice.

--- applications/harvester-api/src/main/java/no/dcat/harvester/crawler/converters/BrregAgentConverter.java
+++ applications/harvester-api/src/main/java/no/dcat/harvester/crawler/converters/BrregAgentConverter.java
@@ -5,10 +5,9 @@ import no.acando.xmltordf.Builder;
 import no.acando.xmltordf.PostProcessingJena;
 import no.acando.xmltordf.XmlToRdfAdvancedJena;
 import no.dcat.datastore.domain.dcat.Publisher;
-import no.dcat.datastore.domain.dcat.builders.DcatBuilder;
 import no.dcat.datastore.domain.dcat.builders.PublisherBuilder;
 import no.dcat.datastore.domain.dcat.vocabulary.DCATNO;
-import no.dcat.harvester.theme.builders.vocabulary.EnhetsregisteretRDF;
+import no.dcat.datastore.domain.dcat.vocabulary.EnhetsregisteretRDF;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.apache.jena.rdf.model.Model;
@@ -22,6 +21,7 @@ import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.sparql.vocabulary.FOAF;
 import org.apache.jena.util.ResourceUtils;
 import org.apache.jena.vocabulary.DCTerms;
+import org.apache.jena.vocabulary.RDF;
 import org.apache.jena.vocabulary.SKOS;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,21 +35,24 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class BrregAgentConverter {
 
-    public String publisherIdURI = Publisher.PUBLISHERID_ENHETSREGISTERET_URI;
+    private String publisherIdURI = Publisher.PUBLISHERID_ENHETSREGISTERET_URI;
     private final XmlToRdfAdvancedJena xmlToRdfObject;
     private final LoadingCache<URL, String> brregCache;
     private HashMap<String, String> canonicalNames = new HashMap<>();
+    private Set<String> processedPublishers = new HashSet<>();
 
     private static final Logger logger = LoggerFactory.getLogger(BrregAgentConverter.class);
 
@@ -60,26 +63,23 @@ public class BrregAgentConverter {
                 .setBaseNamespace("http://data.brreg.no/meta/", Builder.AppliesTo.bothElementsAndAttributes)
                 .convertComplexElementsWithOnlyAttributesAndSimpleTypeChildrenToPredicate(true)
                 .convertComplexElementsWithOnlyAttributesToPredicate(true)
-                .renameElement("http://data.brreg.no/meta/navn", FOAF.name.getURI())
                 .renameElement("http://data.brreg.no/meta/enhet", FOAF.Agent.getURI()).build();
         logger.debug("end ");
 
-        org.springframework.core.io.Resource canonicalNamesFile = new ClassPathResource("kanoniske.csv");
-        Reader in = null;
-        Iterable<CSVRecord> records = null;
-        try {
-            in = new BufferedReader(new InputStreamReader(canonicalNamesFile.getInputStream()));
-            records = CSVFormat.EXCEL.parse(in);
-        } catch (IOException e) {
-            logger.error("Could not read canonical names: {}", e.getMessage());
-        }
 
-        if (records != null) {
+        org.springframework.core.io.Resource canonicalNamesFile = new ClassPathResource("kanoniske.csv");
+        Iterable<CSVRecord> records;
+        try (Reader input = new BufferedReader(new InputStreamReader(canonicalNamesFile.getInputStream()))) {
+            records = CSVFormat.EXCEL.parse(input);
+
             for (CSVRecord line : records) {
                 canonicalNames.put(line.get(0), line.get(1));
             }
 
             logger.debug("Read {} canonical names from file.", canonicalNames.size());
+
+        } catch (IOException e) {
+            logger.error("Could not read canonical names: {}", e.getMessage());
         }
 
     }
@@ -120,6 +120,7 @@ public class BrregAgentConverter {
         processAgents(model, DCTerms.publisher);
         processAgents(model, DCTerms.creator);
 
+        postprocessAgentNames(model);
         processAgentHierarchy(model);
     }
 
@@ -140,6 +141,34 @@ public class BrregAgentConverter {
                 }
             } else {
                 logger.warn("{} is not a resource. Probably really broken input!", next);
+            }
+        }
+    }
+
+    /**
+     * Run through all agents and make sure that FOAF.name is set. It should be equal to riginal name or official name.
+     * Original name should be in SKOS.prefLabel.
+     *
+     * @param model the model to iterate over and update
+     */
+    void postprocessAgentNames(Model model) {
+        ResIterator resourceIterator = model.listResourcesWithProperty(RDF.type, FOAF.Agent);
+        while (resourceIterator.hasNext()) {
+            Resource resource = resourceIterator.nextResource();
+
+            Statement foafName = resource.getProperty(FOAF.name);
+            Statement prefLabel = resource.getProperty(SKOS.prefLabel);
+            if (foafName == null || prefLabel != null) {
+                Statement officialName = resource.getProperty(EnhetsregisteretRDF.navn);
+                if (officialName != null) {
+                    String officialNameString = officialName.getObject().asLiteral().getString();
+                    logger.debug("Official name: {} replaces FOAF.name", officialNameString);
+
+                    if (foafName != null) {
+                        resource.removeAll(FOAF.name);
+                    }
+                    resource.addProperty(FOAF.name, officialNameString);
+                }
             }
         }
     }
@@ -171,8 +200,7 @@ public class BrregAgentConverter {
      * (central coordinating register of legal entities)
      * then change the uri to point to Actor resource collected from Enhetstregisteret
      *
-     * @param dataset  the dataset to be examined
-     *
+     * @param dataset                 the dataset to be examined
      * @param masterPublisherResource the Actor resource collected from Enhetsregisteret
      */
     private void substitutePublisherResourceInDataset(Resource dataset, Resource nonStandardPublisherResource, Resource masterPublisherResource) {
@@ -186,7 +214,7 @@ public class BrregAgentConverter {
     }
 
     String extractOrganizationPath(Publisher publisher, Map<String, Publisher> publisherMap) {
-        String prefix = "";
+        String prefix;
         if (publisher != null) {
             if (publisher.getOverordnetEnhet() != null && !publisher.getOverordnetEnhet().isEmpty() && !"/".equals(publisher.getOverordnetEnhet())) {
                 Publisher overordnetEnhet = publisherMap.get(publisher.getOverordnetEnhet());
@@ -215,14 +243,13 @@ public class BrregAgentConverter {
                 }
             }
 
-
             return prefix + "/" + (publisher.getId().startsWith("http") ? publisher.getName() : publisher.getId());
         }
 
         return null;
     }
 
-    /* For each organisation, transform the RDF to match what we expect from it */
+    /* For each organisation, lookup master data and transform the RDF to match what we expect from it */
 
     protected void collectFromUri(String uri, Model model, Resource publisherResource) {
 
@@ -236,118 +263,134 @@ public class BrregAgentConverter {
 
         String masterPublisherUri = uri.substring(0, uri.length() - ".xml".length());
 
+        if (processedPublishers.contains(masterPublisherUri)) {
+            // publisher is already processed, no need to process once more
+            return;
+        }
+
         logger.debug("Collecting from URL {} using subject URI {}", uri, publisherResource.toString());
-        try {
-            if (brregCache != null) {
 
-                String organisationNumber = getOrgnrFromIdentifier(model, publisherResource);
-                String originalOrganisationName = getOrgName(publisherResource);
+        String organisationNumber = getOrgnrFromIdentifier(model, publisherResource);
+        String originalOrganisationName = getOrgName(publisherResource);
 
-                if (organisationNumber == null) {
-                    organisationNumber = getOrgnrFromUri(uri);
-                    if (organisationNumber != null) {
-                        model.add(publisherResource, DCTerms.identifier, organisationNumber);
-                    }
-                }
-
-                if (organisationNumber == null) {
-                    logger.warn("Publisher does not have a organisation number [{}]", uri);
-                }
-                String content = null;
-                try {
-                    content = brregCache.get(new URL(uri));
-                } catch (IOException e) {
-                    // hack to not overload data.brreg.no
-                    Thread.sleep(200);
-                    content = brregCache.get(new URL(uri));
-                }
-
-                logger.trace("[model_before_conversion] {}", content);
-
-                InputStream inputStream = new ByteArrayInputStream(content.getBytes(StandardCharsets.UTF_8));
-                Model masterDataModel = convert(inputStream);
-
-                if (masterPublisherUri.equals(publisherResource.getURI())) {
-                    //later merge of model resulted in publisher having two names, therefore we make sure it is removed
-                    removeDuplicateProperties(model, masterDataModel, FOAF.name); //TODO: remove all duplicate properties?
-                } else {
-                    // replace old uri with masterPublisher uri
-
-                    Resource masterPublisherResource = masterDataModel.getResource(masterPublisherUri);
-
-                    //exchange non-standard uri for organization number with standard one
-                    ResIterator datasetIterator = model.listResourcesWithProperty(DCTerms.publisher, publisherResource);
-                    while (datasetIterator.hasNext()) {
-                        Resource dataset = datasetIterator.next().asResource();
-                        substitutePublisherResourceInDataset(dataset, publisherResource, masterPublisherResource);
-                    }
-
-                    //add missing attributes to master publisher received from Enhetsregisteret
-                    masterPublisherResource.addProperty(DCTerms.identifier, organisationNumber);
-
-                    //remove no longer used publisher, since it will not produce a valid orgpath
-
-                    // remove statements where resource is subject
-                    model.removeAll(publisherResource, null, (RDFNode) null);
-                    // remove statements where resource is object
-                    model.removeAll(null, null, publisherResource);
-
-                    // make sure we use master publisher as resource from now on
-                    publisherResource = masterPublisherResource;
-                }
-
-                lookupPublisherHierarchy(model, masterDataModel);
-                processBlankNodes(masterDataModel, uri);
-
-                logger.debug("Adding {} triples to the model for {}", masterDataModel.size(), uri);
-
-                // merge master data into model. The model will only be updated if model and master have same publisher uri.
-                model.add(masterDataModel);
-
-                addPreferredOrganisationName(model, publisherResource, organisationNumber, originalOrganisationName);
-
-                model.addLiteral(publisherResource, DCTerms.valid, true);
-
-            } else {
-                model.addLiteral(publisherResource, DCTerms.valid, false);
-                logger.warn("Unable to lookup publisher {} - cache is not initiatilized.", uri);
+        if (organisationNumber == null) {
+            organisationNumber = getOrgnrFromUri(uri);
+            if (organisationNumber != null) {
+                model.add(publisherResource, DCTerms.identifier, organisationNumber);
             }
+        }
 
+        if (organisationNumber == null) {
+            logger.warn("Publisher does not have a organisation number [{}]", uri);
+        }
+
+        try {
+            publisherResource = lookupMasterdata(uri, model, publisherResource, masterPublisherUri, organisationNumber);
+            model.addLiteral(publisherResource, DCTerms.valid, true);
         } catch (Exception e) {
             model.addLiteral(publisherResource, DCTerms.valid, false);
-            logger.warn("Failed to lookup publisher: {}. Reason {}", uri, e.getMessage());
+            logger.warn("Unable to lookup publisher {} in master data service. Reason {}", uri, e.getMessage(),e);
         }
+
+        addPreferredOrganisationName(model, publisherResource, organisationNumber, originalOrganisationName);
+
+        processedPublishers.add(publisherResource.getURI());
+
+    }
+
+    private Resource lookupMasterdata(String uri, Model model, Resource publisherResource, String masterPublisherUri, String organisationNumber)
+            throws IOException, SAXException, ParserConfigurationException, ExecutionException, InterruptedException {
+
+        if (brregCache == null) {
+            throw new IOException("BrregCache is not initialized!");
+        }
+
+        String content;
+        try {
+            content = brregCache.get(new URL(uri));
+        } catch (IOException e) {
+            // hack to not overload data.brreg.no
+            Thread.sleep(200);
+            content = brregCache.get(new URL(uri));
+        }
+
+        logger.trace("[model_before_conversion] {}", content);
+
+        try (InputStream inputStream = new ByteArrayInputStream(content.getBytes(StandardCharsets.UTF_8))) {
+
+            Model masterDataModel = convert(inputStream);
+
+            if (!masterPublisherUri.equals(publisherResource.getURI())) {
+
+                // replace old uri with masterPublisher uri
+
+                Resource masterPublisherResource = masterDataModel.getResource(masterPublisherUri);
+
+                //exchange non-standard uri for organization number with standard one
+                ResIterator datasetIterator = model.listResourcesWithProperty(DCTerms.publisher, publisherResource);
+                while (datasetIterator.hasNext()) {
+                    Resource dataset = datasetIterator.next().asResource();
+                    substitutePublisherResourceInDataset(dataset, publisherResource, masterPublisherResource);
+                }
+
+                //add missing attributes to master publisher received from Enhetsregisteret
+                masterPublisherResource.addProperty(DCTerms.identifier, organisationNumber);
+
+                //remove no longer used publisher, since it will not produce a valid orgpath
+
+                // remove statements where resource is subject
+                model.removeAll(publisherResource, null, null);
+                // remove statements where resource is object
+                model.removeAll(null, null, publisherResource);
+
+                // make sure we use master publisher as resource from now on
+                publisherResource = masterPublisherResource;
+            }
+            processBlankNodes(masterDataModel, uri);
+
+            logger.debug("Adding {} triples to the model for {}", masterDataModel.size(), uri);
+
+            // merge master data into model. The model will only be updated if model and master have same publisher uri.
+            model.add(masterDataModel);
+
+            lookupSuperiorPublisher(model, masterDataModel);
+        } catch (Exception e) {
+            logger.error("Error lookup masterdata of {}. Reason {}", uri, e.getMessage(), e);
+        }
+
+        return publisherResource;
     }
 
     void addPreferredOrganisationName(Model model, Resource publisherResource, String organisationNumber, String originalOrganisationName) {
         String preferredName = null;
 
         if (originalOrganisationName != null) {
-            // use original organisation name in prefLabel
+            // make agent name used by catalog owner as preferred name
             preferredName = originalOrganisationName;
         }
 
-        if (organisationNumber != null && canonicalNames.containsKey(organisationNumber)) {
-            // Name hack: use predefined organization name from canonical names table
+        if (preferredName == null && organisationNumber != null && canonicalNames.containsKey(organisationNumber)) {
+            // if no preferredName is given and we have a name in canonicalNames choose that name
             preferredName = canonicalNames.get(organisationNumber);
         }
 
         if (preferredName != null) {
+            // add preferred name to prefLabel
             model.add(publisherResource, SKOS.prefLabel, model.createLiteral(preferredName, "no"));
         }
     }
 
-    private void lookupPublisherHierarchy(Model model, Model masterRegistryModel) {
-        ResIterator subjects = masterRegistryModel.listSubjectsWithProperty(EnhetsregisteretRDF.organisasjonsform);
+    private void lookupSuperiorPublisher(Model model, Model masterDataModel) {
+        ResIterator subjects = masterDataModel.listSubjectsWithProperty(EnhetsregisteretRDF.organisasjonsform);
 
         while (subjects.hasNext()) {
             Resource subject = subjects.next();
-            String organisasjonsform = subject.getProperty(EnhetsregisteretRDF.organisasjonsform).getString();
             Statement overordnetEnhet = subject.getProperty(EnhetsregisteretRDF.overordnetEnhet);
 
             if (overordnetEnhet != null) {
 
-                logger.trace("Found superior publisher: {}", overordnetEnhet.getObject());
+                logger.debug("Found superior publisher: {}", overordnetEnhet.getObject());
+
                 String supOrgUri = String.format(publisherIdURI, overordnetEnhet.getObject().toString());
                 collectFromUri(supOrgUri, model, model.createResource(supOrgUri));
             }
@@ -377,7 +420,8 @@ public class BrregAgentConverter {
                 "forretningsadresse",
                 "postadresse",
                 "naeringskode1",
-                "institusjonellSektorkode"
+                "institusjonellSektorkode",
+                "orgform"
         };
 
         if (resourceURI.endsWith(".xml")) {
@@ -422,11 +466,11 @@ public class BrregAgentConverter {
 
             if (oldProperty == null) {
                 if (officialProperty != null) {
-                    logger.debug("Publisher name: is missing from dataset. Found " + officialProperty.getString() + " which is added to dataset. for resource " + incomingResource.getURI());
+                    logger.debug("Property is missing from dataset. Found " + officialProperty.getString() + " which is added to dataset. for resource " + incomingResource.getURI());
                 }
             } else {
                 if (officialProperty != null) {
-                    logger.debug("Publisher name: found " + oldProperty.getString() + " which is replaced with " + officialProperty.getString() + " for resource " + incomingResource.getURI());
+                    logger.debug("Property found " + oldProperty.getString() + " which is replaced with " + officialProperty.getString() + " for resource " + incomingResource.getURI());
                     existingResource.removeAll(property);
                 }
             }
@@ -435,7 +479,10 @@ public class BrregAgentConverter {
 
     private static void applyNamespaces(Model extractedModel) {
 
+        extractedModel.setNsPrefix("er", EnhetsregisteretRDF.getUri());
         extractedModel.setNsPrefix("foaf", FOAF.getURI());
+        extractedModel.setNsPrefix("skos", SKOS.getURI());
+        extractedModel.setNsPrefix("dcatno", DCATNO.getURI());
     }
 
     private String getOrgName(Resource resource) {
--- libraries/datastore/src/main/java/no/dcat/datastore/domain/dcat/vocabulary/EnhetsregisteretRDF.java
+++ libraries/datastore/src/main/java/no/dcat/datastore/domain/dcat/vocabulary/EnhetsregisteretRDF.java
@@ -1,22 +1,29 @@
 package no.dcat.datastore.domain.dcat.vocabulary;
 
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Property;
-import org.apache.jena.rdf.model.ResourceFactory;
 
 /**
  * Vocabulary definitions from enhetsregisteret
  * @author Marcus Gustafson
  */
 public class EnhetsregisteretRDF {
-    public static final Property overordnetEnhet = metaProperty("overordnetEnhet");
-    public static final Property organisasjonsform = metaProperty("organisasjonsform");
-    public static final Property naeringskode = metaProperty("naeringskode1");
-    public static final Property beskrivelse = metaProperty("beskrivelse");
-    public static final Property kode = metaProperty("kode");
-    public static final Property institusjonellSektorkode = metaProperty("institusjonellSektorkode");
 
-    private static Property metaProperty(String local) {
-        return ResourceFactory.createProperty("http://data.brreg.no/meta/", local);
+    private static final Model model = ModelFactory.createDefaultModel();
+
+    public static final String NS = "http://data.brreg.no/meta/";
+
+    public static String getUri() {
+        return NS;
     }
-}
 
+    public static final Property overordnetEnhet = model.createProperty(NS, "overordnetEnhet");
+    public static final Property organisasjonsform = model.createProperty(NS, "organisasjonsform");
+    public static final Property navn = model.createProperty(NS, "navn");
+    public static final Property naeringskode = model.createProperty(NS, "naeringskode1");
+    public static final Property beskrivelse = model.createProperty(NS, "beskrivelse");
+    public static final Property kode = model.createProperty(NS, "kode");
+    public static final Property institusjonellSektorkode = model.createProperty(NS, "institusjonellSektorkode" );
+
+}
