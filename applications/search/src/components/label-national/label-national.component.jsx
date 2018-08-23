3149d0fb22c76f93e0b9eb368418f5150e5d8ed5
Øyvind Isaksen <oyvind.isaksen@acando.no>
2018-08-23 12:10:00 +0200

    (search) rename DatasetLabelNational to LabelNational and update imports

--- applications/search/src/components/label-national/label-national.component.jsx
+++ applications/search/src/components/label-national/label-national.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import localization from '../../lib/localization';
 
-export const DatasetLabelNational = () => (
+export const LabelNational = () => (
   <div className="fdk-national">
     <i className="fa fa-star mr-2 fdk-color-cta" />
     <strong>{localization.search_hit.NationalBuildingBlock}</strong>
