6aedc163771a6faf7bb61a345b9afa50d610cf7d
Øyvind Isaksen <oyvind.isaksen@acando.no>
2018-08-23 15:06:30 +0200

    (search) result-tab responsive styling and fix paths

--- applications/search/src/pages/search-page/results-tabs/results-tabs.component.jsx
+++ applications/search/src/pages/search-page/results-tabs/results-tabs.component.jsx
@@ -2,23 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { FeatureToggle } from 'react-feature-toggles';
+import cx from 'classnames';
 
 import { addOrReplaceParamWithoutURL } from '../../../lib/addOrReplaceUrlParam';
 import localization from '../../../lib/localization';
 import './results-tabs.scss';
 import { FEATURES } from '../../../app/features';
+import {
+  PATHNAME_DATASETS,
+  PATHNAME_APIS,
+  PATHNAME_CONCEPTS
+} from '../../../constants/constants';
 
 export const ResultsTabs = props => {
-  const { location, countDatasets, countTerms } = props;
-  let { search } = location;
-  search = addOrReplaceParamWithoutURL(search, 'from', '');
+  const { activePath, searchParam, countDatasets, countTerms } = props;
+  const search = addOrReplaceParamWithoutURL(searchParam, 'from', '');
   return (
     <div className="row">
       <div className="col-12 col-lg-8 offset-lg-4">
-        <ul className="search-results-tabs">
-          <li className={location.pathname === '/' ? 'li-active' : ''}>
+        <ul className="search-results-tabs d-flex align-items-center justify-content-between">
+          <li
+            className={cx('d-flex justify-content-center', {
+              'li-active': activePath === PATHNAME_DATASETS
+            })}
+          >
             <Link
-              to={{ pathname: '/', search }}
+              to={{ pathname: PATHNAME_DATASETS, search }}
               aria-label="Link til side for datasett:"
             >
               {localization.page.datasetTab}
@@ -26,9 +35,13 @@ export const ResultsTabs = props => {
             </Link>
           </li>
           <FeatureToggle featureName={FEATURES.API}>
-            <li className={location.pathname === '/api' ? 'li-active' : ''}>
+            <li
+              className={cx('d-flex justify-content-center', {
+                'li-active': activePath === PATHNAME_APIS
+              })}
+            >
               <Link
-                to={{ pathname: '/api', search }}
+                to={{ pathname: PATHNAME_APIS, search }}
                 aria-label="Link til side for api:"
               >
                 {localization.page.apiTab}
@@ -36,9 +49,13 @@ export const ResultsTabs = props => {
               </Link>
             </li>
           </FeatureToggle>
-          <li className={location.pathname === '/concepts' ? 'li-active' : ''}>
+          <li
+            className={cx('d-flex justify-content-center', {
+              'li-active': activePath === PATHNAME_CONCEPTS
+            })}
+          >
             <Link
-              to={{ pathname: '/concepts', search }}
+              to={{ pathname: PATHNAME_CONCEPTS, search }}
               aria-label="Link til side for begrep:"
             >
               {localization.page.termTab}
@@ -52,12 +69,15 @@ export const ResultsTabs = props => {
 };
 
 ResultsTabs.defaultProps = {
+  activePath: null,
+  searchParam: '',
   countDatasets: null,
   countTerms: null
 };
 
 ResultsTabs.propTypes = {
-  location: PropTypes.object.isRequired,
+  activePath: PropTypes.string,
+  searchParam: PropTypes.string,
   countDatasets: PropTypes.number,
   countTerms: PropTypes.number
 };
--- applications/search/src/pages/search-page/results-tabs/results-tabs.component.test.jsx
+++ applications/search/src/pages/search-page/results-tabs/results-tabs.component.test.jsx
@@ -1,29 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { ResultsTabs } from './results-tabs.component';
+import {
+  PATHNAME_DATASETS,
+  PATHNAME_CONCEPTS
+} from '../../../constants/constants';
 
 test('should render ResultsTabs with "Dataset" active correctly', () => {
-  const defaultProps = {
-    location: {
-      pathname: '/',
-      search: '',
-      hash: '',
-      key: 'ublg0e'
-    }
-  };
-  const wrapper = shallow(<ResultsTabs {...defaultProps} />);
+  const wrapper = shallow(<ResultsTabs activePath={PATHNAME_DATASETS} />);
   expect(wrapper).toMatchSnapshot();
 });
 
 test('should render ResultsTabs with "Concepts" active correctly', () => {
-  const defaultProps = {
-    location: {
-      pathname: '/concepts',
-      search: '',
-      hash: '',
-      key: 'ublg0e'
-    }
-  };
-  const wrapper = shallow(<ResultsTabs {...defaultProps} />);
+  const wrapper = shallow(<ResultsTabs activePath={PATHNAME_CONCEPTS} />);
   expect(wrapper).toMatchSnapshot();
 });
