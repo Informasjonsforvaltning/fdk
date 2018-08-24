d7ff0d77db3f64e960b5588b7c8d9fbc98d75bae
Øyvind Isaksen <oyvind.isaksen@acando.no>
2018-08-24 09:36:06 +0200

    (search) change pathname 'api' to 'apis'

--- applications/search/src/constants/constants.js
+++ applications/search/src/constants/constants.js
@@ -1,6 +1,6 @@
 export const PATHNAME_DATASETS = '/';
 export const PATHNAME_DATASET_DETAILS = '/datasets';
-export const PATHNAME_APIS = '/api';
+export const PATHNAME_APIS = '/apis';
 export const PATHNAME_CONCEPTS = '/concepts';
 export const PATHNAME_REPORTS = '/reports';
 export const PATHNAME_ABOUT = '/about';
