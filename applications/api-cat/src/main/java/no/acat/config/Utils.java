906e7b76644c36929c92111d082ea44d136bdcdd
Markko Paas <markko.paas@gmail.com>
2018-09-01 15:16:23 +0200

    (api-cat) Use OpenAPI POJO-s from swagger

--- applications/api-cat/src/main/java/no/acat/config/Utils.java
+++ applications/api-cat/src/main/java/no/acat/config/Utils.java
@@ -1,5 +1,6 @@
 package no.acat.config;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -8,6 +9,7 @@ public class Utils {
     public static ObjectMapper jsonMapper() {
         ObjectMapper mapper = new ObjectMapper();
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
 
         return mapper;
     }
--- applications/api-cat/src/main/java/no/acat/model/openapi3/Server.java
+++
@@ -1,9 +0,0 @@
-package no.acat.model.openapi3;
-
-import lombok.Data;
-
-@Data
-public class Server {
-    String url;
-    String description;
-}
