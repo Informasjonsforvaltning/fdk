5d07f3ab658c32a6ad300198b87146c0a72dfbf6
Øyvind Isaksen <oyvind.isaksen@acando.no>
2018-09-04 13:58:59 +0200

    (search) add API to custom-hits-stats

--- applications/search/src/pages/search-page/search-box/custom-hits-stats/custom-hits-stats.component.jsx
+++ applications/search/src/pages/search-page/search-box/custom-hits-stats/custom-hits-stats.component.jsx
@@ -3,42 +3,75 @@ import PropTypes from 'prop-types';
 import localization from '../../../../lib/localization';
 
 export const CustomHitsStats = props => {
-  const { countDatasets, countTerms, filteringOrTextSearchPerformed } = props;
+  const {
+    countDatasets,
+    countTerms,
+    countApis,
+    filteringOrTextSearchPerformed
+  } = props;
 
   if (filteringOrTextSearchPerformed) {
+    const datasetTextCount =
+      countDatasets > 0
+        ? `${countDatasets} ${localization.hitstats.datasetHits}`
+        : '';
+    const apisTextCount =
+      countApis > 0 ? `${countApis} ${localization.hitstats.apiHits}` : '';
+    const termsTextCount =
+      countTerms > 0
+        ? `${countTerms} ${localization.hitstats.conceptHits}`
+        : '';
     return (
       <div className="sk-hits-stats" data-qa="hits-stats">
         <div className="sk-hits-stats__info" data-qa="info">
           {countDatasets > 0 &&
-            countTerms === 0 && (
-              <div>
-                <span>{localization.hitstats.search}&nbsp;</span>
-                <span>
-                  {countDatasets} {localization.hitstats.datasetHits}
-                </span>
-              </div>
-            )}
-          {countDatasets > 0 &&
+            countApis > 0 &&
             countTerms > 0 && (
-              <div>
+              <React.Fragment>
                 <span>{localization.hitstats.search}&nbsp;</span>
-                <span>
-                  {countDatasets} {localization.hitstats.datasetHits}
-                </span>
+                <span>{datasetTextCount}</span>
+                <span>,&nbsp;{apisTextCount}</span>
                 <span>&nbsp;{localization.hitstats.and}&nbsp;</span>
-                {countTerms} {localization.hitstats.conceptHits}
-              </div>
+                <span>{termsTextCount}</span>
+              </React.Fragment>
             )}
-          {countDatasets === 0 &&
-            countTerms > 0 && (
+
+          {countDatasets > 0 &&
+            ((countApis > 0 && countTerms === 0) ||
+              (countApis === 0 && countTerms > 0)) && (
               <div>
                 <span>{localization.hitstats.search}&nbsp;</span>
+                <span>{datasetTextCount}</span>
                 <span>
-                  {countTerms} {localization.hitstats.conceptHits}
+                  &nbsp;{localization.hitstats.and}&nbsp;{apisTextCount}
+                  {termsTextCount}
                 </span>
               </div>
             )}
+
           {countDatasets === 0 &&
+            (countApis > 0 || countTerms > 0) && (
+              <React.Fragment>
+                <span>{localization.hitstats.search}&nbsp;</span>
+                <span>{apisTextCount}</span>
+                {countApis > 0 &&
+                  countTerms > 0 && (
+                    <span>&nbsp;{localization.hitstats.and}&nbsp;</span>
+                  )}
+                <span>{termsTextCount}</span>
+              </React.Fragment>
+            )}
+
+          {countDatasets > 0 &&
+            (countApis === 0 && countTerms === 0) && (
+              <React.Fragment>
+                <span>{localization.hitstats.search}&nbsp;</span>
+                <span>{datasetTextCount}</span>
+              </React.Fragment>
+            )}
+
+          {countDatasets === 0 &&
+            countApis === 0 &&
             countTerms === 0 && <span>{localization.hitstats.noHits}</span>}
         </div>
       </div>
@@ -52,6 +85,11 @@ export const CustomHitsStats = props => {
             {localization.hitstats.nosearch.search} {countDatasets}{' '}
             {localization.hitstats.nosearch.descriptions}
           </span>
+          {countApis > 0 && (
+            <span>
+              , {countApis} {localization.hitstats.apis}
+            </span>
+          )}
           <span>
             &nbsp;{localization.hitstats.and} {countTerms}{' '}
             {localization.hitstats.concepts}
@@ -65,11 +103,13 @@ export const CustomHitsStats = props => {
 CustomHitsStats.defaultProps = {
   countDatasets: null,
   countTerms: null,
+  countApis: null,
   filteringOrTextSearchPerformed: false
 };
 
 CustomHitsStats.propTypes = {
   countDatasets: PropTypes.number,
   countTerms: PropTypes.number,
+  countApis: PropTypes.number,
   filteringOrTextSearchPerformed: PropTypes.bool
 };
