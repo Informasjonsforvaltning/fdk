321dac655ca22c46da947e8c41b892b0f98f9c19
Øyvind Isaksen <oyvind.isaksen@acando.no>
2018-09-11 14:17:37 +0200

    (search) api-details-page add empty space bottom

--- applications/search/src/pages/api-details-page/api-details-page.jsx
+++ applications/search/src/pages/api-details-page/api-details-page.jsx
@@ -282,10 +282,7 @@ export const ApiDetailsPage = props => {
         <div className="row">
           <div className="col-12 col-lg-4 ">{renderStickyMenu(apiItem)}</div>
 
-          <section
-            className="col-12 col-lg-8 mt-3"
-            style={{ marginBottom: '400px' }}
-          >
+          <section className="col-12 col-lg-8 mt-3">
             {renderDescription(description)}
 
             {renderFormats(_.get(apiItem, 'formats'))}
@@ -301,6 +298,8 @@ export const ApiDetailsPage = props => {
             {renderDatasetReferences(_.get(apiItem, 'datasetReferences'))}
 
             {renderContactPoints(_.get(apiItem, 'contactPoint'))}
+
+            <div style={{ height: '75vh' }} />
           </section>
         </div>
       </article>
