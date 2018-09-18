845ab133ee49884455d2f0f63cc3189c122fcc21
David Skogan <david.skogan@gmail.com>
2018-09-18 16:37:04 +0200

    Updated runDocker to build registration and search

--- runDocker.sh
+++ runDocker.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
- mvn clean install -T 2C -pl applications/$1 -am -DskipTests
+if [ $1 = "registration-react" ]
+then
+    cd applications/registration-react
+    docker build -t dcatno/registration-react:latest .
+else if [ $1 = "search" ]
+  then
+    cd applications/search
+    docker build -t dcatno/search:latest .
+  else
+    mvn clean install -T 2C -pl applications/$1 -am -DskipTests
+  fi
+fi
 
  docker-compose stop $1 || true
  docker-compose up -d $1
