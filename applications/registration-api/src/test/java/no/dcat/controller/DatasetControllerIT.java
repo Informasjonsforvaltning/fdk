43dd68d6e9da4e6f8a35262837642da5bee0c23c
Markko Paas <markko.paas@gmail.com>
2018-09-18 07:32:16 +0200

    (registration-api) Use spring-boot-starter-parent as parent instead of fdk

--- applications/registration-api/src/test/java/no/dcat/controller/DatasetControllerIT.java
+++ applications/registration-api/src/test/java/no/dcat/controller/DatasetControllerIT.java
@@ -88,7 +88,7 @@ public class DatasetControllerIT {
                                 .post("/catalogs")
                                 .content(asJsonString(catalog))
                                 .contentType(MediaType.APPLICATION_JSON_UTF8))
-                .andExpect(content().string("{\"id\":\"910244132\",\"uri\":\"http://brreg.no/catalogs/910244132\",\"title\":{},\"description\":{},\"publisher\":{\"uri\":\"http://data.brreg.no/enhetsregisteret/enhet/910244132\",\"id\":\"910244132\",\"name\":\"RAMSUND OG ROGNAN REVISJON\"}}"))
+                .andExpect(content().string("{\"id\":\"910244132\",\"uri\":\"http://brreg.no/catalogs/910244132\",\"title\":{},\"description\":{},\"publisher\":{\"uri\":\"https://data.brreg.no/enhetsregisteret/enhet/910244132\",\"id\":\"910244132\",\"name\":\"RAMSUND OG ROGNAN REVISJON\"}}"))
                 .andExpect(status().isOk());
 
 
