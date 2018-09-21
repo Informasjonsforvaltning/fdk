c6270222049bf0f9bd7add25aa8ed45daf2a5abb
Markko Paas <markko.paas@gmail.com>
2018-09-21 14:36:25 +0200

    (e2e) Improve commands to run tests on host, headless and in container

--- applications/e2e/test/app/app.test.js
+++ applications/e2e/test/app/app.test.js
@@ -1,38 +1,16 @@
-import puppeteer from "puppeteer";
-import { isDebugging } from './../testingInit';
 import delay from 'delay'
-
-const APP = process.env.HOST_SEARCH;
-let page;
-let browser;
-const width = 1920;
-const height = 1080;
-
-beforeAll(async () => {
-    try {
-        browser = await puppeteer.launch(isDebugging().puppeteer);
-        page = await browser.newPage();
-        page.on('console', consoleMessage => {
-            console.log('CONSOLE MESSAGE TYPE:', consoleMessage.type())
-            console.log('CONSOLE MESSAGE TEXT:', consoleMessage.text())
-            consoleMessage.args().forEach(msgArgJSHandle => {
-                console.log('CONSOLE MESSAGE ARG:', msgArgJSHandle.jsonValue())
-            })
-        });
-        await page.setViewport({ width, height });
-    } catch (e) {
-        console.error('got error', e)
-    }
-});
+import * as browser from "../../lib/browser";
+import {config} from "../../config";
 
 afterAll(() => {
-    browser.close();
+    browser.close();// we might not want to close the browser between each suite
 });
 
 describe('App', () => {
 
     test('search page opens', async () => {
-        await page.goto(APP)
+        const page = await browser.getNewBrowserPage();
+        await page.goto(config.searchHost)
 
         // Search page component now waits for resolving API request before rendering.
         // If some other solution is implemented with initial rendering, delay can be removed
