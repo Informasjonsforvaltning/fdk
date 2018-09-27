be99c730199223adc3c016afe63c40ec7d5f529d
Øyvind Isaksen <oyvind.isaksen@acando.no>
2018-09-27 14:09:38 +0200

    (registration-react) refactor redux datasets

--- applications/registration-react/src/redux/modules/datasets.js
+++ applications/registration-react/src/redux/modules/datasets.js
@@ -1,32 +1,71 @@
+import _ from 'lodash';
 import {
   DATASETS_REQUEST,
   DATASETS_SUCCESS,
   DATASETS_FAILURE
 } from '../../constants/ActionTypes';
+import { fetchActions } from '../fetchActions';
 
-export default function datasets(
-  state = { isFetchingDatasets: false, datasetItems: null },
-  action
-) {
+function shouldFetch(metaState) {
+  const threshold = 60 * 1000; // seconds
+  return (
+    !metaState ||
+    (!metaState.isFetching &&
+      (metaState.lastFetch || 0) < Date.now() - threshold)
+  );
+}
+
+export function fetchDatasetsIfNeeded(catalogId) {
+  return (dispatch, getState) =>
+    shouldFetch(_.get(getState(), ['datasets', catalogId, 'meta'])) &&
+    dispatch(
+      fetchActions(`/catalogs/${catalogId}/datasets?size=100`, [
+        { type: DATASETS_REQUEST, meta: { catalogId } },
+        { type: DATASETS_SUCCESS, meta: { catalogId } },
+        { type: DATASETS_FAILURE, meta: { catalogId } }
+      ])
+    );
+}
+
+const initialState = {};
+
+export default function datasets(state = initialState, action) {
   switch (action.type) {
     case DATASETS_REQUEST:
       return {
         ...state,
-        isFetchingDatasets: true
+        [action.meta.catalogId]: {
+          meta: {
+            isFetching: true,
+            lastFetch: null
+          }
+        }
       };
     case DATASETS_SUCCESS:
       return {
         ...state,
-        isFetchingDatasets: false,
-        datasetItems: action.payload
+        [action.meta.catalogId]: {
+          items: action.payload,
+          meta: {
+            isFetching: false,
+            lastFetch: Date.now()
+          }
+        }
       };
     case DATASETS_FAILURE:
       return {
         ...state,
-        isFetchingDatasets: false,
-        datasetItems: null
+        [action.meta.catalogId]: {
+          meta: {
+            isFetching: false,
+            lastFetch: null
+          }
+        }
       };
     default:
       return state;
   }
 }
+
+export const getDatasetItemsByCatalogId = (datasets, catalogId) =>
+  _.get(datasets, [catalogId, 'items']);
