67ad8b50331ab4a5221adf3e40d5de0df91f3a6a
Øyvind Isaksen <oyvind.isaksen@acando.no>
2018-09-28 09:35:43 +0200

    (search) API-298 design - change list-regular items to be flex-boxes and change spacing

--- applications/search/src/components/list-regular/twoColRow/twoColRow.jsx
+++ applications/search/src/components/list-regular/twoColRow/twoColRow.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 export const TwoColRow = props => {
   const { col1, col2 } = props;
   return (
-    <div className="row list-regular--item">
+    <div className="d-flex list-regular--item">
       <div className="col-4 pl-0 fdk-text-strong">{col1}</div>
       <div className="col-8">{col2}</div>
     </div>
