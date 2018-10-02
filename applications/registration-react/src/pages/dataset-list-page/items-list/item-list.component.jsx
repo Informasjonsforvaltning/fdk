62d0b1e46ba4c56d24465e3364dd95eef674af74
Øyvind Isaksen <oyvind.isaksen@acando.no>
2018-10-02 13:55:31 +0200

    (registration-react) add list-items to components and refactor dataset-list-page/items-list
    (cherry picked from commit 6ecdf3c)

--- applications/registration-react/src/pages/dataset-list-page/items-list/item-list.component.jsx
+++ applications/registration-react/src/pages/dataset-list-page/items-list/item-list.component.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import orderBy from 'lodash/orderBy';
 import axios from 'axios';
 import _ from 'lodash';
 
 import localization from '../../../utils/localization';
-import DatasetItemsListItem from './list-item/list-item.component';
 import ImportModal from '../../../components/import-modal/import-modal.component';
-import SortButtons from '../../../components/sort-button/sort-button.component';
+import { ListItems } from '../../../components/list-items/list-items.component';
 import './items-list.scss';
 
 const handleCreateDataset = () => {
@@ -88,43 +86,9 @@ class DatasetItemsList extends React.Component {
       });
   }
 
-  _renderDatasetItems() {
-    const { catalogId, datasetItems } = this.props;
-    if (datasetItems) {
-      if (this.state.sortField === 'title') {
-        // order by title and ignore case
-        orderBy(
-          datasetItems,
-          [
-            item => {
-              const { nb } = item.title;
-              if (nb) {
-                return nb.toLowerCase();
-              }
-              return null;
-            }
-          ],
-          [this.state.sortType]
-        );
-      } else if (this.state.sortField === 'registrationStatus') {
-        orderBy(datasetItems, 'registrationStatus', [this.state.sortType]);
-      }
-
-      return datasetItems.map(item => (
-        <DatasetItemsListItem key={item.id} catalogId={catalogId} item={item} />
-      ));
-    }
-    return (
-      <div className="fdk-datasets-list-item d-flex">
-        <span className="fdk-text-size-small fdk-color2">
-          {localization.datasets.list.missingItems}
-        </span>
-      </div>
-    );
-  }
-
   render() {
     const { sortField, sortType } = this.state;
+    const { catalogId, datasetItems } = this.props;
     return (
       <div>
         <div className="d-flex mb-3">
@@ -144,32 +108,14 @@ class DatasetItemsList extends React.Component {
           </button>
         </div>
 
-        <div className="fdk-datasets-list-header d-flex">
-          <div className="d-flex align-items-center w-75">
-            <span className="header-item mr-1">
-              {localization.datasets.list.header.title}
-            </span>
-            <SortButtons
-              field="title"
-              sortField={sortField}
-              sortType={sortType}
-              onSortField={this.onSortField}
-            />
-          </div>
+        <ListItems
+          catalogId={catalogId}
+          items={datasetItems}
+          sortField={sortField}
+          sortType={sortType}
+          onSortField={this.onSortField}
+        />
 
-          <div className="d-flex align-items-center w-25">
-            <span className="header-item mr-1">
-              {localization.datasets.list.header.status}
-            </span>
-            <SortButtons
-              field="registrationStatus"
-              sortField={sortField}
-              sortType={sortType}
-              onSortField={this.onSortField}
-            />
-          </div>
-        </div>
-        {this._renderDatasetItems()}
         <ImportModal
           modal={this.state.showImportModal}
           handleAction={this.handleImportDataset}
