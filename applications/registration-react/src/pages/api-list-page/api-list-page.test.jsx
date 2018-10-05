2d8001faea931adcb68f8ad2efe81c0ed3e65139
Øyvind Isaksen <oyvind.isaksen@acando.no>
2018-10-05 14:16:54 +0200

    (registration-react) add api-list

---
+++ applications/registration-react/src/pages/api-list-page/api-list-page.test.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { APIListPage } from './api-list-page';
+
+let defaultProps;
+let wrapper;
+let fetchCatalogIfNeeded;
+let fetchApisIfNeeded;
+
+beforeEach(() => {
+  fetchCatalogIfNeeded = jest.fn();
+  fetchApisIfNeeded = jest.fn();
+  const catalogItem = {
+    id: '910244132',
+    uri: 'http://brreg.no/catalogs/910244132',
+    title: {
+      nb: 'Datakatalog for RAMSUND OG ROGNAN REVISJON'
+    },
+    description: {},
+    publisher: {
+      uri: 'http://data.brreg.no/enhetsregisteret/enhet/910244132',
+      name: 'RAMSUND OG ROGNAN REVISJON'
+    }
+  };
+  const match = {
+    path: '/catalogs/:catalogId/apiSpecs',
+    url: '/catalogs/910244132/apiSpecs',
+    isExact: true,
+    params: { catalogId: '910244132' }
+  };
+  defaultProps = {
+    catalogItem,
+    fetchCatalogIfNeeded,
+    fetchApisIfNeeded,
+    match
+  };
+  wrapper = shallow(<APIListPage {...defaultProps} />);
+});
+
+test('should render APIListPageComponent correctly', () => {
+  expect(wrapper).toMatchSnapshot();
+});
