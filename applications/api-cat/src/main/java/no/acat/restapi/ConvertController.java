f1b11a188e7dc719281b38e3ed75eb14ebba5577
Markko Paas <markko.paas@gmail.com>
2018-10-06 21:46:24 +0200

    (api-cat) convert endpoint for parsing url and spec into internal model

---
+++ applications/api-cat/src/main/java/no/acat/restapi/ConvertController.java
@@ -0,0 +1,52 @@
+package no.acat.restapi;
+
+import com.google.common.base.Strings;
+import io.swagger.v3.oas.models.OpenAPI;
+import lombok.AllArgsConstructor;
+import no.acat.model.ConvertRequest;
+import no.acat.model.ConvertResponse;
+import no.acat.spec.ParseException;
+import no.acat.spec.Parser;
+import no.dcat.webutils.exceptions.BadRequestException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+public class ConvertController {
+    @RequestMapping("/convert")
+    @PostMapping(produces = "application/json")
+    public ConvertResponse convert(@RequestBody ConvertRequest request) throws BadRequestException {
+        ConvertResponse responseBody = new ConvertResponse();
+        List<String> messages = new ArrayList();
+        String spec = request.getSpec();
+        String url = request.getUrl();
+
+        if (Strings.isNullOrEmpty(spec) && Strings.isNullOrEmpty(spec)) {
+            throw new BadRequestException("Url or spec parameter is required");
+        }
+
+        if (Strings.isNullOrEmpty(spec) && !Strings.isNullOrEmpty(url)) {
+            try {
+                spec = Parser.getSpecFromUrl(url);
+            } catch (Exception e) {
+                throw new BadRequestException("Error downloading spec: " + e.getMessage());
+            }
+        }
+
+        try {
+            OpenAPI openAPI = Parser.parse(spec);
+            responseBody.setOpenApi(openAPI);
+        } catch (ParseException e) {
+            messages.add(e.getMessage());
+        }
+
+        if (messages.size() > 0) {
+            responseBody.setMessages(messages);
+        }
+        return responseBody;
+    }
+
+}
