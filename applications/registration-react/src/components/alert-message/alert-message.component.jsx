0985cfcb462b44ee4defa793e5b25257823039d4
Øyvind Isaksen <oyvind.isaksen@acando.no>
2018-10-11 10:37:41 +0200

    (registration-react) add component alert-message
    (cherry picked from commit 897d8b1)

---
+++ applications/registration-react/src/components/alert-message/alert-message.component.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+
+export const AlertMessage = props => {
+  const { type, children } = props;
+  if (!(type && children)) {
+    return null;
+  }
+  const alertClassnames = cx('alert', 'pt-3', 'pb-3', {
+    'alert-success': type === 'success',
+    'alert-warning': type === 'warning',
+    'alert-danger': type === 'danger',
+    'alert-info': type === 'info'
+  });
+  const iconClassnames = cx('fa', 'mr-2', {
+    'fa-exclamation-triangle': type === 'danger',
+    'fa-check': type === 'success'
+  });
+  return (
+    <div className={alertClassnames}>
+      <i className={iconClassnames} />
+      <span>{children}</span>
+    </div>
+  );
+};
+
+AlertMessage.propTypes = {
+  type: PropTypes.oneOf(['success', 'warning', 'danger', 'info']).isRequired,
+  children: PropTypes.node.isRequired
+};
