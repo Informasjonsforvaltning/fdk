a43f011447ff2fedd2d8db5a3b664766b2bae20a
Frode Roxrud Gill <extfrg@brreg.no>
2018-10-15 12:55:02 +0200

    Refactor HtmlCleaner into a separate library module

--- applications/harvester-api/src/main/java/no/dcat/harvester/DataEnricher.java
+++ applications/harvester-api/src/main/java/no/dcat/harvester/DataEnricher.java
@@ -1,7 +1,7 @@
 package no.dcat.harvester;
 
 
-import no.dcat.harvester.clean.HtmlCleaner;
+import no.dcat.htmlclean.HtmlCleaner;
 import no.dcat.datastore.domain.dcat.vocabulary.DCAT;
 import org.apache.jena.datatypes.xsd.XSDDatatype;
 import org.apache.jena.rdf.model.*;
--- libraries/html-cleaner/src/main/java/no/dcat/htmlclean/HtmlCleaner.java
+++ libraries/html-cleaner/src/main/java/no/dcat/htmlclean/HtmlCleaner.java
@@ -1,4 +1,4 @@
-package no.dcat.harvester.clean;
+package no.dcat.htmlclean;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
