f1c40747a432a1f8d02744add2ca65a560464838
Markko Paas <markko.paas@gmail.com>
2018-10-16 12:57:28 +0200

    (api-cat) Fix invalid parameter check for /convert endpoint

--- applications/api-cat/src/main/java/no/acat/restapi/ConvertController.java
+++ applications/api-cat/src/main/java/no/acat/restapi/ConvertController.java
@@ -8,7 +8,12 @@ import no.acat.spec.Parser;
 import no.dcat.client.apicat.ConvertRequest;
 import no.dcat.client.apicat.ConvertResponse;
 import no.dcat.webutils.exceptions.BadRequestException;
-import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +21,8 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 public class ConvertController {
+    private static final Logger logger = LoggerFactory.getLogger(ConvertController.class);
+
     @RequestMapping("/convert")
     @PostMapping(produces = "application/json")
     public ConvertResponse convert(@RequestBody ConvertRequest request) throws BadRequestException {
@@ -23,8 +30,9 @@ public class ConvertController {
         List<String> messages = new ArrayList();
         String spec = request.getSpec();
         String url = request.getUrl();
+        logger.debug("Starting convert spec:{}, url:{}", spec, url);
 
-        if (Strings.isNullOrEmpty(spec) && Strings.isNullOrEmpty(spec)) {
+        if (Strings.isNullOrEmpty(spec) && Strings.isNullOrEmpty(url)) {
             throw new BadRequestException("Url or spec parameter is required");
         }
 
