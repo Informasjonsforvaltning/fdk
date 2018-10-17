c6489f0a3a4073f9690e5d4fd957c2d697c8fecb
Thomas Nøkleby <thomas.nokleby@gmail.com>
2018-10-17 08:50:20 +0200

    (search-api) API-251 Removed traces of path variable urldecoder

--- applications/search-api/src/main/java/no/dcat/portal/query/QueryApplication.java
+++ applications/search-api/src/main/java/no/dcat/portal/query/QueryApplication.java
@@ -34,38 +34,6 @@ public class QueryApplication extends WebMvcConfigurerAdapter {
         SpringApplication.run(QueryApplication.class, args);
     }
 
-    // Needed to fix handling of encoded uri in dataset identifiers
-    @Override
-    public void configurePathMatch(PathMatchConfigurer configurer) {
-        UrlPathHelperFixed urlPathHelper = new UrlPathHelperFixed();
-        urlPathHelper.setUrlDecode(false);
-        configurer.setUrlPathHelper(urlPathHelper);
-    }
-
-    public class UrlPathHelperFixed extends UrlPathHelper {
-
-        public UrlPathHelperFixed() {
-            super.setUrlDecode(false);
-        }
-
-        @Override
-        public void setUrlDecode(boolean urlDecode) {
-            if (urlDecode) {
-                throw new IllegalArgumentException("Handler [" + UrlPathHelperFixed.class.getName() + "] does not support URL decoding.");
-            }
-        }
-
-        @Override
-        public String getServletPath(HttpServletRequest request) {
-            return getOriginatingServletPath(request);
-        }
-
-        @Override
-        public String getOriginatingServletPath(HttpServletRequest request) {
-            return request.getRequestURI().substring(request.getContextPath().length());
-        }
-    }
-
     @Bean
     public Docket swaggerDocket() {
 
