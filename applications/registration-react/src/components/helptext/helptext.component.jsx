341c7a4033caf95b5e5f4708b8116f0fb7e94a48
Øyvind Isaksen <oyvindisaks1@gmail.com>
2018-10-18 11:56:49 +0200

    (registration-react) refactor component helptext to stateless and update all snapshots

--- applications/registration-react/src/components/helptext/helptext.component.jsx
+++ applications/registration-react/src/components/helptext/helptext.component.jsx
@@ -1,46 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import { Collapse } from 'reactstrap';
+import _ from 'lodash';
+import { withState, withHandlers, compose } from 'recompose';
 
 import localization from '../../utils/localization';
 import './helptext.scss';
 
-export default class Helptext extends Component {
-  constructor(props) {
-    super(props);
-    this.toggle = this.toggle.bind(this);
-    this.state = { collapse: false };
-  }
+export const Helptext = props => {
+  const { title, required, helptextItems, onToggle, collapse } = props;
 
-  toggle(e) {
-    e.preventDefault();
-    this.setState({ collapse: !this.state.collapse });
-  }
+  const collapseClass = cx('fa', 'fdk-fa-left', {
+    'fa-angle-double-down': !collapse,
+    'fa-angle-double-up': collapse
+  });
 
-  render() {
-    const collapseClass = cx('fa', 'fdk-fa-left', {
-      'fa-angle-double-down': !this.state.collapse,
-      'fa-angle-double-up': this.state.collapse
-    });
+  const shortTextClass = cx('m-0', {
+    'text-ellipsis': !collapse
+  });
 
-    const shortTextClass = cx('m-0', {
-      'text-ellipsis': !this.state.collapse
-    });
-    const { title, required, helptextItems } = this.props;
-    const { shortdesc, description } = helptextItems;
+  const shortdesc = _.get(helptextItems, 'shortdesc', '');
+  const description = _.get(helptextItems, 'description', '');
 
-    return (
-      <div className="fdk-reg-helptext mb-3 p-3">
-        <div className="d-flex align-items-center">
-          <h3>{title}</h3>
-          {required && (
-            <span className="fdk-badge badge badge-secondary ml-2">
-              {localization.helptext.required}
-            </span>
-          )}
-        </div>
-        <div className="d-md-flex">
+  return (
+    <div className="fdk-reg-helptext mb-3 p-3">
+      <div className="d-flex align-items-center">
+        <h3>{title}</h3>
+        {required && (
+          <span className="fdk-badge badge badge-secondary ml-2">
+            {localization.helptext.required}
+          </span>
+        )}
+      </div>
+      <div className="d-md-flex">
+        {shortdesc && (
           <p
             className={shortTextClass}
             dangerouslySetInnerHTML={{
@@ -50,38 +44,55 @@ export default class Helptext extends Component {
                   : ''
             }}
           />
-
+        )}
+        {description && (
           <button
             className="fdk-btn-no-border text-left p-0 ml-1 fdk-reg-helptext-more align-self-start"
-            onClick={e => this.toggle(e)}
+span class="pl-smi">             onClick={onToggle}
           >
             <i className={collapseClass} />
             {localization.helptext.more}
           </button>
-        </div>
-        <Collapse className="mt-3" isOpen={this.state.collapse}>
-          <p
-            dangerouslySetInnerHTML={{
-              __html:
-                description && description.nb
-                  ? description.nb.replace(new RegExp('\n', 'g'), '<br />')
-                  : ''
-            }}
-          />
-        </Collapse>
+        )}
       </div>
-    );
-  }
-}
+      <Collapse className="mt-3" isOpen={collapse}>
+        <p
+          dangerouslySetInnerHTML={{
+            __html:
+              description && description.nb
+                ? description.nb.replace(new RegExp('\n', 'g'), '<br />')
+                : ''
+          }}
+        />
+      </Collapse>
+    </div>
+  );
+};
 
 Helptext.defaultProps = {
   title: '',
   required: false,
-  helptextItems: null
+  helptextItems: null,
+  onToggle: _.noop(),
+  collapse: false
 };
 
 Helptext.propTypes = {
   title: PropTypes.string,
   required: PropTypes.bool,
-  helptextItems: PropTypes.object
+  helptextItems: PropTypes.object,
+  onToggle: PropTypes.func,
+  collapse: PropTypes.bool
 };
+
+const enhance = compose(
+  withState('collapse', 'toggleCollapse', false),
+  withHandlers({
+    onToggle: props => e => {
+      e.preventDefault();
+      props.toggleCollapse(!props.collapse);
+    }
+  })
+);
+
+export default enhance(Helptext);
