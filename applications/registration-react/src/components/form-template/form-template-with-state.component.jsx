abb1533d3237f68370de8783865e4e8ee4e41dc6
Øyvind Isaksen <oyvind.isaksen@acando.no>
2018-10-18 08:33:00 +0200

    (registration-react) refactor component form-template to accept initial prop open or collapsed
    (cherry picked from commit 2b1fee7)

--- applications/registration-react/src/components/form-template/form-template-with-state.component.jsx
+++ applications/registration-react/src/components/form-template/form-template-with-state.component.jsx
@@ -2,7 +2,7 @@ import { withState, withHandlers, compose } from 'recompose';
 import { FormTemplate } from './form-template.component';
 
 const enhance = compose(
-  withState('collapse', 'toggleCollapse', false),
+  withState('collapse', 'toggleCollapse', ({ showInitially }) => showInitially),
   withHandlers({
     onToggle: props => () => {
       props.toggleCollapse(!props.collapse);
