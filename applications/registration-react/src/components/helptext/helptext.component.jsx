b962c14e35c2caa8783d19768152575f46660fdd
Markko Paas <markkopaas@users.noreply.github.com>
2018-10-18 11:55:20 +0200

    fix 2 markko

--- applications/registration-react/src/components/helptext/helptext.component.jsx
+++ applications/registration-react/src/components/helptext/helptext.component.jsx
@@ -48,7 +48,7 @@ export const Helptext = props => {
         {description && (
           <button
             className="fdk-btn-no-border text-left p-0 ml-1 fdk-reg-helptext-more align-self-start"
-            onClick={e => onToggle(e)}
+span class="pl-smi">             onClick={onToggle}
           >
             <i className={collapseClass} />
             {localization.helptext.more}
