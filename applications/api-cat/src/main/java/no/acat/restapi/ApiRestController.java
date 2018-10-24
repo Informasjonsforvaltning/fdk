806c0d1ede45808138a7e5053120d3623a611e86
Markko Paas <markko.paas@gmail.com>
2018-10-24 16:32:54 +0200

    (api-cat) Use spring jackson in ApiRestController

--- applications/api-cat/src/main/java/no/acat/restapi/ApiRestController.java
+++ applications/api-cat/src/main/java/no/acat/restapi/ApiRestController.java
@@ -1,6 +1,6 @@
 package no.acat.restapi;
 
-import com.google.gson.Gson;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.ApiOperation;
 import no.acat.model.ApiDocument;
 import no.acat.service.ElasticsearchService;
@@ -11,6 +11,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+
 @CrossOrigin
 @RestController
 @RequestMapping(value = "/apis")
@@ -18,15 +20,17 @@ public class ApiRestController {
     private static final Logger logger = LoggerFactory.getLogger(ApiRestController.class);
 
     private ElasticsearchService elasticsearch;
+    private ObjectMapper mapper;
 
     @Autowired
-    public ApiRestController(ElasticsearchService elasticsearchService) {
+    public ApiRestController(ElasticsearchService elasticsearchService, ObjectMapper mapper) {
         this.elasticsearch = elasticsearchService;
+        this.mapper = mapper;
     }
 
     @ApiOperation(value = "Get a specific api", response = ApiDocument.class)
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = "application/json")
-    public ApiDocument getApiDocument(@PathVariable String id) throws NotFoundException {
+    public ApiDocument getApiDocument(@PathVariable String id) throws NotFoundException, IOException {
         logger.info("request for {}", id);
 
         GetResponse getResponse = elasticsearch.getClient().prepareGet("acat", "apidocument", id).get();
@@ -34,7 +38,7 @@ public class ApiRestController {
         if (!getResponse.isExists()) {
             throw new NotFoundException();
         }
-
-        return new Gson().fromJson(getResponse.getSourceAsString(), ApiDocument.class);
+        ApiDocument apiDocument = mapper.readValue(getResponse.getSourceAsString(), ApiDocument.class);
+        return apiDocument;
     }
 }
