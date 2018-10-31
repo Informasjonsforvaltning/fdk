6a3ffa7fe78719470134d89cc0403a0dee560974
Thomas Nøkleby <thomas.nokleby@gmail.com>
2018-10-31 14:52:44 +0100

    Glemte denne

--- applications/concept-cat/src/main/java/no/ccat/service/Controller.java
+++ applications/concept-cat/src/main/java/no/ccat/service/Controller.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import java.net.MalformedURLException;
 
@@ -19,8 +18,6 @@ public class Controller {
 
     static private final Logger logger = LoggerFactory.getLogger(Controller.class);
 
-
-    @PreAuthorize("hasAuthority('INTERNAL_CALL')")
     @CrossOrigin
     @RequestMapping(value = "/subjects",  method = RequestMethod.GET)
     public Subject getRemoteResourceForSubject(@RequestParam String uri) throws MalformedURLException {
