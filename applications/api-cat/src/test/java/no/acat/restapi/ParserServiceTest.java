063ad8e1a289c1e5dec85b848bf8911ba52fdd18
Markko Paas <markko.paas@gmail.com>
2018-11-05 08:59:24 +0100

    (api-cat) Add failing test for invlid additional properties specification

--- applications/api-cat/src/test/java/no/acat/restapi/ParserServiceTest.java
+++ applications/api-cat/src/test/java/no/acat/restapi/ParserServiceTest.java
@@ -6,6 +6,7 @@ import no.acat.service.ParserService;
 import no.acat.utils.Utils;
 import no.dcat.shared.testcategories.UnitTest;
 import org.apache.commons.io.IOUtils;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
@@ -40,5 +41,21 @@ public class ParserServiceTest {
         assertThat(openApi.getInfo().getTitle(), is("Åpne Data fra Enhetsregisteret - API Dokumentasjon"));
         assertThat(openApi.getInfo().getContact().getName(), is("Forenkling og Brukerdialog hos Brønnøysundregistrene"));
     }
+
+    @Test
+    @Ignore
+    public void parse_v3_0_1_as_v3_0_0_SpecWithInvalidAdditionalProperties() throws Throwable {
+        ClassPathResource resource = new ClassPathResource("api-cat-invalid-additionalproperties.json");
+
+        ObjectMapper mapper = Utils.jsonMapper();
+        ParserService parserService = new ParserService(mapper);
+
+        String spec = IOUtils.toString(resource.getInputStream(), "UTF-8");
+
+        OpenAPI openApi = parserService.parse(spec);
+
+        assertThat(openApi.getInfo().getTitle(), is("National API Directory Search API"));
+    }
+
 
 }
