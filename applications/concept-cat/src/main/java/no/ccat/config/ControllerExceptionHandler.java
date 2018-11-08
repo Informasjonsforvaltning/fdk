9f48d668da281e043707ad8fa75431115e779384
Markko Paas <markko.paas@gmail.com>
2018-11-08 10:10:44 +0100

    (concept-cat) Respond 404 on GET /api/concepts/{id} if not found

---
+++ applications/concept-cat/src/main/java/no/ccat/config/ControllerExceptionHandler.java
@@ -0,0 +1,8 @@
+package no.ccat.config;
+
+import no.dcat.webutils.exceptions.GlobalControllerExceptionHandler;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ControllerExceptionHandler extends GlobalControllerExceptionHandler {
+}
