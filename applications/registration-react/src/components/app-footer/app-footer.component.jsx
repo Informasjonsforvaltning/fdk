3bda71d30f64bba0b487e62265d1d67d89e3ed5a
fheitmann <frederic.andre@gmail.com>
2018-11-12 12:09:46 +0100

    registration-react API-411 Footer: blue links on one line and bolding of text  (#933)
    * changed colors, line breaks, and font weight
    
    * changed colors, line breaks, and font weight

--- applications/registration-react/src/components/app-footer/app-footer.component.jsx
+++ applications/registration-react/src/components/app-footer/app-footer.component.jsx
@@ -1,34 +1,33 @@
 import React from 'react';
 import localization from '../../utils/localization';
+import './app-footer.scss';
 
 const Footer = () => (
   <footer className="fdk-footer">
     <div className="container">
       <div className="row">
-        <div className="col-md-3">
+        <div className="col-md-4">
           <p className="fdk-p-footer">
             <a href="https://www.brreg.no/personvernerklaering/">
               {localization.footer.information}
-              <br />
               {localization.footer.privacy}
               <i className="fa fa-external-link fdk-fa-right" />
             </a>
           </p>
         </div>
-        <div className="col-md-6 text-center">
+        <div className="col-md-4 text-center">
           <span className="uu-invisible" aria-hidden="false">
             Felles Datakatalog.
           </span>
           <p className="fdk-p-footer">{localization.footer.information_text}</p>
         </div>
-        <div className="col-md-3 text-right">
+        <div className="col-md-4 text-right">
           <p className="fdk-p-footer">
             <a href="mailto:fellesdatakatalog@brreg.no">
               <span className="uu-invisible" aria-hidden="false">
                 Mailadresse.
               </span>
               {localization.footer.contact}
-              <br />
               {localization.footer.mail}
             </a>
           </p>
