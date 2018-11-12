40347c6cf8ab264a60b1f29b1f3ac90488f07550
David Skogan <david.skogan@gmail.com>
2018-11-12 14:28:44 +0100

    (registration-api) prevent spring from automatically creating endpoints for repositories

--- applications/registration-api/src/main/java/no/dcat/service/ApiRegistrationRepository.java
+++ applications/registration-api/src/main/java/no/dcat/service/ApiRegistrationRepository.java
@@ -4,9 +4,11 @@ import no.dcat.model.ApiRegistration;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Optional;
 
+@RepositoryRestResource(exported = false)
 public interface ApiRegistrationRepository
     extends ElasticsearchRepository<ApiRegistration, String> {
 
