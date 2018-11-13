3f07432b58e4731cc2ad124321d1f9d598d2338d
Markko Paas <markko.paas@gmail.com>
2018-11-13 13:07:23 +0100

    (api-cat) Schedule HarvestAll on server startup.

--- applications/api-cat/src/main/java/no/acat/harvester/HarvestQueue.java
+++ applications/api-cat/src/main/java/no/acat/harvester/HarvestQueue.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.LinkedList;
 
 @Service
@@ -11,6 +12,13 @@ public class HarvestQueue {
     private static final Logger logger = LoggerFactory.getLogger(HarvestQueue.class);
 
     private final LinkedList<String> scheduledTasks = new LinkedList<>();
+
+    @PostConstruct
+    public void init() {
+        String task = HarvestExecutor.HARVEST_ALL;
+        logger.debug("Initial trigger task {}", task);
+        addTask(task);
+    }
 
     public void addTask(String task) {
         synchronized (scheduledTasks) {
