68199dc47895b88bfa861d269921a3bb2e2346f6
Markko Paas <markko.paas@gmail.com>
2018-11-13 16:30:00 +0100

    (shared) Add unit tests to HarvestMetadataUtil.createOrUpdate

---
+++ libraries/shared/src/test/java/no/dcat/shared/HarvestMetadataUtilTest.java
@@ -0,0 +1,51 @@
+package no.dcat.shared;
+
+import no.dcat.shared.testcategories.UnitTest;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import java.util.Date;
+
+@Category(UnitTest.class)
+public class HarvestMetadataUtilTest {
+
+    @Test
+    public void initialize() throws Exception {
+        Date harvestDate = new Date();
+
+        HarvestMetadata metadata =
+            HarvestMetadataUtil.createOrUpdate(null, harvestDate, false);
+        Assert.assertEquals(metadata.getFirstHarvested(),harvestDate);
+        Assert.assertEquals(metadata.getLastHarvested(),harvestDate);
+    }
+
+    @Test
+    public void updateLastHarvested() throws Exception {
+        Date createDate = new Date();
+        Date harvestDate = new Date();
+        harvestDate.setTime(createDate.getTime() + 1000);
+
+        HarvestMetadata oldMetadata =
+            HarvestMetadataUtil.createOrUpdate(null, createDate, false);
+
+        HarvestMetadata metadata =
+            HarvestMetadataUtil.createOrUpdate(oldMetadata, harvestDate, false);
+        Assert.assertEquals(metadata.getFirstHarvested(), createDate);
+        Assert.assertEquals(metadata.getLastHarvested(), harvestDate);
+    }
+
+    @Test
+    public void updateChangesIfChanged() throws Exception {
+        Date createDate = new Date();
+        Date harvestDate = new Date();
+
+        HarvestMetadata oldMetadata =
+            HarvestMetadataUtil.createOrUpdate(null, createDate, false);
+
+        HarvestMetadata metadata =
+            HarvestMetadataUtil.createOrUpdate(oldMetadata, harvestDate, true);
+        Assert.assertEquals(metadata.getChanged().get(0), harvestDate);
+    }
+
+}
