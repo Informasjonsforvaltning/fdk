057e84ccdc120ee67afccd01de73d20191f9d8ca
Markko Paas <markko.paas@gmail.com>
2018-11-15 15:37:31 +0100

    (concept-cat) API-530 Load concepts from url

--- applications/concept-cat/src/main/java/no/ccat/service/ConceptHarvester.java
+++ applications/concept-cat/src/main/java/no/ccat/service/ConceptHarvester.java
@@ -6,11 +6,16 @@ import no.dcat.shared.HarvestMetadataUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.List;
@@ -25,6 +30,9 @@ public class ConceptHarvester {
     private final ConceptDenormalizedRepository conceptDenormalizedRepository;
     private final RDFToModelTransformer rdfToModelTransformer;
 
+    @Value("${application.harvestSourceUri}")
+    private String harvestSourceUri;
+
     @Autowired
     public ConceptHarvester(ConceptDenormalizedRepository conceptDenormalizedRepository, RDFToModelTransformer rdfToModelTransformer) {
         this.conceptDenormalizedRepository = conceptDenormalizedRepository;
@@ -33,10 +41,26 @@ public class ConceptHarvester {
 
     @PostConstruct
     public void harvestFromSource() {
-        logger.info("harvest from source");
+        Reader reader;
 
-        Reader reader = resourceAsReader("jira-example-big.ttl");
+        try {
+            URL url = new URL(harvestSourceUri);
+            logger.info("Start harvest from url: {}", url);
+            URLConnection urlConnection = url.openConnection();
+            urlConnection.setRequestProperty("Accept", "text/turtle");
+
+            InputStream inputStream = urlConnection.getInputStream();
+
+            reader = new InputStreamReader(inputStream);
+        } catch (IOException e) {
+            logger.warn("Downloading concepts from url failed:" + harvestSourceUri);
+            return;
+        }
+
         List<ConceptDenormalized> concepts = rdfToModelTransformer.getConceptsFromStream(reader);
+
+        logger.info("Harvested {} concepts", concepts.size());
+
 
         Date harvestDate = new Date();
         concepts.stream().forEach(concept -> {
