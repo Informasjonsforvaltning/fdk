f096f87bdc941acb239c2bfd0ccc0d0ff75ae36b
Thomas Nøkleby <thomas.nokleby@gmail.com>
2018-11-21 10:28:01 +0100

    (concept-cat)Removed unused code

--- applications/concept-cat/src/main/java/no/ccat/service/ConceptBuilderService.java
+++ applications/concept-cat/src/main/java/no/ccat/service/ConceptBuilderService.java
@@ -1,16 +1,10 @@
 package no.ccat.service;
 
-import com.google.common.collect.ImmutableMap;
-import no.ccat.model.ConceptDenormalized;
-import no.ccat.model.Definition;
-import no.ccat.model.Source;
-import no.dcat.shared.*;
+import no.dcat.shared.Publisher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Date;
 
 @Service
 public class ConceptBuilderService {
@@ -21,47 +15,6 @@ public class ConceptBuilderService {
     @Autowired
     public ConceptBuilderService(PublisherCatClient publisherCatClient) {
         this.publisherCatClient = publisherCatClient;
-    }
-
-    private static Publisher getFakePublisher(String id) {
-        Publisher publisher = new Publisher();
-        publisher.setOrgPath("/ba/be/bi/" + id);
-        publisher.setId("idid");
-        publisher.setPrefLabel(ImmutableMap.of("no", "norsk pub preflabel" + id, "en", "engelsk pub preflabel" + id));
-        return publisher;
-    }
-
-    public ConceptDenormalized create(String id) {
-        ConceptDenormalized concept = new ConceptDenormalized();
-        concept.setId(id);
-        concept.setUri("testuri" + id);
-        concept.setHarvestSourceUri("testharvestsourceuri" + id);
-
-        HarvestMetadata harvest = HarvestMetadataUtil.createOrUpdate(null, new Date(), false);
-        concept.setHarvest(harvest);
-        Publisher publisher = lookupPublisher(id);
-        if (publisher == null) {
-            publisher = getFakePublisher(id);
-        }
-        concept.setPublisher(publisher);
-
-        Definition definition = new Definition();
-        
-        definition.setText(ImmutableMap.of("no", "norsk definitin" + id, "en", "engelsk definition" + id));
-
-        Source source = new Source();
-
-        source.setUri("www.testuri.no");
-
-        source.setPrefLabel(ImmutableMap.of("no", "norsk kildetekst", "en", "engelsk kildetekst"));
-
-        definition.setSource(source);
-
-        concept.setDefinition(definition);
-
-        concept.setPrefLabel(ImmutableMap.of("no", "norsk preflabel" + id, "en", "engelsk preflabel" + id));
-
-        return concept;
     }
 
     Publisher lookupPublisher(String orgNr) {
