66600bddb46e3da4e1ec893eca64f189fae63fd7
Markko Paas <markko.paas@gmail.com>
2018-11-22 01:17:30 +0100

    (concept-cat) Initialize ElasticsearchService with @PostConstruct

--- applications/api-cat/src/test/java/no/acat/service/ElasticsearchServiceTest.java
+++ applications/api-cat/src/test/java/no/acat/service/ElasticsearchServiceTest.java
@@ -1,25 +1,16 @@
 package no.acat.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import no.dcat.shared.testcategories.IntegrationTest;
-import org.junit.Ignore;
+import no.dcat.shared.testcategories.UnitTest;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@ActiveProfiles("unit-integration")
-@Category(IntegrationTest.class)
-@RunWith(SpringRunner.class)
+@Category(UnitTest.class)
 public class ElasticsearchServiceTest {
 
     @Test
-    @Ignore
-    public void check() throws Throwable {
-        ElasticsearchService elasticsearchService = new ElasticsearchService(new ObjectMapper());
-        elasticsearchService.setElasticserchCluster("localhost:9300", "elasticsearch");
-
+    public void constructorTest() {
+        new ElasticsearchService(new ObjectMapper(), "localhost:9300", "elasticsearch");
     }
 
 }
