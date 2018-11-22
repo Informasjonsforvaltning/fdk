237a3fc7f0acbb72c359b8be8b2d116b81d35148
Markko Paas <markko.paas@gmail.com>
2018-11-22 01:18:34 +0100

    (api-cat) API-45 Implement /actuator/health and /actuator/readiness endpoints

---
+++ applications/api-cat/src/main/java/no/acat/health/ReadinessEndPoint.java
@@ -0,0 +1,35 @@
+package no.acat.health;
+
+import no.acat.repository.ApiDocumentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.web.WebEndpointResponse;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.stereotype.Component;
+
+@Component
+@Endpoint(id = "readiness")
+public class ReadinessEndPoint {
+    private ApiDocumentRepository apiDocumentRepository;
+
+    @Autowired
+    public ReadinessEndPoint(ApiDocumentRepository apiDocumentRepository) {
+        this.apiDocumentRepository = apiDocumentRepository;
+    }
+
+    @ReadOperation
+    public WebEndpointResponse<Health> health() {
+        boolean up = false;
+
+        try {
+            long count = this.apiDocumentRepository.getCount();
+            up = count > 0;
+        } catch (Exception e) {
+        }
+
+        return up ?
+            new WebEndpointResponse(Health.up().build(), 200) :
+            new WebEndpointResponse(Health.down().build(), 503);
+    }
+}
