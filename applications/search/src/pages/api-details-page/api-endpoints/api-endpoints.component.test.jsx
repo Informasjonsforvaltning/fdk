c9cd22b64bc7a265140d0a5fccf5ba92af12abac
Markko Paas <markko.paas@gmail.com>
2018-11-23 15:53:15 +0100

    (search) Refactor api-details-page

--- applications/search/src/pages/api-details-page/api-endpoints/api-endpoints.component.test.jsx
+++ applications/search/src/pages/api-details-page/api-endpoints/api-endpoints.component.test.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import _ from 'lodash';
 import { ApiEndpoints } from './api-endpoints.component';
 import apiItem from './__fixtures/apiItem.json';
 
@@ -14,9 +13,9 @@ test('should render api-ApiEndpoints correctly with no props', () => {
 test('should render api-ApiEndpoints correctly', () => {
   defaultProps = {
     name: 'Test',
-    paths: _.get(apiItem, ['openApi', 'paths']),
-    apiSpecUrl: _.get(apiItem, 'apiSpecUrl'),
-    apiDocUrl: _.get(apiItem, 'apiDocUrl')
+    paths: apiItem.openApi && apiItem.openApi.paths,
+    apiSpecUrl: apiItem.apiSpecUrl,
+    apiDocUrl: apiItem.apiDocUrl
   };
   const wrapper = shallow(<ApiEndpoints {...defaultProps} />);
   expect(wrapper).toMatchSnapshot();
