9639aeffa3e63c7e4346e64753cfb0a7ba9934ca
Markko Paas <markko.paas@gmail.com>
2018-11-27 09:30:14 +0100

    (e2e)  API-287 Wait for dependencies before starting tests and update README

---
+++ applications/e2e/wait_for_dependencies.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+script_full_path=$(dirname "$0")
+echo "full path: $script_full_path"
+
+HOST_SEARCH=${HOST_SEARCH:-http://localhost:8080}
+
+dependencies="$HOST_SEARCH/metrics/apis/readiness $HOST_SEARCH/metrics/reference-data/readiness"
+
+echo dependencies: $dependencies
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+source ${__dir}/scripts/wait_for_all.sh $dependencies
