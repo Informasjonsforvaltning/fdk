18c40645766c2ff798ae464b66bbd9bcaa7f52c9
fheitmann <frederic.andre@gmail.com>
2018-11-27 13:46:12 +0100

    search API-634 removed hover effect in compare box

--- applications/search/src/pages/search-page/results-concepts/compare-terms/compare-terms.component.jsx
+++ applications/search/src/pages/search-page/results-concepts/compare-terms/compare-terms.component.jsx
@@ -4,12 +4,14 @@ import PropTypes from 'prop-types';
 import localization from '../../../../lib/localization';
 import { getTranslateText } from '../../../../lib/translateText';
 
+import './compare-terms.scss';
+
 export const CompareTerms = props => {
   const { uri, prefLabel, creator, onDeleteTerm } = props;
   let title = getTranslateText(prefLabel);
   title = title.charAt(0).toUpperCase() + title.substring(1).toLowerCase();
   return (
-    <div className="fdk-container p-4">
+    <div className="fdk-container p-4 fdk-container-compare-terms">
       <div className="d-flex align-items-baseline justify-content-between">
         <h3 className="">{title}</h3>
         <button
