629ec89f0a689e24aedca1dfb329557b9c07162a
Øyvind Isaksen <oyvind.isaksen@acando.no>
2018-11-29 13:42:29 +0100

    (registration-react) add component form-publish

---
+++ applications/registration-react/src/components/form-publish/form-publish.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Field } from 'redux-form';
+
+import { ButtonRegistrationStatus } from './button-registration-status/button-registration-status.component';
+
+export const FormPublish = () => (
+  <form>
+    <Field
+      name="registrationStatus"
+      component={ButtonRegistrationStatus}
+      label="Status"
+    />
+  </form>
+);
