93c34b6d1f3e75f3c513a5585cb58183b7a4aecd
Øyvind Isaksen <oyvind.isaksen@acando.no>
2018-11-29 10:21:37 +0100

    (registration-react) add v1.4.9 designsystemet and import animations.scss, update package-lock
    (cherry picked from commit 7ec789b)

--- applications/registration-react/src/components/app-header/styles.js
+++ applications/registration-react/src/components/app-header/styles.js
@@ -2,4 +2,5 @@ import 'designsystemet/fdk-designsystem-bootstrap4/scss/helper.scss';
 import 'designsystemet/fdk-designsystem-bootstrap4/scss/register.scss';
 import 'designsystemet/fdk-designsystem-bootstrap4/scss/typo.scss';
 import 'designsystemet/fdk-designsystem-bootstrap4/scss/common.scss';
+import 'designsystemet/fdk-designsystem-bootstrap4/scss/animations.scss';
 import 'font-awesome/scss/font-awesome.scss';
