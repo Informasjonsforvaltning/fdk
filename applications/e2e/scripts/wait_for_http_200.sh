56834505217b3fb372f7571eb11452a75a9921f5
Markko Paas <markko.paas@gmail.com>
2018-11-30 12:37:59 +0100

    fiks

--- applications/e2e/scripts/wait_for_http_200.sh
+++ applications/e2e/scripts/wait_for_http_200.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 url=($1)
-echo "Testing url" $url
+echo "Waiting 200 from " $url
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' $url)" != "200" ]];
     do sleep 5;
-    echo "Retrying url" $url;
+    echo "Retrying " $url;
 done
 echo "Success 200 from " $url
