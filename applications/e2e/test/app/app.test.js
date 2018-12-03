0f359a509707b9ba45aeb157a0ca7698b0c4b029
Markko Paas <markko.paas@gmail.com>
2018-12-03 15:51:17 +0100

    (e2e) Tests for search main page

--- applications/e2e/test/app/app.test.js
+++ applications/e2e/test/app/app.test.js
@@ -1,4 +1,3 @@
-import delay from 'delay';
 import * as browser from "../../lib/browser";
 import { config } from "../../config";
 
@@ -6,19 +5,47 @@ afterAll(() => {
   browser.close();// we might not want to close the browser between each suite
 });
 
-describe('App', () => {
+describe('SUITE: Main page', () => {
+  let page;
+  beforeAll(async () => {
+    page = await browser.getNewBrowserPage();
+  });
 
-  test('search page opens', async () => {
-    const page = await browser.getNewBrowserPage();
-    await page.goto(config.searchHost);
+  describe('GIVEN: Address is bar is blank', () => {
+    describe('WHEN: Navigate to the search address', () => {
+        beforeAll(async () => {
+          const datasetsContentSelector = '#content[data-test-id="datasets"]';
 
-    // Search page component now waits for resolving API request before rendering.
-    // If some other solution is implemented with initial rendering, delay can be removed
-    await delay(10000);
+          await page.goto(config.searchHost);
+          await page.waitForSelector(datasetsContentSelector);
+        });
 
-    await expect(page.title()).resolves.toBe('Felles datakatalog');
-    await expect(page.$('input[name=searchBox]')).resolves.toBeTruthy();
-    await expect(page.$('#content')).resolves.toBeTruthy();
-  }, 20000);
+        test('Window has title \'Felles datakatalog\'', async () =>
+          expect(page.title()).resolves.toBe('Felles datakatalog')
+        );
 
+        test('Search box and button are visible', async () =>
+          expect(page.$('input[name=searchBox]')).resolves.toBeTruthy()
+        );
+
+        test('Concepts tab is active', async () => {
+          const activeTabSelector = '.search-results-tabs .li-active';
+
+          const activeTabText = await page.$eval(activeTabSelector, el => el.innerText);
+          expect(activeTabText).toContain("Datasett"); // default language is nb
+        });
+
+        test('Api tab label shows number larger than 3', async () => {
+          const apiTabSelector = '.search-results-tabs a[href="/apis"]';
+
+          const apiTab = await page.$(apiTabSelector);
+          expect(apiTab).toBeTruthy();
+
+          const label = await page.$eval(apiTabSelector, el => el.innerText);
+          const count = parseInt(label.match(/\d+/g)[0]);
+          expect(count).toBeGreaterThan(5);
+        });
+      }
+    );
+  });
 });
