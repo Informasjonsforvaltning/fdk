6ca9deb52bbf9a69ba8c6d487ebce43ba9b09bd9
Øyvind Isaksen <oyvind.isaksen@acando.no>
2018-12-07 10:31:33 +0100

    (registration-react) API-702 footer bottom space

--- applications/registration-react/src/components/app-footer/app-footer.component.jsx
+++ applications/registration-react/src/components/app-footer/app-footer.component.jsx
@@ -1,40 +1,71 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import _ from 'lodash';
+
 import localization from '../../utils/localization';
 import './app-footer.scss';
 
-const Footer = () => (
-  <footer className="fdk-footer">
-    <div className="container">
-      <div className="row">
-        <div className="col-md-4">
-          <p className="fdk-p-footer">
-            <a href="https://www.brreg.no/personvernerklaering/">
-              {localization.footer.information}
-              {localization.footer.privacy}
-              <i className="fa fa-external-link fdk-fa-right" />
-            </a>
-          </p>
-        </div>
-        <div className="col-md-4 text-center">
-          <span className="uu-invisible" aria-hidden="false">
-            Felles Datakatalog.
-          </span>
-          <p className="fdk-p-footer">{localization.footer.information_text}</p>
-        </div>
-        <div className="col-md-4 text-right">
-          <p className="fdk-p-footer">
-            <a href="mailto:fellesdatakatalog@brreg.no">
-              <span className="uu-invisible" aria-hidden="false">
-                Mailadresse.
-              </span>
-              {localization.footer.contact}
-              {localization.footer.mail}
-            </a>
-          </p>
+export const Footer = props => {
+  const { location } = props;
+  let isRegistrationPage = false;
+
+  // check if matches dataset registration page or api registration page
+  if (
+    /^\/catalogs\/[0-9a-z]+\/[apis\\datasets]+\/[0-9a-z-]+\/?$/.test(
+      _.get(location, 'pathname')
+    )
+  ) {
+    isRegistrationPage = true;
+  }
+
+  return (
+    <footer
+      className="fdk-footer"
+      style={{ marginBottom: isRegistrationPage ? '5em' : '0' }}
+    >
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <p className="fdk-p-footer">
+              <a href="https://www.brreg.no/personvernerklaering/">
+                {localization.footer.information}
+                {localization.footer.privacy}
+                <i className="fa fa-external-link fdk-fa-right" />
+              </a>
+            </p>
+          </div>
+          <div className="col-md-4 text-center">
+            <span className="uu-invisible" aria-hidden="false">
+              Felles Datakatalog.
+            </span>
+            <p className="fdk-p-footer">
+              {localization.footer.information_text}
+            </p>
+          </div>
+          <div className="col-md-4 text-right">
+            <p className="fdk-p-footer">
+              <a href="mailto:fellesdatakatalog@brreg.no">
+                <span className="uu-invisible" aria-hidden="false">
+                  Mailadresse.
+                </span>
+                {localization.footer.contact}
+                {localization.footer.mail}
+              </a>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  </footer>
-);
+    </footer>
+  );
+};
 
-export default Footer;
+Footer.defaultProps = {
+  location: null
+};
+
+Footer.propTypes = {
+  location: PropTypes.object
+};
+
+export default withRouter(Footer);
--- applications/registration-react/src/components/app-footer/app-footer.test.jsx
+++ applications/registration-react/src/components/app-footer/app-footer.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Footer from './app-footer.component';
+import { Footer } from './app-footer.component';
 
 test('should render Footer correctly', () => {
   const wrapper = shallow(<Footer />);
