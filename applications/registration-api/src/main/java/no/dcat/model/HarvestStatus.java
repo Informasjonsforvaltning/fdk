25d3675bfce0e0367f9cffbe60d89098a54a8bae
Markko Paas <markko.paas@gmail.com>
2018-12-14 14:04:13 +0100

    (registration-api) API-609 Store harvestStatus on api catalog and on apiRegistration

---
+++ applications/registration-api/src/main/java/no/dcat/model/HarvestStatus.java
@@ -0,0 +1,17 @@
+package no.dcat.model;
+
+import lombok.Data;
+
+@Data
+public class HarvestStatus {
+    private final Boolean success;
+    private final String errorMessage;
+
+    public static HarvestStatus Success() {
+        return new HarvestStatus(true, null);
+    }
+
+    public static HarvestStatus Error(String message) {
+        return new HarvestStatus(false, message);
+    }
+}
