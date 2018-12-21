67ad98c53b9e073c3a607a8bc2d743ddcc348e9c
Øyvind Isaksen <oyvind.isaksen@acando.no>
2018-12-21 12:55:35 +0100

    (search) apis to redux
    (cherry picked from commit 31da853)
    
    (cherry picked from commit f902656)

--- applications/search/src/pages/search-page/resolved-search-page.jsx
+++ applications/search/src/pages/search-page/resolved-search-page.jsx
@@ -2,14 +2,11 @@ import _ from 'lodash';
 import { resolve } from 'react-resolver';
 import { SearchPageWithState } from './search-page';
 import { getConcepts } from '../../api/get-concepts';
-import { getApis } from '../../api/get-apis';
 
 const memoizedGetConcepts = _.memoize(getConcepts);
-const memoizedGetApis = _.memoize(getApis);
 
 const mapProps = {
-  conceptItems: props => memoizedGetConcepts(props.location.search),
-  apiItems: props => memoizedGetApis(props.location.search)
+  conceptItems: props => memoizedGetConcepts(props.location.search)
 };
 
 export const ResolvedSearchPage = resolve(mapProps)(SearchPageWithState);
