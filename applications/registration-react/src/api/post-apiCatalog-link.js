0c37f135c0d0da408e2d29a06df696cafd7cf387
Markko Paas <markko.paas@gmail.com>
2019-01-02 12:00:03 +0100

    (registration-api) API-752 Use REST conventions in apicatalog api

--- applications/registration-react/src/api/post-apiCatalog-link.js
+++ applications/registration-react/src/api/post-apiCatalog-link.js
@@ -1,9 +1,9 @@
 import { post } from 'axios';
 
 export const postApiCatalogLink = (catalogId, importUrl) => {
-  const url = `/apicatalogs/save`;
+  const url = `/catalogs/${catalogId}/apicatalog`;
 
-  return post(url, { orgNo: catalogId, harvestSourceUri: importUrl }).then(
+  return post(url, { harvestSourceUri: importUrl }).then(
     response => response.data
   );
 };
