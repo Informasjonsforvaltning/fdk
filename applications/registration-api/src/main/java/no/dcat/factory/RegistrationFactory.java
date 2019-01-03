0a870adbfc11dfdb0d1766a9758037d02c82d282
Markko Paas <markko.paas@gmail.com>
2019-01-03 12:09:16 +0100

    refactor (registration-api): Remove unused dataset registration factory methods.

--- applications/registration-api/src/main/java/no/dcat/factory/RegistrationFactory.java
+++ applications/registration-api/src/main/java/no/dcat/factory/RegistrationFactory.java
@@ -1,8 +1,6 @@
 package no.dcat.factory;
 
-import no.dcat.shared.Contact;
 import no.dcat.model.Dataset;
-import no.dcat.shared.Distribution;
 
 import java.util.UUID;
 
@@ -18,37 +16,8 @@ public class RegistrationFactory {
         return dataset;
     }
 
-    public static Contact createContact(String catalogId) {
-        Contact contact = new Contact();
-        contact.setId(UUID.randomUUID().toString());
-        contact.setUri(getCatalogUri(catalogId) + "/contacts/" + contact.getId());
-
-        return contact;
-    }
-
-    public static Distribution createDistribution(String catalogId, String datasetId) {
-        Distribution distribution = new Distribution();
-        distribution.setId(UUID.randomUUID().toString());
-        distribution.setUri(getDatasetUri(catalogId, datasetId) + "/distributions/" + distribution.getId());
-
-        return distribution;
-    }
-
     public static String getCatalogUri(String catalogId) {
         return "http://brreg.no" + "/catalogs/" + catalogId;
     }
-
-    public static String getDatasetUri(String catalogId, String datasetId) {
-        return getCatalogUri(catalogId) + "/datasets/" + datasetId;
-    }
-
-    public static String getContactUri(String catalogId, String contactId) {
-        return getCatalogUri(catalogId) + "/contacts/" + contactId;
-    }
-
-    public static String getDistributionUri(Distribution distribution, Dataset parentDataset) {
-        return parentDataset.getUri() + "/distributions/" + distribution.getId();
-    }
-
 
 }
--- applications/registration-api/src/test/java/no/dcat/factory/RegistrationFactoryTest.java
+++ applications/registration-api/src/test/java/no/dcat/factory/RegistrationFactoryTest.java
@@ -1,15 +1,13 @@
 package no.dcat.factory;
 
 import no.dcat.model.Dataset;
-import no.dcat.shared.Contact;
-import no.dcat.shared.Distribution;
 import no.dcat.shared.testcategories.UnitTest;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+
 /**
  * Created by bjg on 02.05.2018.
  */
@@ -17,44 +15,11 @@ import static org.junit.Assert.assertThat;
 public class RegistrationFactoryTest {
 
     @Test
-    public void datasetCreatedWithCorrectUri() throws Exception {
+    public void datasetCreatedWithCorrectUri() {
         String catalogId = "12345";
         Dataset result = RegistrationFactory.createDataset(catalogId);
 
         assertThat(result.getUri(), containsString("http://brreg.no/catalogs/" + catalogId + "/datasets/"));
 
-    }
-
-    @Test
-    public void contactCreatedWithCorrectUri() throws Exception {
-        String catalogId = "23456";
-        Contact result = RegistrationFactory.createContact(catalogId);
-
-        assertThat(result.getUri(), containsString("http://brreg.no/catalogs/" + catalogId + "/contacts/"));
-    }
-
-    @Test
-    public void distributionCreatedWithCorrectUri() throws Exception {
-        String catalogId = "98765";
-        String datasetId = "12345";
-        Distribution result = RegistrationFactory.createDistribution(catalogId, datasetId);
-
-        assertThat("distribution uri is correct", result.getUri(), containsString("http://brreg.no/catalogs/" + catalogId
-                + "/datasets/" + datasetId + "/distributions/"));
-    }
-
-    @Test
-    public void getContactUriIsCorrect() throws Exception {
-        String result = RegistrationFactory.getContactUri("111", "222");
-        assertThat(result, is("http://brreg.no/catalogs/111/contacts/222"));
-    }
-
-    @Test
-    public void getDistributionUriIsCorrect() throws Exception {
-        Dataset dataset = RegistrationFactory.createDataset("111");
-        Distribution distribution = RegistrationFactory.createDistribution("111", "222");
-        String result = RegistrationFactory.getDistributionUri(distribution, dataset);
-        assertThat(result, is("http://brreg.no/catalogs/111/datasets/"
-                + dataset.getId() +"/distributions/" + distribution.getId()));
     }
 }
