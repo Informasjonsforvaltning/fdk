c2f7a16df91e366c76952e4496aa7026c55c771b
Markko Paas <markko.paas@gmail.com>
2019-01-15 11:38:55 +0100

    feat (api-cat) Convert endpoint includes conversion to ApiSpecification

--- libraries/api-cat-bindings/src/main/java/no/fdk/acat/bindings/ConvertResponse.java
+++ libraries/api-cat-bindings/src/main/java/no/fdk/acat/bindings/ConvertResponse.java
@@ -2,6 +2,7 @@ package no.fdk.acat.bindings;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import lombok.Data;
+import no.fdk.acat.common.model.apispecification.ApiSpecification;
 
 import java.util.List;
 
@@ -9,6 +10,8 @@ import java.util.List;
 public class ConvertResponse {
 
     OpenAPI openApi;
+
+    ApiSpecification apiSpecification;
 
     List<String> messages;
 }
