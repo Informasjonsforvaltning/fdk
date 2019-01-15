f4de095dd449f1fac64f980e4c5ac74bdacd6834
Thomas Nøkleby <thomas.nokleby@gmail.com>
2019-01-15 08:16:39 +0100

    feature (informationmodel-cat) Initial (empty) version of im-cat. Contains module and model objects.

---
+++ applications/informationmodel-cat/src/main/java/no/dmcat/model/InformationModel.java
@@ -0,0 +1,38 @@
+package no.dmcat.model;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import no.ccat.common.model.Concept;
+import no.dcat.shared.HarvestMetadata;
+import no.dcat.shared.Publisher;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Mapping;
+import org.springframework.data.elasticsearch.annotations.Setting;
+import org.springframework.hateoas.core.Relation;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Relation(value = "informationmoden", collectionRelation = "informationmodels")
+@Document(indexName = "imcat", type = "informationomdel")
+@Setting(settingPath = "imcat.settings.json")
+@Mapping(mappingPath = "informationmodel.mapping.json")
+public class InformationModel extends Concept {
+
+    @ApiModelProperty("The publisher of the information model")
+    private Publisher publisher;
+
+    @ApiModelProperty("The source where the record was harvested from")
+    private String harvestSourceUri;
+
+    @ApiModelProperty("Information about when the model was first and last harvested by the system")
+    private HarvestMetadata harvest;
+
+    @ApiModelProperty("The title of the information model")
+    private String title;
+
+    @ApiModelProperty("The model itself, expressed in JSON-SCHEMA")
+    private String schema;//TODO: Find better name
+
+
+}
