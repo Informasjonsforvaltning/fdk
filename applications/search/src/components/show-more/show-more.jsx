9d856d2a13129906c348be27a4e8fe1b1a686fe1
Markko Paas <markko.paas@gmail.com>
2019-01-17 18:15:36 +0100

    refactor (search): Convert ShowMore component to stateless

--- applications/search/src/components/show-more/show-more.jsx
+++ applications/search/src/components/show-more/show-more.jsx
@@ -1,81 +1,89 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import _ from 'lodash';
+import { withStateHandlers } from 'recompose';
 
 import localization from '../../lib/localization';
 import './show-more.scss';
 
-export class ShowMore extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showAll: false
-    };
-    this.toggleShowAll = this.toggleShowAll.bind(this);
-  }
-
-  toggleShowAll() {
-    this.setState({ showAll: !this.state.showAll });
-  }
-
-  _renderContent() {
+export const ShowMorePure = ({
+  showMoreButtonText,
+  showLessButtonText,
+  toggleShowAll,
+  showAll,
+  contentHtml,
+  label
+}) => {
+  function _renderContent() {
     return (
       <span className="fdk-ingress">
-        {this.props.label && <strong>{this.props.label}: </strong>}
+        {label && <strong>{label}: </strong>}
         <span
           dangerouslySetInnerHTML={{
-            __html: this.props.contentHtml
+            __html: contentHtml
           }}
         />
       </span>
     );
   }
+  const textLength = contentHtml.length;
+  const COLLAPSE_THRESHOLD = 300;
+  let content;
 
-  render() {
-    const textLength = this.props.contentHtml.length;
-    const COLLAPSE_THRESHOLD = 300;
-    let content;
-
-    if (textLength < COLLAPSE_THRESHOLD) {
-      content = this._renderContent();
-    } else if (!this.state.showAll) {
-      content = (
-        <React.Fragment>
-          <div className="show-more__cropped-box">{this._renderContent()}</div>
-          <button className="fdk-button-small" onClick={this.toggleShowAll}>
-            <i className="fa mr-2 fa-angle-double-down" />
-            <span>{this.props.showMoreButtonText}</span>
-          </button>
-        </React.Fragment>
-      );
-    } else if (this.state.showAll) {
-      content = (
-        <React.Fragment>
-          <div>{this._renderContent()}</div>
-          <button className="fdk-button-small" onClick={this.toggleShowAll}>
-            <i className="fa mr-2 fa-angle-double-up" />
-            <span>{this.props.showLessButtonText}</span>
-          </button>
-        </React.Fragment>
-      );
-    }
-    return (
-      <div className="mb-5" name={this.props.label}>
-        {content}
-      </div>
+  if (textLength < COLLAPSE_THRESHOLD) {
+    content = _renderContent();
+  } else if (!showAll) {
+    content = (
+      <React.Fragment>
+        <div className="show-more__cropped-box">{_renderContent()}</div>
+        <button className="fdk-button-small" onClick={toggleShowAll}>
+          <i className="fa mr-2 fa-angle-double-down" />
+          <span>{showMoreButtonText}</span>
+        </button>
+      </React.Fragment>
+    );
+  } else if (showAll) {
+    content = (
+      <React.Fragment>
+        <div>{_renderContent()}</div>
+        <button className="fdk-button-small" onClick={toggleShowAll}>
+          <i className="fa mr-2 fa-angle-double-up" />
+          <span>{showLessButtonText}</span>
+        </button>
+      </React.Fragment>
     );
   }
-}
+  return (
+    <div className="mb-5" name={label}>
+      {content}
+    </div>
+  );
+};
 
-ShowMore.defaultProps = {
+ShowMorePure.defaultProps = {
   label: '',
   contentHtml: '',
   showMoreButtonText: localization.showLess,
-  showLessButtonText: localization.showLess
+  showLessButtonText: localization.showLess,
+  toggleShowAll: _.noop,
+  showAll: false
 };
 
-ShowMore.propTypes = {
+ShowMorePure.propTypes = {
   label: PropTypes.string,
   contentHtml: PropTypes.string,
   showMoreButtonText: PropTypes.string,
-  showLessButtonText: PropTypes.string
+  showLessButtonText: PropTypes.string,
+  toggleShowAll: PropTypes.func,
+  showAll: PropTypes.bool
 };
+
+const enhance = withStateHandlers(
+  ({ initialShowAll = false }) => ({ showAll: initialShowAll }),
+  {
+    toggleShowAll: ({ showAll }) => () => ({ showAll: !showAll })
+  }
+);
+
+export const ShowMoreWithState = enhance(ShowMorePure);
+export const ShowMore = ShowMoreWithState;
