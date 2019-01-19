6475b0e9bb8c80087ab53d53627f422ba3a4024a
Markko Paas <markko.paas@gmail.com>
2019-01-19 17:36:50 +0100

    chore (api-cat): Remove OpenApi response from converter

--- libraries/api-cat-bindings/src/main/java/no/fdk/acat/bindings/ConvertResponse.java
+++ libraries/api-cat-bindings/src/main/java/no/fdk/acat/bindings/ConvertResponse.java
@@ -1,6 +1,5 @@
 package no.fdk.acat.bindings;
 
-import io.swagger.v3.oas.models.OpenAPI;
 import lombok.Data;
 import no.fdk.acat.common.model.apispecification.ApiSpecification;
 
@@ -8,8 +7,6 @@ import java.util.List;
 
 @Data
 public class ConvertResponse {
-
-    OpenAPI openApi;
 
     ApiSpecification apiSpecification;
 
