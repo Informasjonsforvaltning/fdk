eab49a7bd55f70c5abaa6cd779eed058108eed92
Thomas Nøkleby <thomas.nokleby@gmail.com>
2019-01-24 20:40:01 +0100

    feature (informationmodel-cat) Now has a queue and a trigger for harvesting

---
+++ applications/informationmodel-cat/src/main/java/no/fdk/imcat/harvester/HarvestQueue.java
@@ -0,0 +1,40 @@
+package no.fdk.imcat.harvester;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.LinkedList;
+import java.util.Queue;
+
+@Service
+public class HarvestQueue {
+    private static final Logger logger = LoggerFactory.getLogger(HarvestQueue.class);
+
+    private final Queue<String> scheduledTasks = new LinkedList<>();
+
+    @PostConstruct
+    public void init() {
+        String task = HarvestExecutor.HARVEST_ALL;
+        logger.debug("Initial trigger task {}", task);
+        addTask(task);
+    }
+
+    public void addTask(String task) {
+        synchronized (scheduledTasks) {
+            if (scheduledTasks.contains(task)) {
+                logger.debug("Task already exists in queue, skipping: {}", task);
+                return;
+            }
+            scheduledTasks.add(task);
+            logger.debug("Task added to queue: {}", task);
+        }
+    }
+
+    public String poll() {
+        synchronized (scheduledTasks) {
+            return scheduledTasks.poll();
+        }
+    }
+}
