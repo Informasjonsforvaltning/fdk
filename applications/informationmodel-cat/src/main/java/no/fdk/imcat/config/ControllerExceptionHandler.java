7cabe6d17881e415ab6dffaaa4e8a5136e9cc713
Markko Paas <markko.paas@gmail.com>
2019-01-28 11:33:49 +0100

    chore: (informationomodel-cat) Add standard controller exception handler

---
+++ applications/informationmodel-cat/src/main/java/no/fdk/imcat/config/ControllerExceptionHandler.java
@@ -0,0 +1,8 @@
+package no.fdk.imcat.config;
+
+import no.dcat.webutils.exceptions.GlobalControllerExceptionHandler;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ControllerExceptionHandler extends GlobalControllerExceptionHandler {
+}
