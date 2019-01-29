3e8053efbba8d39a3ff0f6b645960d9348e165e4
Øyvind Isaksen <oyvind.isaksen@acando.no>
2019-01-29 13:53:34 +0100

    feat (search) add styling to information model tabs

--- applications/search/src/pages/informationmodel-details-page/tabs/tabs.component.jsx
+++ applications/search/src/pages/informationmodel-details-page/tabs/tabs.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { TabContent, TabPane, Nav, NavItem, NavLink } from 'reactstrap';
 import classnames from 'classnames';
+import './tabs.component.scss';
 
 const renderTabContent = (tabContent, activeTab) => {
   if (!tabContent) {
