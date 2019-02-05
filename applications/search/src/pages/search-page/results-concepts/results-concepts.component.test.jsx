9e0e3b261d8fc8836122c98da3d66e9ac6054c63
Markko Paas <markko.paas@gmail.com>
2019-02-05 17:15:21 +0100

    chore (search) Clean up results-concepts component tests

--- applications/search/src/pages/search-page/results-concepts/results-concepts.component.test.jsx
+++ applications/search/src/pages/search-page/results-concepts/results-concepts.component.test.jsx
@@ -3,67 +3,20 @@ import { shallow } from 'enzyme';
 import _ from 'lodash';
 import { ResultsConceptsPure } from './results-concepts.component';
 import concepts from '../../../../test/fixtures/concepts';
-import { HITS_PER_PAGE } from '../../../constants/constants';
 import { normalizeAggregations } from '../../../lib/normalizeAggregations';
 
-let onClearFilters;
-let onPageChange;
-let onFilterPublisherHierarchy;
-let closeFilterModal;
-let setConceptSort;
-let onSortByLastModified;
-let onSortByScore;
-let defaultProps;
-let wrapper;
-
-beforeEach(() => {
-  onClearFilters = jest.fn();
-  onPageChange = jest.fn();
-  onFilterPublisherHierarchy = jest.fn();
-  closeFilterModal = jest.fn();
-  setConceptSort = jest.fn();
-  onSortByLastModified = jest.fn();
-  onSortByScore = jest.fn();
-
-  defaultProps = {
-    conceptItems: concepts,
-    onClearFilters,
-    onPageChange,
-    onFilterPublisherHierarchy,
-    searchQuery: {},
-    hitsPerPage: HITS_PER_PAGE,
-    showFilterModal: false,
-    closeFilterModal,
-    showClearFilterButton: false,
-    publisherArray: null,
-    publishers: null,
-    setConceptSort,
-    onSortByLastModified,
-    onSortByScore
-  };
-  wrapper = shallow(<ResultsConceptsPure {...defaultProps} />);
-});
-
 test('should render ResultsConcepts correctly with minimum of props', () => {
-  const minWrapper = shallow(
-    <ResultsConceptsPure
-      setConceptSort={setConceptSort}
-      onSortByLastModified={onSortByLastModified}
-      onSortByScore={onSortByScore}
-    />
-  );
-  expect(minWrapper).toMatchSnapshot();
-});
-
-test('should render ResultsConcepts correctly with props', () => {
-  expect(wrapper).toMatchSnapshot();
+  const result = shallow(<ResultsConceptsPure />);
+  expect(result).toMatchSnapshot();
 });
 
 test('should render ResultsConcepts correctly with hits', () => {
-  wrapper.setProps({
-    conceptItems: _.get(concepts, ['_embedded', 'concepts']),
+  const props = {
+    conceptItems: _.get(concepts, ['hits', 'hits']),
     conceptAggregations: _.get(normalizeAggregations(concepts), 'aggregations'),
-    conceptTotal: _.get(concepts, ['page', 'totalElements'])
-  });
-  expect(wrapper).toMatchSnapshot();
+    conceptTotal: _.get(concepts, ['hits', 'total'])
+  };
+
+  const result = shallow(<ResultsConceptsPure {...props} />);
+  expect(result).toMatchSnapshot();
 });
