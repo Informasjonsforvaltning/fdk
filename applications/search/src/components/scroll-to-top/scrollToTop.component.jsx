1173b0b8462c7ba8ca6632a0140779d5b551e897
Markko Paas <markko.paas@gmail.com>
2019-02-05 15:53:57 +0100

    fix (search) API-873 Scroll to top on address change

--- applications/search/src/components/scroll-to-top/scrollToTop.component.jsx
+++ applications/search/src/components/scroll-to-top/scrollToTop.component.jsx
@@ -3,7 +3,10 @@ import { withRouter } from 'react-router-dom';
 
 class ScrollToTop extends React.Component {
   componentDidUpdate(prevProps) {
-    if (this.props.location.pathname !== prevProps.location.pathname) {
+    if (
+      this.props.location.pathname !== prevProps.location.pathname ||
+      this.props.location.search !== prevProps.location.search
+    ) {
       window.scrollTo(0, 0);
     }
   }
