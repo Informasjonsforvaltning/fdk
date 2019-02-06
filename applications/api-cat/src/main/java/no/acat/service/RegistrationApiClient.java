55bcb55b6884f5f89e619083a9e277588123805c
Markko Paas <markko.paas@gmail.com>
2019-02-06 13:27:20 +0100

    refactor (registration-api-client) Rename RegistrationApiClient to RegistrationApiBindings

--- applications/api-cat/src/main/java/no/acat/service/RegistrationApiClient.java
+++ applications/api-cat/src/main/java/no/acat/service/RegistrationApiClient.java
@@ -1,12 +1,13 @@
 package no.acat.service;
 
+import no.fdk.registration.bindings.RegistrationApiBindings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 
 @Service
-public class RegistrationApiClient extends no.dcat.client.registrationapi.RegistrationApiClient {
+public class RegistrationApiClient extends RegistrationApiBindings {
     @Autowired
     private Environment env;
 
