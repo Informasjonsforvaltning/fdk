295287746efd31ac04e27c39ef6f7816afe42e38
Øyvind Isaksen <oyvind.isaksen@acando.no>
2019-02-08 10:37:06 +0100

    feat (registration-react) add API status select field

---
+++ applications/registration-react/src/pages/api-registration-page/form-apiStatus/configured-form-apiStatus.jsx
@@ -0,0 +1,12 @@
+import { reduxForm } from 'redux-form';
+import { FormApiStatus } from './form-apiStatus.component';
+import { asyncValidate } from '../async-patch/async-patch';
+import shouldAsyncValidate from '../../../utils/shouldAsyncValidate';
+
+const config = {
+  form: 'apiStatus',
+  shouldAsyncValidate,
+  asyncValidate
+};
+
+export const ConfiguredFormApiStatus = reduxForm(config)(FormApiStatus);
