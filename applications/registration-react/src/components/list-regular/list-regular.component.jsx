e27cd17ac83af3a2a11fd7b195bc724afccf914c
Øyvind Isaksen <oyvind.isaksen@acando.no>
2019-02-11 14:16:09 +0100

    feat (registration-react) add tag as prop to component
    (cherry picked from commit 453bbd5)

--- applications/registration-react/src/components/list-regular/list-regular.component.jsx
+++ applications/registration-react/src/components/list-regular/list-regular.component.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import './list-regular.scss';
 
 export const ListRegular = props => {
-  const { title, bottomMargin } = props;
+  const { title, subTitle, bottomMargin, tag: Tag } = props;
   return (
     <section
       className={`list-regular ${bottomMargin ? 'mb-5' : ''}`}
@@ -11,7 +11,8 @@ export const ListRegular = props => {
     >
       {title && (
         <div className="list-regular--item">
-          <h3>{title}</h3>
+          <Tag>{title}</Tag>
+          {subTitle && <span>{subTitle}</span>}
         </div>
       )}
       {props.children}
@@ -21,12 +22,16 @@ export const ListRegular = props => {
 
 ListRegular.defaultProps = {
   title: null,
+  subTitle: null,
   children: null,
-  bottomMargin: true
+  bottomMargin: true,
+  tag: 'h2'
 };
 
 ListRegular.propTypes = {
   title: PropTypes.string,
+  subTitle: PropTypes.string,
   children: PropTypes.oneOfType([PropTypes.array, PropTypes.object]),
-  bottomMargin: PropTypes.bool
+  bottomMargin: PropTypes.bool,
+  tag: PropTypes.string
 };
