9fa0fb70f52ca6dcef52060e08866ef914adafd5
Øyvind Isaksen <oyvind.isaksen@acando.no>
2019-02-14 14:07:02 +0100

    chor (registration-react) field-select accept string or object as value
    (cherry picked from commit f61c22c)

--- applications/registration-react/src/components/field-select/field-select.component.jsx
+++ applications/registration-react/src/components/field-select/field-select.component.jsx
@@ -3,27 +3,44 @@ import PropTypes from 'prop-types';
 import Select from 'react-select';
 import 'react-select/dist/react-select.css';
 
-const handleChange = (props, selectedURI) => {
+const handleChange = (props, selectedValue, valueKey, saveObject) => {
   const { input, items } = props;
-  if (!selectedURI) {
-    input.onChange({
-      uri: null,
-      prefLabel: {
-        no: null
-      }
-    });
+  let valueToBeSaved;
+
+  // if delete value
+  if (!selectedValue) {
+    if (saveObject) {
+      valueToBeSaved = {
+        uri: null,
+        prefLabel: {
+          no: null
+        }
+      };
+    } else {
+      valueToBeSaved = '';
+    }
+  } else if (saveObject) {
+    const selectedFrequencyItem = items.find(
+      item => item[valueKey] === selectedValue
+    );
+    valueToBeSaved = selectedFrequencyItem;
   } else {
-    const selectedFrequencyItem = items.find(item => item.uri === selectedURI);
-    input.onChange(selectedFrequencyItem);
+    valueToBeSaved = selectedValue;
   }
+  input.onChange(valueToBeSaved);
 };
 
-const SelectField = props => {
-  const {
-    input,
-    meta: { touched, error, warning },
-    items
-  } = props;
+const SelectField = ({
+  input,
+  meta: { touched, error, warning },
+  items,
+  valueKey = 'uri',
+  saveObject
+}) => {
+  let referencedInputObject;
+  if (typeof input.value === 'string' && items) {
+    referencedInputObject = items.find(item => item[valueKey] === input.value);
+  }
   return (
     <div className="pl-2 mt-3">
       <Select
@@ -33,10 +50,16 @@ const SelectField = props => {
         clearable
         name="selected-state"
         disabled={false}
-        value={input.value.uri}
-        valueKey="uri"
+        value={
+          referencedInputObject
+            ? referencedInputObject[valueKey]
+            : input.value[valueKey]
+        }
+        valueKey={valueKey}
         labelKey="prefLabel_no"
-        onChange={selectedURI => handleChange(props, selectedURI)}
+        onChange={selectedValue =>
+          handleChange({ input, items }, selectedValue, valueKey, saveObject)
+        }
         rtl={false}
         searchable
         placeholder="Velg"
@@ -53,13 +76,17 @@ const SelectField = props => {
 SelectField.defaultProps = {
   input: null,
   meta: null,
-  items: null
+  items: null,
+  valueKey: 'uri',
+  saveObject: true
 };
 
 SelectField.propTypes = {
   input: PropTypes.object,
   meta: PropTypes.object,
-  items: PropTypes.array
+  items: PropTypes.array,
+  valueKey: PropTypes.string,
+  saveObject: PropTypes.bool
 };
 
 export default SelectField;
