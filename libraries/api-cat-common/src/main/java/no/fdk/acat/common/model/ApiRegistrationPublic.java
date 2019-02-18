9d9550973788db9311e19b68925febc4ac8700ad
Markko Paas <markko.paas@gmail.com>
2019-02-18 17:58:35 +0100

    refactor (api-cat-common) Separate out editable api properties, so they can be shared between registration and search

--- libraries/api-cat-common/src/main/java/no/fdk/acat/common/model/ApiRegistrationPublic.java
+++ libraries/api-cat-common/src/main/java/no/fdk/acat/common/model/ApiRegistrationPublic.java
@@ -6,12 +6,11 @@ import lombok.Data;
 import lombok.ToString;
 import no.fdk.acat.common.model.apispecification.ApiSpecification;
 
-import java.util.Set;
 
 @Data
 @ToString(includeFieldNames = false)
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class ApiRegistrationPublic {
+public class ApiRegistrationPublic extends ApiEditableProperties{
 
     private String id;
 
@@ -24,33 +23,5 @@ public class ApiRegistrationPublic {
     @ApiModelProperty("Original API spec")
     private String apiSpec;
 
-    @ApiModelProperty("The url of the API documentation")
-    private String apiDocUrl;
-
-    @ApiModelProperty("The dataset references")
-    private Set<String> datasetUris;
-
     private ApiSpecification apiSpecification;
-
-    @ApiModelProperty("Indication if the api is from an authoritative source (a National Component)")
-    private Boolean nationalComponent;
-
-    @ApiModelProperty("Indication if the api has open access")
-    private Boolean isOpenAccess;
-
-    @ApiModelProperty("Indication if the api has open licence")
-    private Boolean isOpenLicense;
-
-    @ApiModelProperty("Indication if the api is free")
-    private Boolean isFree;
-
-    private String cost;
-
-    private String usageLimitation;
-
-    private String performance;
-
-    private String availability;
-
-    private String statusCode;
 }
