f7f8dd5390d8ce2bc99592e8893a246d2af214da
Markko Paas <markko.paas@gmail.com>
2019-02-20 22:09:43 +0100

    feature (informationmodel-cat) Set up cron for harvesting informationmodels every sixth hour

--- applications/informationmodel-cat/src/main/java/no/fdk/imcat/ImcatApiApplication.java
+++ applications/informationmodel-cat/src/main/java/no/fdk/imcat/ImcatApiApplication.java
@@ -2,8 +2,10 @@ package no.fdk.imcat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ImcatApiApplication {
 
     public static void main(String[] args) {
