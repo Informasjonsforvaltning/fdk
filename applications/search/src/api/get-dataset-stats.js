8bbc23a7ab990bcec359fd66c8921c035f304c9f
Markko Paas <markko.paas@gmail.com>
2019-03-11 12:18:08 +0100

    chore(search-api): Fix aggregation names

--- applications/search/src/api/get-dataset-stats.js
+++ applications/search/src/api/get-dataset-stats.js
@@ -20,25 +20,22 @@ function getFromAggregation(data, aggregation) {
 export function extractStats(data) {
   return {
     total: _.get(data, 'hits.total', 0),
-    public: getFromBucketArray(data, 'accessRightsCount', 'PUBLIC'),
-    restricted: getFromBucketArray(data, 'accessRightsCount', 'RESTRICTED'),
-    nonPublic: getFromBucketArray(data, 'accessRightsCount', 'NON_PUBLIC'),
-    unknown: getFromBucketArray(data, 'accessRightsCount', 'UKJENT'),
+    public: getFromBucketArray(data, 'accessRights', 'PUBLIC'),
+    restricted: getFromBucketArray(data, 'accessRights', 'RESTRICTED'),
+    nonPublic: getFromBucketArray(data, 'accessRights', 'NON_PUBLIC'),
+    unknown: getFromBucketArray(data, 'accessRights', 'UKJENT'),
     opendata: getFromAggregation(data, 'opendata'),
     newLastWeek: getFromBucketArray(data, 'firstHarvested', 'last7days'),
     newLastMonth: getFromBucketArray(data, 'firstHarvested', 'last30days'),
     newLastYear: getFromBucketArray(data, 'firstHarvested', 'last365days'),
-    distributions: getFromAggregation(data, 'distCount'),
-    distOnPublicAccessCount: getFromAggregation(
-      data,
-      'distOnPublicAccessCount'
-    ),
-    subjectCount: getFromAggregation(data, 'subjectCount'),
-    catalogCounts: _.get(data, ['aggregations', 'catalogs', 'buckets'], [])
+    distributions: getFromAggregation(data, 'withDistribution'),
+    distOnPublicAccessCount: getFromAggregation(data, 'publicWithDistribution'),
+    subjectCount: getFromAggregation(data, 'subject'),
+    catalogCounts: _.get(data, ['aggregations', 'catalog', 'buckets'], [])
   };
 }
 
-const statsAggregations = `${searchAggregations},distCount,distOnPublicAccessCount,distOnPublicAccessCount,subjectCount`;
+const statsAggregations = `${searchAggregations},firstHarvested,withDistribution,publicWithDistribution,subject,catalog,opendata`;
 
 export const statsUrl = query =>
   `${datasetsUrlBase}${qs.stringify(
