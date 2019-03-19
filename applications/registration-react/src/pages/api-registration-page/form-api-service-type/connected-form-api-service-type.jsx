cd3c3901dd1f14d962382bd1792c44ef0dae57b1
Frédéric Heitmann-André <frederic.andre@gmail.com>
2019-03-19 14:15:03 +0100

    initial commit

--- applications/registration-react/src/pages/api-registration-page/form-api-service-type/connected-form-api-service-type.jsx
+++ applications/registration-react/src/pages/api-registration-page/form-api-service-type/connected-form-api-service-type.jsx
@@ -1,8 +1,13 @@
+<<<<<<< HEAD
 import { getFormSyncErrors } from 'redux-form';
+=======
+import { formValueSelector, getFormSyncErrors } from 'redux-form';
+>>>>>>> initial commit
 import { connect } from 'react-redux';
 import _ from 'lodash';
 import { ConfiguredFormApiServiceType } from './configured-form-api-service-type';
 
+<<<<<<< HEAD
 const mapStateToProps = (state, ownProps) => {
   const { apiItem } = ownProps;
   return {
@@ -10,6 +15,28 @@ const mapStateToProps = (state, ownProps) => {
     initialValues: {
       serviceType: _.get(apiItem, 'serviceType')
     }
+=======
+const selector = formValueSelector('apiServiceType');
+
+const mapStateToProps = (state, ownProps) => {
+  const { apiItem } = ownProps;
+  const apiServiceTypeCodeFromForm = selector(state, 'statusCode');
+  return {
+    syncErrors: getFormSyncErrors('apiServiceType')(state.form),
+    initialValues: {
+      statusCode: _.get(apiItem, 'statusCode'),
+      deprecationInfoExpirationDate: _.get(
+        apiItem,
+        'deprecationInfoExpirationDate'
+      ),
+      deprecationInfoMessage: _.get(apiItem, 'deprecationInfoMessage'),
+      deprecationInfoReplacedWithUrl: _.get(
+        apiItem,
+        'deprecationInfoReplacedWithUrl'
+      )
+    },
+    apiServiceTypeCodeFromForm
+>>>>>>> initial commit
   };
 };
 
--- applications/registration-react/src/pages/api-registration-page/form-api-service-type/form-api-service-type.component.jsx
+++ applications/registration-react/src/pages/api-registration-page/form-api-service-type/form-api-service-type.component.jsx
@@ -6,6 +6,7 @@ import { Field } from 'redux-form';
 import localization from '../../../lib/localization';
 import Helptext from '../../../components/helptext/helptext.component';
 import SelectField from '../../../components/field-select/field-select.component';
+<<<<<<< HEAD
 
 const apiServiceTypeSortOrder = {
   Kundeforhold: 0,
@@ -26,14 +27,101 @@ export const FormApiServiceType = ({ apiServiceTypeItems }) => (
         valueKey="code"
         saveObject={false}
       />
+=======
+import DatepickerField from '../../../components/field-datepicker/field-datepicker.component';
+import TextAreaField from '../../../components/field-textarea/field-textarea.component';
+import InputField from '../../../components/field-input/field-input.component';
+
+const apiServiceTypeSortOrder = {
+  EXPERIMENTAL: 0,
+  STABLE: 1,
+  DEPRECATED: 2,
+  REMOVED: 3
+};
+
+export const FormApiServiceType = ({ apiServiceTypeItems, apiServiceTypeCodeFromForm }) => (
+  <form>
+    <div className="form-group">
+      <Helptext title={localization.schema.apiServiceType.helptext.apiServiceType} />
+      <Field
+        name="statusCode"
+        component={SelectField}
+        items={_.sortBy(apiServiceTypeItems, item => apiServiceTypeSortOrder[item.code])}
+        valueKey="code"
+        saveObject={false}
+      />
+
+      {(apiServiceTypeCodeFromForm === 'DEPRECATED' ||
+        apiServiceTypeCodeFromForm === 'REMOVED') && (
+        <div className="mt-4">
+          <div className="form-group">
+            <Helptext
+              title={
+                localization.schema.apiServiceType.helptext
+                  .deprecationInfoExpirationDate
+              }
+            />
+            <Field
+              name="deprecationInfoExpirationDate"
+              type="text"
+              component={DatepickerField}
+              label={
+                localization.schema.apiServiceType.helptext
+                  .deprecationInfoExpirationDate
+              }
+            />
+          </div>
+          <div className="form-group">
+            <Helptext
+              title={
+                localization.schema.apiServiceType.helptext.deprecationInfoMessage
+              }
+            />
+            <Field
+              name="deprecationInfoMessage"
+              component={TextAreaField}
+              label={
+                localization.schema.apiServiceType.helptext.deprecationInfoMessage
+              }
+            />
+          </div>
+          <div className="form-group">
+            <Helptext
+              title={
+                localization.schema.apiServiceType.helptext
+                  .deprecationInfoReplacedWithUrl
+              }
+            />
+            <Field
+              name="deprecationInfoReplacedWithUrl"
+              component={InputField}
+              label={
+                localization.schema.apiServiceType.helptext
+                  .deprecationInfoReplacedWithUrl
+              }
+            />
+          </div>
+        </div>
+      )}
+>>>>>>> initial commit
     </div>
   </form>
 );
 
 FormApiServiceType.defaultProps = {
+<<<<<<< HEAD
   apiServiceTypeItems: []
 };
 
 FormApiServiceType.propTypes = {
   apiServiceTypeItems: PropTypes.array
+=======
+  apiServiceTypeItems: null,
+  apiServiceTypeCodeFromForm: null
+};
+
+FormApiServiceType.propTypes = {
+  apiServiceTypeItems: PropTypes.array,
+  apiServiceTypeCodeFromForm: PropTypes.string
+>>>>>>> initial commit
 };
