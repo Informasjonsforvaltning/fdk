7474172358f4f6800f3f62eb074dd5dac0f68a7d
Øyvind Isaksen <oyvind.isaksen@acando.no>
2019-03-21 12:59:57 +0100

    feat(search): #1413 show api-distributions

--- applications/search/src/pages/dataset-details-page/resolved-dataset-details-page.jsx
+++ applications/search/src/pages/dataset-details-page/resolved-dataset-details-page.jsx
@@ -2,9 +2,11 @@ import _ from 'lodash';
 import { resolve } from 'react-resolver';
 import { getDataset, getDatasetByURI } from '../../api/datasets';
 import { DatasetDetailsPage } from './dataset-details-page';
+import { getApi } from '../../api/apis';
 
 const memoizedGetDataset = _.memoize(getDataset);
 const memoizedGetDatasetByURI = _.memoize(getDatasetByURI);
+const memoizedGetApi = _.memoize(getApi);
 
 const mapProps = {
   datasetItem: props => memoizedGetDataset(props.match.params.id),
@@ -16,6 +18,18 @@ const mapProps = {
     const promiseMap = urlArray.map(url =>
       memoizedGetDatasetByURI(encodeURIComponent(url))
     );
+    const result = await Promise.all(promiseMap);
+    return result;
+  },
+  referencedAPIsFromDistribution: async props => {
+    const datasetItem = await memoizedGetDataset(props.match.params.id);
+    const apiIdArray = _.get(datasetItem, 'distribution')
+      .filter(item => item.accessService)
+      .map(item =>
+        _.get(item, ['accessService', 'endpointDescription', 0, 'uri'])
+      );
+
+    const promiseMap = apiIdArray.map(id => memoizedGetApi(id));
     const result = await Promise.all(promiseMap);
     return result;
   }
