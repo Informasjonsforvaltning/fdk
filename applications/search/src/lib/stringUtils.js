39ffb5e654cfe13a15c89cf8f5bbea2a09be8f4d
Øyvind Isaksen <oyvind.isaksen@acando.no>
2019-03-21 12:59:16 +0100

    chore(search): add lib-function and update use

--- applications/search/src/lib/stringUtils.js
+++ applications/search/src/lib/stringUtils.js
@@ -11,3 +11,23 @@ export function addValue(listAsString, value) {
     .filter(v => !!v)
     .join(',');
 }
+
+export function getFirstLineOfText(text) {
+  if (!text) {
+    return null;
+  }
+  const breakPattern = /(\n|<br|<\/p|<\/h)/;
+  const match = breakPattern.exec(text);
+
+  let firstLine = match ? text.substr(0, match.index) : text;
+
+  if (firstLine.length > 250) {
+    firstLine = firstLine.substr(0, 220);
+    firstLine = firstLine.substr(0, firstLine.lastIndexOf(' '));
+  }
+
+  if (firstLine.length < text.length) {
+    firstLine += ' ...';
+  }
+  return firstLine;
+}
