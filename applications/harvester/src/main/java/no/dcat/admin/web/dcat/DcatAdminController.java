13e1f9ded61b31c8fe44bb0c7fefc00f29f2356f
bgrova <bjorn.grova@brreg.no>
2019-03-21 14:38:11 +0100

    force build

--- applications/harvester/src/main/java/no/dcat/admin/web/dcat/DcatAdminController.java
+++ applications/harvester/src/main/java/no/dcat/admin/web/dcat/DcatAdminController.java
@@ -27,7 +27,7 @@ import java.util.Optional;
 @CrossOrigin(origins = "*")
 public class DcatAdminController {
 
-    //ttttttxxxxx
+    //ttttttx
 
     @Autowired
     private FusekiSettings fusekiSettings;
