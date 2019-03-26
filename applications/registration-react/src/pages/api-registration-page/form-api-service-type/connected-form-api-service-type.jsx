c494eb93a96eb81fe58c6da9a01c10111a4f16bc
Frédéric Heitmann-André <frederic.andre@gmail.com>
2019-03-26 14:13:58 +0100

    fixed value not displaying

--- applications/registration-react/src/pages/api-registration-page/form-api-service-type/connected-form-api-service-type.jsx
+++ applications/registration-react/src/pages/api-registration-page/form-api-service-type/connected-form-api-service-type.jsx
@@ -19,12 +19,12 @@ const mapStateToProps = (state, ownProps) => {
 const selector = formValueSelector('apiServiceType');
 
 const mapStateToProps = (state, ownProps) => {
-  const { apiServiceTypeItems } = ownProps;
+  const { apiServiceTypeItems, apiItem } = ownProps;
   const apiServiceTypeCodeFromForm = selector(state, 'serviceType');
   return {
     syncErrors: getFormSyncErrors('apiServiceType')(state.form),
     initialValues: {
-      serviceType: _.get(apiServiceTypeItems, 'serviceType'),
+      serviceType: _.get(apiItem, 'serviceType'),
       deprecationInfoExpirationDate: _.get(
         apiServiceTypeItems,
         'deprecationInfoExpirationDate'
