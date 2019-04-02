6c109ff3626cc3df64fd81e9a204b654c0fa7dda
Markko Paas <markko.paas@gmail.com>
2019-04-02 11:24:55 +0200

    feat(keycloak-oidc-protocol-rest-mapper): #1690 Custom Keyclak OIDC protocol mapper for enriching tokens from rest services

---
+++ keycloak-oidc-protocol-rest-mapper/src/test/java/no/fdk/keycloak/protocol/NoDuplicateMapperTest.java
@@ -0,0 +1,24 @@
+//package no.fdk.keycloak.protocol;
+//
+//import org.junit.Test;
+//import org.keycloak.common.util.StreamUtil;
+//import org.keycloak.protocol.ProtocolMapper;
+//
+//import java.util.Collection;
+//import java.util.ServiceLoader;
+//import java.util.stream.Collectors;
+//import java.util.stream.StreamSupport;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//public class NoDuplicateMapperTest {
+//
+//    @Test
+//    public void shouldNotHaveMappersWithDuplicateIds() {
+//        final ServiceLoader<ProtocolMapper> serviceLoader = ServiceLoader.load(ProtocolMapper.class);
+//        final Collection<String> mapperIds = StreamSupport.stream(serviceLoader.spliterator(), false).map(elem -> elem.getId()).collect(Collectors.toList());
+//
+//        assertThat(mapperIds).size().isGreaterThan(1);
+//        assertThat(mapperIds).doesNotHaveDuplicates();
+//    }
+//}
