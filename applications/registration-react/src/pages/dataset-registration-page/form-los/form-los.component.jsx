94f834a9867afb3fb9a036b51a94a58dc2193436
Øyvind Isaksen <oyvind.isaksen@acando.no>
2019-04-10 09:30:17 +0200

    chore(registration-react): add form-los early exit

--- applications/registration-react/src/pages/dataset-registration-page/form-los/form-los.component.jsx
+++ applications/registration-react/src/pages/dataset-registration-page/form-los/form-los.component.jsx
@@ -7,22 +7,22 @@ import Helptext from '../../../components/helptext/helptext.component';
 import { FieldTreeLos } from '../field-tree-los/field-tree-los.component';
 
 export const FormLOS = ({ losItems }) => {
-  if (losItems) {
-    return (
-      <form>
-        <div className="form-group">
-          <Helptext title={localization.schema.los.helptext.title} />
-          <Field
-            name="theme"
-            component={FieldTreeLos}
-            label={localization.schema.los.title}
-            losItems={losItems}
-          />
-        </div>
-      </form>
-    );
+  if (!losItems) {
+    return null;
   }
-  return null;
+  return (
+    <form>
+      <div className="form-group">
+        <Helptext title={localization.schema.los.helptext.title} />
+        <Field
+          name="theme"
+          component={FieldTreeLos}
+          label={localization.schema.los.title}
+          losItems={losItems}
+        />
+      </div>
+    </form>
+  );
 };
 
 FormLOS.defaultProps = {
