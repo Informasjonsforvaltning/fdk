6d557a541215719623a1bd2e65753ee9eda640c3
Markko Paas <markko.paas@gmail.com>
2019-04-29 09:34:27 +0200

    chore(search): Load environment config from server

--- applications/search/src/config.js
+++ applications/search/src/config.js
@@ -1,4 +1,9 @@
+// Environment is configured in server and sent over to the client over jsonp.
+// See here: search/server/views/index.ejs
+
+const serverEnv = window.__SERVER_ENV__ || {};
+
 export const config = {
-  reduxLog: process.env.REDUX_LOG === '1',
-  disqusShortname: process.env.DISQUS_SHORTNAME
+  reduxLog: serverEnv.REDUX_LOG === 'true',
+  disqusShortname: serverEnv.DISQUS_SHORTNAME
 };
