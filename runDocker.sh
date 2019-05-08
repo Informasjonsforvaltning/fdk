fc5f5173ed910b487ce46b92f2e684e9468a5c72
Markko Paas <markko.paas@gmail.com>
2019-05-08 10:35:13 +0200

    chore(build): change common build script name to buildDocker.sh

--- runDocker.sh
+++ runDocker.sh
@@ -1,21 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
-if [ $1 = "registration-react" ]
-then
-    cd applications/registration-react
-    docker build -t dcatno/registration-react:latest .
-else if [ $1 = "search" ]
-  then
-    cd applications/search
-    docker build -t dcatno/search:latest .
-  else
-    mvn clean install -T 2C -pl applications/$1 -am -DskipTests
-  fi
-fi
+. ./buildGroupsEnv.sh
 
- docker-compose stop $1 || true
- docker-compose up -d $1
+for i in "${!BUILD_APPS[@]}"; do
+    if [ "$1" == "${BUILD_APPS[$i]}" ]; then
+        ./buildApplication.sh "${BUILD_APPS[$i]}" "${BUILD_CMD[$i]}"
+    fi
+done
+
+docker-compose stop $1 || true
+docker-compose up -d $1
 
 if [ "$2" == "logs" ]; then
    docker-compose logs -f $1
