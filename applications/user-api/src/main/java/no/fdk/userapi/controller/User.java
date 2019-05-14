3815ec2b4ee4815fe8e0901179163963207fef80
Markko Paas <markko.paas@gmail.com>
2019-05-14 07:52:50 +0200

    feat(user-api): #1689 User api service

---
+++ applications/user-api/src/main/java/no/fdk/userapi/controller/User.java
@@ -0,0 +1,44 @@
+package no.fdk.userapi.controller;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import no.fdk.altinn.Organisation;
+import no.fdk.altinn.Person;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+//todo, this is actually an adapter. Declare JsonProperty on interface or abstract class
+public class User {
+    List<Organisation> organisations;
+    private Person person;
+
+    User(Person person, List<Organisation> organisations) {
+        this.person = person;
+        this.organisations = organisations;
+    }
+
+    public String getId() {
+        return person.getSocialSecurityNumber();
+    }
+
+    private List<String> getNames() {
+        return Arrays.asList(person.getName().split("\\s+"));
+    }
+
+    public String getFirstName() {
+        List<String> firstNamesList = getNames().subList(0, (getNames().size() - 1));
+        return String.join(" ", firstNamesList);
+    }
+
+    public String getLastName() {
+        return getNames().get(getNames().size() - 1);
+    }
+
+    @JsonProperty("fdk_access")
+    public String getPrivileges() {
+        List<String> privilegesList = organisations.stream().map(o -> "publisher:" + o.getOrganisationNumber() + ":admin").collect(Collectors.toList());
+        return String.join(" ", privilegesList);
+    }
+}
