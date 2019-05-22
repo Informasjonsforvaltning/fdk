1a3dbad9446c996cb4dc8853a8afe0b58ad995e8
Markko Paas <markko.paas@gmail.com>
2019-05-22 20:18:08 +0200

    chore(search): Add comments to FeatureToggle redux module

--- applications/search/src/redux/modules/featureToggle.js
+++ applications/search/src/redux/modules/featureToggle.js
@@ -1,13 +1,24 @@
-import _ from 'lodash';
+import fp from 'lodash/fp';
 
-const initialState = {
-  toggles: {}
+export const FEATURES = {
+  // API: 'API' //example
 };
+
+// featureToggle state gets rehydrated from localstore on init,
+// initialState is the setting for the new and incognito users
+const initialState = {
+  toggles: {
+    // [FEATURES.API]: true //example
+  }
+};
+
+const filterCurrentToggles = fp.pick(Object.keys(FEATURES));
 
 export function featureToggleResolver(state = initialState, action) {
   switch (action.type) {
     case 'STORE_INIT':
-      return { toggles: _.pick(state.toggles, []) };
+      // after rehydrating the store, keep only currently supported toggles
+      return { toggles: filterCurrentToggles(state.toggles) };
 
     default:
       return state;
