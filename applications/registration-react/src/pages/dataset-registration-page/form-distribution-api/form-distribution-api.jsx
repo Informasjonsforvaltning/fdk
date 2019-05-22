b398b04fa174f6c4fd1f63c9b791abaf985f92d5
Markko Paas <markko.paas@gmail.com>
2019-05-22 12:29:28 +0200

    refactor(registration-react): Separate props-enhancer from connector in FormDistributionApi

--- applications/registration-react/src/pages/dataset-registration-page/form-distribution-api/form-distribution-api.jsx
+++ applications/registration-react/src/pages/dataset-registration-page/form-distribution-api/form-distribution-api.jsx
@@ -2,10 +2,12 @@ import { compose } from 'recompose';
 import { FormDistributionApiPure } from './form-distribution-api-pure';
 import { resolver } from './resolver';
 import { formConfigurer } from './form-configurer';
+import { propsEnhancer } from './props-enhancer';
 import { connector } from './connector';
 
 const enhance = compose(
   connector,
+  propsEnhancer,
   formConfigurer,
   resolver
 );
