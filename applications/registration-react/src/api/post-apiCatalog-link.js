ac1c16887cfa121c0b3cd106cf7067a490e23a5d
Markko Paas <markko.paas@gmail.com>
2019-05-27 13:41:33 +0200

    chore(registration-react): Remove unused postApiCatalogLink

--- applications/registration-react/src/api/post-apiCatalog-link.js
+++
@@ -1,9 +0,0 @@
-import { post } from 'axios';
-
-export const postApiCatalogLink = (catalogId, importUrl) => {
-  const url = `/catalogs/${catalogId}/apicatalog`;
-
-  return post(url, { harvestSourceUri: importUrl }).then(
-    response => response.data
-  );
-};
