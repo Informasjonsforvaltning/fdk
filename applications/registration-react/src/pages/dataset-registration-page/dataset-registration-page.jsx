e0c958628c539e9e88744e01ecd713c0d6754bd7
Markko Paas <markko.paas@gmail.com>
2019-05-30 15:12:56 +0200

    refactor(registration-react): Split out the ensureData decorator from the datasetRegistrationConnector in order to clearly distinguish between data loading actions and state mapping

--- applications/registration-react/src/pages/dataset-registration-page/dataset-registration-page.jsx
+++ applications/registration-react/src/pages/dataset-registration-page/dataset-registration-page.jsx
@@ -2,13 +2,22 @@ import _ from 'lodash';
 import { compose, mapProps } from 'recompose';
 import { DatasetRegistrationPagePure } from './dataset-registration-page-pure';
 import { datasetRegistrationConnector } from './dataset-registration-connector';
+import { withDispatchOnPropsChange } from '../../lib/redux-dispatch-on-props-change';
+import { datasetRegistrationEnsureDataThunk } from './dataset-registration-ensure-data-thunk';
 
 const mapRouteParams = mapProps(({ match: { params } }) =>
   _.pick(params, ['catalogId', 'datasetId'])
 );
 
+const ensureData = withDispatchOnPropsChange(
+  (dispatch, props) =>
+    dispatch(datasetRegistrationEnsureDataThunk(props.catalogId)),
+  props => [props.catalogId]
+);
+
 const enhance = compose(
   mapRouteParams,
+  ensureData,
   datasetRegistrationConnector
 );
 export const DatasetRegistrationPage = enhance(DatasetRegistrationPagePure);
