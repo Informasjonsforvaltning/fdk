3dd87f7b1e6cb3b672a7f215fac848030c0bf452
Markko Paas <markko.paas@gmail.com>
2019-06-03 13:10:28 +0200

    fix(reference-data): Enable all hosts in Cors configuration for reference-data

--- applications/reference-data/src/main/java/no/dcat/themes/security/WebSecurityConfig.java
+++ applications/reference-data/src/main/java/no/dcat/themes/security/WebSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
@@ -27,6 +28,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic();
         http.csrf().disable();
+        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
     }
 
     protected WebSecurityConfig() {
