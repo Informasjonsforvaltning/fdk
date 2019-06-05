25e48ad92c9aefed903e19f6b745626a1d9b221f
Markko Paas <markko.paas@gmail.com>
2019-06-05 15:19:35 +0200

    refactor(registration-react): Extract route parameter mapper for dataset-registration-page

--- applications/registration-react/src/pages/dataset-registration-page/dataset-registration-page.jsx
+++ applications/registration-react/src/pages/dataset-registration-page/dataset-registration-page.jsx
@@ -1,6 +1,14 @@
-import { compose } from 'recompose';
+import _ from 'lodash';
+import { compose, mapProps } from 'recompose';
 import { DatasetRegistrationPagePure } from './dataset-registration-page-pure';
 import { datasetRegistrationConnector } from './dataset-registration-connector';
 
-const enhance = compose(datasetRegistrationConnector);
+const mapRouteParams = mapProps(({ match: { params } }) =>
+  _.pick(params, ['catalogId', 'datasetId'])
+);
+
+const enhance = compose(
+  mapRouteParams,
+  datasetRegistrationConnector
+);
 export const DatasetRegistrationPage = enhance(DatasetRegistrationPagePure);
