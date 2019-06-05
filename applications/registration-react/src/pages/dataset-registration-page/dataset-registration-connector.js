5554f7f2a11286b15b5602038049d748deffad5f
Markko Paas <markko.paas@gmail.com>
2019-06-05 19:24:58 +0200

    refactor(registration-react): Extract datasetRegistrationEnsureDataThunk from dataset-registration-connector

--- applications/registration-react/src/pages/dataset-registration-page/dataset-registration-connector.js
+++ applications/registration-react/src/pages/dataset-registration-page/dataset-registration-connector.js
@@ -1,14 +1,12 @@
-import { batch, connect } from 'react-redux';
+import { connect } from 'react-redux';
 import _ from 'lodash';
 
 import {
   deleteDatasetAction,
-  fetchDatasetsIfNeeded,
   getDatasetItemByDatasetiId,
   getDatasetItemsByCatalogId
 } from '../../redux/modules/datasets';
 import {
-  fetchReferenceDataIfNeededAction,
   REFERENCEDATA_PATH_FREQUENCY,
   REFERENCEDATA_PATH_LOS,
   REFERENCEDATA_PATH_OPENLICENCES,
@@ -17,6 +15,7 @@ import {
   REFERENCEDATA_PATH_THEMES
 } from '../../redux/modules/referenceData';
 import { getDatasetFormStatusById } from '../../redux/modules/dataset-form-status';
+import { datasetRegistrationEnsureDataThunk } from './dataset-registration-ensure-data-thunk';
 
 const mapStateToProps = (
   { form, datasetFormStatus, datasets, referenceData },
@@ -107,21 +106,8 @@ const mapStateToProps = (
 };
 
 const mapDispatchToProps = (dispatch, { catalogId }) => ({
-  onChangeDatasetId: () => {
-    batch(() => {
-      dispatch(fetchDatasetsIfNeeded(catalogId));
-      dispatch(fetchReferenceDataIfNeededAction(REFERENCEDATA_PATH_PROVENANCE));
-      dispatch(fetchReferenceDataIfNeededAction(REFERENCEDATA_PATH_FREQUENCY));
-      dispatch(fetchReferenceDataIfNeededAction(REFERENCEDATA_PATH_THEMES));
-      dispatch(
-        fetchReferenceDataIfNeededAction(REFERENCEDATA_PATH_REFERENCETYPES)
-      );
-      dispatch(
-        fetchReferenceDataIfNeededAction(REFERENCEDATA_PATH_OPENLICENCES)
-      );
-      dispatch(fetchReferenceDataIfNeededAction(REFERENCEDATA_PATH_LOS));
-    });
-  },
+  onChangeDatasetId: () =>
+    dispatch(datasetRegistrationEnsureDataThunk(catalogId)),
 
   deleteDatasetItem: (catalogId, datasetId) =>
     dispatch(deleteDatasetAction(catalogId, datasetId))
