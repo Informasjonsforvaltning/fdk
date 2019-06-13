c823d8ebee06635c9c79aea7c18a0f9a27d055da
Markko Paas <markko.paas@gmail.com>
2019-06-13 13:45:55 +0200

    refactor(registration-react): Simplify api-form-status redux state

--- applications/registration-react/src/redux/modules/api-form-status.js
+++ applications/registration-react/src/redux/modules/api-form-status.js
@@ -1,16 +1,12 @@
 import _ from 'lodash';
 
-export const API_FORM_STATUS_LAST_SAVED = 'API_FORM_STATUS_LAST_SAVED';
+export const API_FORM_STATUS_PATCH_SUCCESS = 'API_FORM_STATUS_PATCH_SUCCESS';
 export const API_FORM_STATUS_IS_SAVING = 'API_FORM_STATUS_IS_SAVING';
 export const API_FORM_STATUS_SAVE_ERROR = 'API_FORM_STATUS_SAVE_ERROR';
-export const API_FORM_STATUS_JUSTPUBLISHEDORUNPUBLISHED =
-  'API_FORM_STATUS_JUSTPUBLISHEDORUNPUBLISHED';
 
-export const apiFormPatchSuccessAction = apiId => ({
-  type: API_FORM_STATUS_LAST_SAVED,
-  payload: {
-    apiId
-  }
+export const apiFormPatchSuccessAction = ({ apiId, patch }) => ({
+  type: API_FORM_STATUS_PATCH_SUCCESS,
+  payload: { apiId, patch }
 });
 
 export const apiFormPatchIsSavingAction = apiId => ({
@@ -28,59 +24,33 @@ export const apiFormPatchErrorAction = (apiId, error) => ({
   }
 });
 
-export const apiFormPatchJustPublishedOrUnPublishedAction = (
-  apiId,
-  justChanged,
-  status
-) => ({
-  type: API_FORM_STATUS_JUSTPUBLISHEDORUNPUBLISHED,
-  payload: {
-    apiId,
-    justChanged,
-    status
-  }
-});
-
 const initialState = {};
 
 export default function apiFormStatus(state = initialState, action) {
   switch (action.type) {
-    case API_FORM_STATUS_IS_SAVING:
+    case API_FORM_STATUS_IS_SAVING: {
+      const { apiId } = action.payload;
       return {
         ...state,
-        [action.payload.apiId]: {
-          ..._.get(state, _.get(action.payload, 'apiId'), []),
-          isSaving: true
-        }
+        [apiId]: { isSaving: true }
       };
-    case API_FORM_STATUS_LAST_SAVED:
+    }
+    case API_FORM_STATUS_PATCH_SUCCESS: {
+      const { apiId, patch } = action.payload;
+      const justPublishedOrUnPublished = !!patch.registrationStatus;
+
       return {
         ...state,
-        [action.payload.apiId]: {
-          ..._.get(state, _.get(action.payload, 'apiId'), []),
-          isSaving: false,
-          error: null
-        }
+        [apiId]: { justPublishedOrUnPublished }
       };
-    case API_FORM_STATUS_SAVE_ERROR:
+    }
+    case API_FORM_STATUS_SAVE_ERROR: {
+      const { apiId, error } = action.payload;
       return {
         ...state,
-        [action.payload.apiId]: {
-          ..._.get(state, _.get(action.payload, 'apiId'), []),
-          isSaving: false,
-          justPublishedOrUnPublished: false,
-          error: action.payload.error
-        }
+        [apiId]: { error }
       };
-    case API_FORM_STATUS_JUSTPUBLISHEDORUNPUBLISHED:
-      return {
-        ...state,
-        [action.payload.apiId]: {
-          ..._.get(state, _.get(action.payload, 'apiId'), []),
-          justPublishedOrUnPublished: action.payload.justChanged,
-          status: action.payload.status
-        }
-      };
+    }
     default:
       return state;
   }
