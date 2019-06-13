2616a57e4ce302f7ee222352e94c09492fef92b0
Markko Paas <markko.paas@gmail.com>
2019-06-13 09:00:53 +0200

    chore(registration-react): Remove broken dataset import feature

--- applications/registration-react/src/pages/dataset-list-page/items-list/dataset-item-list.component.jsx
+++ applications/registration-react/src/pages/dataset-list-page/items-list/dataset-item-list.component.jsx
@@ -1,10 +1,8 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import axios from 'axios';
 import _ from 'lodash';
 
 import localization from '../../../lib/localization';
-import ImportModal from '../../../components/import-modal/import-modal.component';
 import { ListItems } from '../../../components/list-items/list-items.component';
 import './dataset-items-list.scss';
 
@@ -15,39 +13,10 @@ export const DatasetItemsList = ({
 }) => {
   const [sortField, setSortField] = useState('');
   const [sortType, setSortType] = useState('');
-  const [showImportModal, setShowImportModal] = useState(false);
-  const toggleShowImportModal = () => setShowImportModal(!showImportModal);
 
   const onSortField = (field, type) => {
     setSortField(field);
     setSortType(type);
-  };
-
-  const handleImportDataset = url => {
-    const catalogURL = window.location.pathname;
-
-    setShowImportModal(false);
-
-    const header = {
-      Accept: 'application/json'
-    };
-
-    const getInit = {
-      headers: header,
-      credentials: 'same-origin'
-    };
-
-    return axios
-      .post(`${catalogURL}`, url, getInit)
-      .then(response => {
-        window.location.replace(
-          `${catalogURL}/${_.get(response, ['data', 'id'])}`
-        );
-      })
-      .catch(response => {
-        const { error } = response;
-        return Promise.reject(error);
-      });
   };
 
   return (
@@ -61,14 +30,6 @@ export const DatasetItemsList = ({
           <i className="fa fa-plus fdk-color0 mr-2" />
           {localization.datasets.list.btnNewDataset}
         </button>
-        <button
-          type="button"
-          className="ml-2 transparentButton"
-          onClick={toggleShowImportModal}
-        >
-          <i className="fa fa-plus fdk-color1 mr-2" />
-          {localization.datasets.list.btnImportDataset}
-        </button>
       </div>
 
       <ListItems
@@ -78,17 +39,6 @@ export const DatasetItemsList = ({
         sortType={sortType}
         onSortField={onSortField}
         prefixPath={`/catalogs/${catalogId}/datasets`}
-      />
-
-      <ImportModal
-        modal={showImportModal}
-        handleAction={handleImportDataset}
-        toggle={toggleShowImportModal}
-        title={localization.datasets.import.title}
-        body={localization.datasets.import.body}
-        setURL={localization.datasets.import.setURL}
-        btnCancel={localization.app.cancel}
-        btnConfirm={localization.app.import}
       />
     </div>
   );
