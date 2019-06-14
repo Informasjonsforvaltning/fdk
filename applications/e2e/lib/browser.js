e8e10d2cac623b78dcd8e4b766749d5e20b64f13
Markko Paas <markko.paas@gmail.com>
2019-06-14 08:50:25 +0200

    chore(e2e): Lint

--- applications/e2e/lib/browser.js
+++ applications/e2e/lib/browser.js
@@ -1,50 +1,54 @@
-import puppeteer from "puppeteer";
-import { config } from "../config";
+import puppeteer from 'puppeteer';
+import { config } from '../config';
 
 const browserConfig = {
-    default: {},
-    chromium: {
-        headless: false,
-        slowMo: 80,
-        args: [`--window-size=1920,1080`]
-    },
-    'google-chrome-unstable': {
-        args: ['--no-sandbox'],
-        executablePath: 'google-chrome-unstable'
-    }
-
+  default: {},
+  chromium: {
+    headless: false,
+    slowMo: 80,
+    args: [`--window-size=1920,1080`]
+  },
+  'google-chrome-unstable': {
+    args: ['--no-sandbox'],
+    executablePath: 'google-chrome-unstable'
+  }
 };
 
 let browser;
 
 export function launchBrowser() {
-    return puppeteer.launch(browserConfig[config.browser]);
+  return puppeteer.launch(browserConfig[config.browser]);
 }
 
 export function getBrowser() {
-    return browser || (browser = launchBrowser());
+  browser = browser || launchBrowser();
+  return browser;
 }
 
 export async function getNewBrowserPage({ width = 1920, height = 1080 } = {}) {
-    const browser = await getBrowser();
-    const page = await browser.newPage();
+  const browser = await getBrowser();
+  const page = await browser.newPage();
 
-    await page.setViewport({ width, height });
+  await page.setViewport({ width, height });
 
-    page.on('console', consoleMessage => {
-        console.log('BROWSER CONSOLE:', consoleMessage.type(), consoleMessage.text());
-        // usually the arguments are included in test form of the message
-        // consoleMessage.args().forEach(msgArgJSHandle => {
-        //     console.log('ARG:', msgArgJSHandle.jsonValue());
-        // });
-    });
+  page.on('console', consoleMessage => {
+    console.warn(
+      'BROWSER CONSOLE:',
+      consoleMessage.type(),
+      consoleMessage.text()
+    );
+    // usually the arguments are included in test form of the message
+    // consoleMessage.args().forEach(msgArgJSHandle => {
+    //     console.log('ARG:', msgArgJSHandle.jsonValue());
+    // });
+  });
 
-    return page;
+  return page;
 }
 
 export async function close() {
-    if (browser) {
-        (await browser).close();
-    }
-    browser = undefined;
+  if (browser) {
+    (await browser).close();
+  }
+  browser = undefined;
 }
