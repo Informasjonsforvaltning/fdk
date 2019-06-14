5838f41b50596987e30ca81b789958510e4646fa
Markko Paas <markko.paas@gmail.com>
2019-06-14 16:55:47 +0200

    refactor(registration-react): Convert ProtectedRoute to function

--- applications/registration-react/src/app-protected-route/app-protected-route.component.jsx
+++ applications/registration-react/src/app-protected-route/app-protected-route.component.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Redirect, Route, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import IdleTimer from 'react-idle-timer';
@@ -8,83 +8,66 @@ import localization from '../lib/localization';
 import { fetchUserIfNeeded } from '../actions/index';
 import TimeoutModal from './timeout-modal/timeout-modal.component';
 
-export class ProtectedRoutePure extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showInactiveWarning: false
-    };
-    this.onIdle = this.onIdle.bind(this);
-    this.toggle = this.toggle.bind(this);
-    this.refreshSession = this.refreshSession.bind(this);
-  }
+export const ProtectedRoutePure = props => {
+  const {
+    userItem,
+    isFetchingUser,
+    component: Component,
+    history,
+    dispatch
+  } = props;
 
-  onIdle() {
-    this.setState({
-      showInactiveWarning: true
-    });
-  }
+  const [showInactiveWarning, setShowInactiveWarning] = useState(false);
 
-  toggle() {
-    this.setState({
-      showInactiveWarning: false
-    });
-    window.location.href = `${window.location.origin}/loggedOut`;
-  }
+  const logOut = () => {
+    setShowInactiveWarning(false);
+    if (history) {
+      history.push('/loggedOut');
+    }
+  };
 
-  refreshSession() {
-    const { dispatch } = this.props;
-    this.setState({
-      showInactiveWarning: false
-    });
+  const refreshSession = () => {
+    setShowInactiveWarning(false);
     dispatch(fetchUserIfNeeded());
-  }
+  };
 
-  render() {
-    const {
-      userItem,
-      isFetchingUser,
-      component: Component,
-      ...props
-    } = this.props;
-    const { showInactiveWarning } = this.state;
-    return (
-      <>
-        {!isFetchingUser && (
-          <Route
-            {...props}
-            render={props =>
-              userItem && userItem.name !== '' ? (
-                <Component {...props} />
-              ) : (
-                <Redirect {...props} to="/loggin" />
-              )
-            }
-          />
-        )}
-        <IdleTimer
-          element={document}
-          onIdle={this.onIdle}
-          timeout={27.5 * 60 * 1000} // gir idle warning etter 27,5 minutter
-          debounce={5000}
+  return (
+    <>
+      {!isFetchingUser && (
+        <Route
+          {...props}
+          render={props =>
+            userItem && userItem.name !== '' ? (
+              <Component {...props} />
+            ) : (
+              <Redirect {...props} to="/loggin" />
+            )
+          }
         />
-        <TimeoutModal
-          modal={showInactiveWarning}
-          toggle={this.toggle}
-          refreshSession={this.refreshSession}
-          title={localization.inactiveSessionWarning.title}
-          ingress={localization.inactiveSessionWarning.loggingOut}
-          body={localization.inactiveSessionWarning.stayLoggedIn}
-          buttonConfirm={localization.inactiveSessionWarning.buttonStayLoggedIn}
-          buttonLogout={localization.inactiveSessionWarning.buttonLogOut}
-        />
-      </>
-    );
-  }
-}
+      )}
+      <IdleTimer
+        element={document}
+        onIdle={() => setShowInactiveWarning(true)}
+        timeout={27.5 * 60 * 1000} // gir idle warning etter 27,5 minutter
+        debounce={5000}
+      />
+      <TimeoutModal
+        modal={showInactiveWarning}
+        toggle={logOut}
+        refreshSession={refreshSession}
+        title={localization.inactiveSessionWarning.title}
+        ingress={localization.inactiveSessionWarning.loggingOut}
+        body={localization.inactiveSessionWarning.stayLoggedIn}
+        buttonConfirm={localization.inactiveSessionWarning.buttonStayLoggedIn}
+        buttonLogout={localization.inactiveSessionWarning.buttonLogOut}
+      />
+    </>
+  );
+};
 
 ProtectedRoutePure.defaultProps = {
   userItem: null,
+  history: null,
   isFetchingUser: false,
   component: null
 };
@@ -92,6 +75,7 @@ ProtectedRoutePure.defaultProps = {
 ProtectedRoutePure.propTypes = {
   dispatch: PropTypes.func.isRequired,
   userItem: PropTypes.object,
+  history: PropTypes.object,
   isFetchingUser: PropTypes.bool,
   component: PropTypes.oneOfType([PropTypes.func, PropTypes.object])
 };
@@ -107,4 +91,6 @@ function mapStateToProps({ user }) {
   };
 }
 
-export const ProtectedRoute = connect(mapStateToProps)(ProtectedRoutePure);
+export const ProtectedRoute = withRouter(
+  connect(mapStateToProps)(ProtectedRoutePure)
+);
