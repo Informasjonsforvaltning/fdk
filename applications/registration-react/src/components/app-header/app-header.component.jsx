9cbe6abd893585cb5cb6070b8310b7b49b903549
Markko Paas <markko.paas@gmail.com>
2019-06-16 23:03:13 +0200

    refactor(registration-react): Convert AppHeader component to function

--- applications/registration-react/src/components/app-header/app-header.component.jsx
+++ applications/registration-react/src/components/app-header/app-header.component.jsx
@@ -9,101 +9,85 @@ import '../../assets/style/react-tags-override.scss';
 import './styles';
 import './app-header.scss';
 
-export class HeaderPure extends React.Component {
-  constructor(props) {
-    super(props);
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      dropdownOpen: false
-    };
+export const HeaderPure = ({ location, userItem }) => {
+  let headerTitle;
+  switch (location.pathname.split('/')[3]) {
+    case 'datasets':
+      headerTitle = localization.header['Registration of Datasets'];
+      break;
+    case 'apis':
+      headerTitle = localization.header["Registration of API's"];
+      break;
+    default:
+      headerTitle = localization.app.title;
   }
-
-  toggle() {
-    const { dropdownOpen } = this.state;
-    this.setState({ dropdownOpen: !dropdownOpen });
-  }
-
-  render() {
-    let headerTitle;
-    const { location, userItem } = this.props;
-    switch (location.pathname.split('/')[3]) {
-      case 'datasets':
-        headerTitle = localization.header['Registration of Datasets'];
-        break;
-      case 'apis':
-        headerTitle = localization.header["Registration of API's"];
-        break;
-      default:
-        headerTitle = localization.app.title;
-    }
-    return (
-      <header>
-        <div>
-          <a
-            id="focus-element"
-            className="uu-invisible"
-            href={`${location.pathname}#content`}
-            aria-hidden="true"
-          >
-            {localization.app.skipLink}
-          </a>
-        </div>
-        <div id="skip-link-wrap">
-          <a id="skip-link" href={`${location.pathname}#content`}>
-            {localization.app.skipLink}
-          </a>
-        </div>
-        <div className="fdk-header">
-          <div className="container">
-            <div className="row">
-              <div className="col-6 col-md-4">
-                <a title="Link til Felles datakatalog" href="/">
-                  <span className="uu-invisible" aria-hidden="false">
-                    {localization.app.navigateFrontpage}
-                  </span>
-                  <img
-                    className="fdk-logo"
-                    src="/static/img/logo-registrering.svg"
-                    alt="Logo for Felles datakatalog"
-                  />
-                </a>
-              </div>
-
-              <div className="col-6 col-md-4 d-flex justify-content-center align-items-center">
-                <span>
-                  <strong>{headerTitle}</strong>
+  return (
+    <header>
+      <div>
+        <a
+          id="focus-element"
+          className="uu-invisible"
+          href={`${location.pathname}#content`}
+          aria-hidden="true"
+        >
+          {localization.app.skipLink}
+        </a>
+      </div>
+      <div id="skip-link-wrap">
+        <a id="skip-link" href={`${location.pathname}#content`}>
+          {localization.app.skipLink}
+        </a>
+      </div>
+      <div className="fdk-header">
+        <div className="container">
+          <div className="row">
+            <div className="col-6 col-md-4">
+              <a title="Link til Felles datakatalog" href="/">
+                <span className="uu-invisible" aria-hidden="false">
+                  {localization.app.navigateFrontpage}
                 </span>
-              </div>
-              <div className="col-md-4 d-flex align-items-center fdk-header-text_items justify-content-end">
-                {userItem &&
-                  userItem.name && (
-                    <div className="mr-4">
-                      <i className="fa fa-user fdk-fa-left fdk-color-cta3" />
-                      {userItem.name}
-                    </div>
-                  )}
-                {userItem && (
-                  <div className="mr-4 fdk-auth-link">
-                    <a href={`${window.location.origin}/logout`}>
-                      {localization.app.logOut}
-                    </a>
+                <img
+                  className="fdk-logo"
+                  src="/static/img/logo-registrering.svg"
+                  alt="Logo for Felles datakatalog"
+                />
+              </a>
+            </div>
+
+            <div className="col-6 col-md-4 d-flex justify-content-center align-items-center">
+              <span>
+                <strong>{headerTitle}</strong>
+              </span>
+            </div>
+            <div className="col-md-4 d-flex align-items-center fdk-header-text_items justify-content-end">
+              {userItem &&
+                userItem.name && (
+                  <div className="mr-4">
+                    <i className="fa fa-user fdk-fa-left fdk-color-cta3" />
+                    {userItem.name}
                   </div>
                 )}
-                {!userItem && (
-                  <div className="mr-4 fdk-auth-link">
-                    <a href={`${window.location.origin}/login`}>
-                      {localization.app.logIn}
-                    </a>
-                  </div>
-                )}
-              </div>
+              {userItem && (
+                <div className="mr-4 fdk-auth-link">
+                  <a href={`${window.location.origin}/logout`}>
+                    {localization.app.logOut}
+                  </a>
+                </div>
+              )}
+              {!userItem && (
+                <div className="mr-4 fdk-auth-link">
+                  <a href={`${window.location.origin}/login`}>
+                    {localization.app.logIn}
+                  </a>
+                </div>
+              )}
             </div>
           </div>
         </div>
-      </header>
-    );
-  }
-}
+      </div>
+    </header>
+  );
+};
 
 HeaderPure.defaultProps = {
   userItem: null
