6a19ce2cd2623a2fb7b526351c1bec85230038ba
Markko Paas <markko.paas@gmail.com>
2019-06-18 12:49:07 +0200

    fix(registration-react): #1973 Fix validation indicators on dataset Los and Theme fields

--- applications/registration-react/src/pages/dataset-registration-page/form-los/configured-form-los.jsx
+++ applications/registration-react/src/pages/dataset-registration-page/form-los/configured-form-los.jsx
@@ -1,12 +1,12 @@
 import { reduxForm } from 'redux-form';
 
 import { FormLOS } from './form-los.component';
-
+import validate from '../form-theme/form-theme-validations';
 import { asyncValidateDatasetInvokePatch } from '../formsLib/asyncValidateDatasetInvokePatch';
 
 const config = {
   form: 'themes',
-
+  validate,
   asyncValidate: asyncValidateDatasetInvokePatch
 };
 
