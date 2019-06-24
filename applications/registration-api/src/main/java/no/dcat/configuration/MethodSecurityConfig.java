1cc7b60da4fc414bf316ac504ea46da3eeb245f8
Markko Paas <markko.paas@gmail.com>
2019-06-24 12:36:12 +0200

    refactor(registration-api): Simplify FdkPermissionEvaluator instantiation via @Bean

--- applications/registration-api/src/main/java/no/dcat/configuration/MethodSecurityConfig.java
+++ applications/registration-api/src/main/java/no/dcat/configuration/MethodSecurityConfig.java
@@ -1,33 +1,14 @@
 package no.dcat.configuration;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
-import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 
-/**
- * Created by dask on 02.06.2017.
- */
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
-    private static Logger logger = LoggerFactory.getLogger(MethodSecurityConfig.class);
 
-    private final FdkPermissionEvaluator fdkPermissionEvaluator;
-
-    public MethodSecurityConfig() {
-        logger.info("method config");
-        this.fdkPermissionEvaluator = new FdkPermissionEvaluator();
+    @Bean
+    public FdkPermissionEvaluator fdkPermissionEvaluator() {
+        return new FdkPermissionEvaluator();
     }
-
-    @Override
-    protected MethodSecurityExpressionHandler createExpressionHandler() {
-        DefaultMethodSecurityExpressionHandler expressionHandler = new DefaultMethodSecurityExpressionHandler();
-
-        expressionHandler.setPermissionEvaluator(fdkPermissionEvaluator);
-
-        return expressionHandler;
-    }
-
 }
