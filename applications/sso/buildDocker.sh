931bcb9c48aac6c45b0d79bc5243a7b4a962cd79
Markko Paas <markko.paas@gmail.com>
2019-06-25 22:59:53 +0200

    conf(sso): Add keycloak-user-storage-rest SPI module to keycloak

--- applications/sso/buildDocker.sh
+++ applications/sso/buildDocker.sh
@@ -3,6 +3,14 @@ set -e
 
 SECONDS=0 # start timer
 
+echo "Start build libraries/keycloak-user-storage-rest";
+
+(cd ../.. && mvn clean package -B -T 2C --projects libraries/keycloak-user-storage-rest --also-make)
+cp ../../libraries/keycloak-user-storage-rest/target/keycloak-user-storage-rest.jar deployments/
+
+echo "SECONDS"
+echo $SECONDS
+
 echo "Start build in docker image";
 docker build -t dcatno/sso:latest .
 
