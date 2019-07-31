ddaefb830754f21fec3a7762f493e04d4472e284
Alexander Zakharov <alexander.zakharov@capgemini.com>
2019-07-31 10:39:14 +0200

    fix(search): move tests to container to ensure platform independent build

--- applications/search/buildDocker.sh
+++ applications/search/buildDocker.sh
@@ -3,27 +3,6 @@ set -e
 
 SECONDS=0 # start timer
 
-echo "Install dependencies"
-npm install
-
-echo "SECONDS"
-echo $SECONDS
-
-echo "Run tests"
-npm test
-
-echo "SECONDS"
-echo $SECONDS
-
-if [[ ! -z "$TRAVIS" ]]
-then
-    echo "Report coverage"
-    codecov
-fi
-
-echo "SECONDS"
-echo $SECONDS
-
 echo "Start build in docker image";
 docker build -t dcatno/search:latest .
 
