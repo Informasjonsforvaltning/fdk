205c08be65be103bcf0f777d0b87db575fe5a8fb
Markko Paas <markko.paas@gmail.com>
2019-08-01 14:40:00 +0200

    conf(sso): Use ID-porten mock service instead of accessing ID-porten test server in dev environment

--- applications/sso/tools/preprocess-import-entrypoint.sh
+++ applications/sso/tools/preprocess-import-entrypoint.sh
@@ -6,6 +6,12 @@ echo "IDPORTEN_CLIENT_ID=$IDPORTEN_CLIENT_ID"
 
 OIDC_CONF=$(curl $IDPORTEN_OIDC_ROOT/.well-known/openid-configuration)
 
+if [ -z "$OIDC_CONF" ]
+then
+    echo "Error fetching ID-porten openid-configuration"
+    exit 1
+fi
+
 IDPORTEN_OIDC_ISSUER=$(echo $OIDC_CONF | jq ".issuer" -r)
 IDPORTEN_OIDC_AUTHORIZATION_URL=$(echo $OIDC_CONF | jq ".authorization_endpoint" -r)
 IDPORTEN_OIDC_TOKEN_URL=$(echo $OIDC_CONF | jq ".token_endpoint" -r)
