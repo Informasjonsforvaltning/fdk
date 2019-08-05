318e6b9659dda8a2e06fc47fed4f44bb5e848168
Alexander Zakharov <alexander.zakharov@capgemini.com>
2019-08-05 16:24:44 +0200

    style(search): #2060 change column widths of api-usage-instructions component to match the design and update snapshots

--- applications/search/src/pages/api-details-page/api-usage-instruction/api-usage-instruction.component.jsx
+++ applications/search/src/pages/api-details-page/api-usage-instruction/api-usage-instruction.component.jsx
@@ -30,15 +30,21 @@ export default function ApiServer({ servers, apiSpecUrl, apiDocUrl }) {
   return (
     <ListRegular title={localization.api.servers.title}>
       <div className="d-flex list-regular--item">
-        <div className="col-4 pl-0">
+        <div className="col-7 pl-0">
           <h4 className="">{localization.api.servers.url}</h4>
         </div>
-        <div className="col-8">
+        <div className="col-5">
           <h4 className="">{localization.api.servers.description}</h4>
         </div>
       </div>
       {servers.map(({ url, description }) => (
-        <TwoColRow key={`${url}${description}`} col1={url} col2={description} />
+        <TwoColRow
+          key={`${url}${description}`}
+          col1={url}
+          col2={description}
+          col1Width={7}
+          col2Width={5}
+        />
       ))}
       {renderLinks()}
     </ListRegular>
