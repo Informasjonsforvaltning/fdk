f450ab2279fa7bdc7651d0778ad093642158bd68
Markko Paas <markko.paas@gmail.com>
2019-08-13 00:44:20 +0200

    conf(sso): Implement idporten-mock as a realm in the sso server

--- applications/sso/tools/preprocess-import-entrypoint.sh
+++ applications/sso/tools/preprocess-import-entrypoint.sh
@@ -6,24 +6,43 @@ echo "Processing import template"
 echo "IDPORTEN_OIDC_ROOT=$IDPORTEN_OIDC_ROOT"
 echo "IDPORTEN_CLIENT_ID=$IDPORTEN_CLIENT_ID"
 
+if [[ $IDPORTEN_OIDC_ROOT =~ ^$SSO_HOST ]]; then
+    # identiy provider is on the same server (another realm)
+    # we cannot query configuration before server has started
 
-OIDC_CONF_ADDRESS=$IDPORTEN_OIDC_ROOT/.well-known/openid-configuration
+   sed -e 's,${SSO_HOST},'$SSO_HOST',g' \
+  </tmp/keycloak/import-template/idporten-mock-realm.template.json >/tmp/keycloak/import/idporten-mock-realm.json
 
-source ${__dir}/wait_for_http_200.sh $OIDC_CONF_ADDRESS
+  cp -f /tmp/keycloak/import-template/idporten-mock-users-0.json /tmp/keycloak/import/
 
-OIDC_CONF=$(curl $OIDC_CONF_ADDRESS)
+    IDPORTEN_OIDC_ISSUER=${IDPORTEN_OIDC_ROOT}
+    IDPORTEN_OIDC_AUTHORIZATION_URL=${IDPORTEN_OIDC_ROOT}/protocol/openid-connect/auth
+    IDPORTEN_OIDC_TOKEN_URL=${IDPORTEN_OIDC_ROOT}/protocol/openid-connect/token
+    IDPORTEN_OIDC_JWKS_URL=${IDPORTEN_OIDC_ROOT}/protocol/openid-connect/certs
+    IDPORTEN_OIDC_LOGOUT_URL=${IDPORTEN_OIDC_ROOT}/protocol/openid-connect/logout
+else
+    # identity provider is in another server
+    # wait until configuration file becomes available and parse it
+    OIDC_CONF_ADDRESS=$IDPORTEN_OIDC_ROOT/.well-known/openid-configuration
 
-if [ -z "$OIDC_CONF" ]
-then
+    source ${__dir}/wait_for_http_200.sh $OIDC_CONF_ADDRESS
+
+    OIDC_CONF=$(curl $OIDC_CONF_ADDRESS)
+
+    if [ -z "$OIDC_CONF" ]
+    then
     echo "Error fetching ID-porten openid-configuration"
     exit 1
+    fi
+
+    IDPORTEN_OIDC_ISSUER=$(echo $OIDC_CONF | jq ".issuer" -r)
+    IDPORTEN_OIDC_AUTHORIZATION_URL=$(echo $OIDC_CONF | jq ".authorization_endpoint" -r)
+    IDPORTEN_OIDC_TOKEN_URL=$(echo $OIDC_CONF | jq ".token_endpoint" -r)
+    IDPORTEN_OIDC_JWKS_URL=$(echo $OIDC_CONF | jq ".jwks_uri" -r)
+    IDPORTEN_OIDC_LOGOUT_URL=$(echo $OIDC_CONF | jq ".end_session_endpoint" -r)
 fi
 
-IDPORTEN_OIDC_ISSUER=$(echo $OIDC_CONF | jq ".issuer" -r)
-IDPORTEN_OIDC_AUTHORIZATION_URL=$(echo $OIDC_CONF | jq ".authorization_endpoint" -r)
-IDPORTEN_OIDC_TOKEN_URL=$(echo $OIDC_CONF | jq ".token_endpoint" -r)
-IDPORTEN_OIDC_JWKS_URL=$(echo $OIDC_CONF | jq ".jwks_uri" -r)
-IDPORTEN_OIDC_LOGOUT_URL=$(echo $OIDC_CONF | jq ".end_session_endpoint" -r)
+set
 
 sed -e 's,${IDPORTEN_CLIENT_ID},'$IDPORTEN_CLIENT_ID',g' \
  -e 's,${IDPORTEN_CLIENT_SECRET},'$IDPORTEN_CLIENT_SECRET',g' \
