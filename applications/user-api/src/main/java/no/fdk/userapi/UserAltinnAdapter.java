3913ba33a37971a11ee28105886b5192b49421fa
Markko Paas <markko.paas@gmail.com>
2019-08-14 08:32:10 +0200

    feature(user-api): Apply organisationForm field filter when calculating user privileges

--- applications/user-api/src/main/java/no/fdk/userapi/UserAltinnAdapter.java
+++ applications/user-api/src/main/java/no/fdk/userapi/UserAltinnAdapter.java
@@ -1,14 +1,19 @@
 package no.fdk.userapi;
 
+import no.fdk.altinn.Organisation;
 import no.fdk.altinn.Person;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 
 public class UserAltinnAdapter implements User {
     private Person person;
+    private List<String> INCLUDED_ORGNR = Arrays.asList("920210023"/* “GJELDSREGISTERET AS”*/);
+    private List<String> INCLUDED_ORGFORM = Arrays.asList("ADOS", "FKF", "FYLK", "IKS", "KF", "KIRK", "KOMM", "ORGL", "SF", "STAT", "SÆR");
+    private Predicate<Organisation> organisationPrivilegeFilter = (o) -> INCLUDED_ORGNR.contains(o.getOrganisationNumber()) || INCLUDED_ORGFORM.contains(o.getOrganisationForm());
 
     UserAltinnAdapter(Person person) {
         this.person = person;
@@ -32,10 +37,8 @@ public class UserAltinnAdapter implements User {
     }
 
     public String getPrivileges() {
-        // todo filter orgform or whitelist organisationnumber
-        //  includedOrgforms: ADOS,FKF,FYLK,IKS,KF,KIRK,KOMM,ORGL,SF,STAT,SÆR
-        //  includedOrgnr: 974760673
         List<String> privilegesList = person.getOrganisations().stream()
+            .filter(organisationPrivilegeFilter)
             .map(o -> "publisher:" + o.getOrganisationNumber() + ":admin").collect(Collectors.toList());
         return String.join(" ", privilegesList);
     }
