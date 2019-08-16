5b1ca76ea6240972b2ebc3b72c8726522f022e3f
Markko Paas <markko.paas@gmail.com>
2019-08-16 14:47:50 +0200

    refactor(registration-react,registration-api): #1897 Use sso in registratation

--- applications/registration-react/src/redux/modules/user.js
+++ applications/registration-react/src/redux/modules/user.js
@@ -1,8 +1,5 @@
 import { compose } from 'recompose';
-import { reduxFsaThunk } from '../../lib/redux-fsa-thunk';
-import { getUserProfile } from '../../auth/auth-service';
 
-export const USER_REQUEST = 'USER_REQUEST';
 export const USER_SUCCESS = 'USER_SUCCESS';
 export const USER_FAILURE = 'USER_FAILURE';
 
@@ -13,40 +10,26 @@ export const selectUser = compose(
   selectAuthState
 );
 
-export const selectIsFetching = compose(
-  authState => !!authState.isFetching,
-  selectAuthState
-);
-
-export const getUserProfileThunk = () => (dispatch, getState) =>
-  !selectIsFetching(getState()) &&
-  dispatch(
-    reduxFsaThunk(() => getUserProfile(), {
-      onBeforeStart: { type: USER_REQUEST },
-      onSuccess: { type: USER_SUCCESS },
-      onError: { type: USER_FAILURE }
-    })
-  );
+export const userSuccessAction = ({ user }) => ({
+  type: USER_SUCCESS,
+  payload: user
+});
+export const userFailureAction = ({ error }) => ({
+  type: USER_FAILURE,
+  payload: error
+});
 
 const initialState = {
   user: undefined,
-  isFetching: undefined
+  error: undefined
 };
 
 export default function user(state = initialState, action) {
   switch (action.type) {
-    case USER_REQUEST:
-      return {
-        // do not remove current user while waiting for new, but clear error
-        user: state.user,
-        isFetching: true
-      };
     case USER_SUCCESS:
-      return {
-        user: action.payload
-      };
+      return { user: action.payload };
     case USER_FAILURE:
-      return initialState;
+      return { error: action.payload };
     default:
       return state;
   }
