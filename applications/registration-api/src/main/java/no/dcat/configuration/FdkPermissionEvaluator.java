b4df1a8174b66255bfa094b1858ca427a9eecc9f
Markko Paas <markko.paas@gmail.com>
2019-08-19 13:57:36 +0200

    chore(registration-api): Use logstash json format for logs, to get correct severity flag in google cloud logging.

--- applications/registration-api/src/main/java/no/dcat/configuration/FdkPermissionEvaluator.java
+++ applications/registration-api/src/main/java/no/dcat/configuration/FdkPermissionEvaluator.java
@@ -1,5 +1,7 @@
 package no.dcat.configuration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.access.PermissionEvaluator;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -7,6 +9,8 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import java.io.Serializable;
 
 public class FdkPermissionEvaluator implements PermissionEvaluator {
+    private static final Logger logger = LoggerFactory.getLogger(FdkPermissionEvaluator.class);
+
 
     public boolean hasPermission(Authentication authentication, Object targetDomainObject, Object permission) {
         throw new UnsupportedOperationException("Object permission method not implemented");
@@ -14,7 +18,7 @@ public class FdkPermissionEvaluator implements PermissionEvaluator {
 
     public boolean hasPermission(Authentication authentication, Serializable targetId, String targetType, Object permission) {
         SimpleGrantedAuthority requiredAuthority = new SimpleGrantedAuthority((String) targetId);
-
+        logger.debug("Checking authrorizattion: granted={} required={} ", authentication.getAuthorities(), requiredAuthority);
         return authentication.getAuthorities().contains(requiredAuthority);
     }
 }
