252c558dea8ea7af14a925a26bdacb722a19dc7f
Markko Paas <markko.paas@gmail.com>
2019-08-19 15:15:25 +0200

    sprint-fix(api-cat): Restore ControllerExceptionHandler ControllerAdvice, that got accidentally removed in #8d04c1903

---
+++ applications/api-cat/src/main/java/no/acat/configuration/ControllerExceptionHandler.java
@@ -0,0 +1,8 @@
+package no.acat.configuration;
+
+import no.fdk.webutils.exceptions.GlobalControllerExceptionHandler;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ControllerExceptionHandler extends GlobalControllerExceptionHandler {
+}
