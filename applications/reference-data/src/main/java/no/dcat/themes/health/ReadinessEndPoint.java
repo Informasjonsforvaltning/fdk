266f42e9783af0a3d1a24aed2c26fbb9c809bfbe
Markko Paas <markko.paas@gmail.com>
2019-08-20 08:24:39 +0200

    chore(reference-data): Autoformat ReadinessEndPoint

--- applications/reference-data/src/main/java/no/dcat/themes/health/ReadinessEndPoint.java
+++ applications/reference-data/src/main/java/no/dcat/themes/health/ReadinessEndPoint.java
@@ -1,6 +1,5 @@
 package no.dcat.themes.health;
 
-import no.dcat.shared.DataTheme;
 import no.dcat.themes.service.ThemesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
@@ -8,8 +7,6 @@ import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.boot.actuate.endpoint.web.WebEndpointResponse;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 @Endpoint(id = "readiness")
