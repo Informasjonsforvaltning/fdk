ea927176cbaff99582db79d01d77a999de357900
Alexander Zakharov <alexander.zakharov@capgemini.com>
2019-08-23 12:20:32 +0200

    feat(search): #1674 show references to datasets on concept details page

--- applications/search/src/pages/concept-details-page/resolved-concept-details-page.jsx
+++ applications/search/src/pages/concept-details-page/resolved-concept-details-page.jsx
@@ -1,12 +1,14 @@
 import _ from 'lodash';
 import { resolve } from 'react-resolver';
 import { ConceptDetailsPage } from './concept-details-page';
-import { getConcept } from '../../api/concepts';
+import { getConcept, getDatasets } from '../../api/concepts';
 
 const memoizedGetConcept = _.memoize(getConcept);
+const memoizedGetDatasets = _.memoize(getDatasets);
 
 const mapProps = {
-  conceptItem: props => memoizedGetConcept(props.match.params.id)
+  conceptItem: props => memoizedGetConcept(props.match.params.id),
+  conceptDatasetReferences: props => memoizedGetDatasets(props.match.params.id)
 };
 
 export const ResolvedConceptDetailsPage = resolve(mapProps)(ConceptDetailsPage);
