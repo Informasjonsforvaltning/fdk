1a2dfe7f895d5b7830df639542ad0d646a9d53da
Markko Paas <markko.paas@gmail.com>
2019-08-26 23:01:45 +0200

    refactor(registration-react): Refactor api search to unify search by dataseturi and title

--- applications/registration-react/src/api/apis.js
+++ applications/registration-react/src/api/apis.js
@@ -1,38 +1,7 @@
 import axios from 'axios';
-import qs from 'qs';
 
-export const getAPIByTitlePrefix = async (
-  title = '',
-  orgPath,
-  returnFields
-) => {
-  const queryParams = `title=${title}${orgPath ? `&orgPath=${orgPath}` : ''}${
-    returnFields ? `&returnfields=${returnFields}` : ''
-  }`;
-
-  const url = `/api/apis?${queryParams}`;
-
-  const response = await axios
-    .get(url)
-    .catch(e => console.log(JSON.stringify(e))); // eslint-disable-line no-console
-
-  return response && response.data;
-};
-
-export const getApiByDatasetUri = async (datasetUri, returnFields) => {
-  const query = `${qs.stringify(
-    {
-      dataseturi: datasetUri,
-      returnFields
-    },
-    { addQueryPrefix: true }
-  )}`;
-
-  const url = `/api/apis${query}`;
-
-  const response = await axios
-    .get(url)
-    .catch(e => console.log(JSON.stringify(e))); // eslint-disable-line no-console
-
-  return response && response.data;
-};
+export const searchApis = ({ title, datasetUri, returnFields }) =>
+  axios({
+    url: `/api/apis`,
+    params: { title, dataseturi: datasetUri, returnFields }
+  }).then(r => r.data);
