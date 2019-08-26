af0abc662b5b6f41ef38f6a260880481ac5baa63
Markko Paas <markko.paas@gmail.com>
2019-08-26 17:46:29 +0200

    refactor(registration-react): Route api search though search api.

--- applications/registration-react/src/api/search-api/host.js
+++ applications/registration-react/src/api/search-api/host.js
@@ -8,5 +8,5 @@ but some are also under different root, e.g. reference-data
 For developer convenience, different sub-apis can be configured on different hosts.
 */
 
-export const searchApiGet = ({ url, authorization }) =>
-  axios.get(url, { headers: { authorization } }).then(r => r.data);
+export const searchApiGet = ({ url, params, authorization }) =>
+  axios.get(url, { params, headers: { authorization } }).then(r => r.data);
