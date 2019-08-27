d5048c02b5b142160338466400a8e0e387efb0db
Markko Paas <markko.paas@gmail.com>
2019-08-27 21:32:03 +0200

    feat(registration-react): Read REGISTRATION_API_HOST from environment variable

--- applications/registration-react/src/config.js
+++ applications/registration-react/src/config.js
@@ -4,6 +4,7 @@ const createConfig = env => {
   const searchHost = env.SEARCH_HOST || 'https://fellesdatakatalog.brreg.no';
   const searchApi = {
     host: env.SEARCH_API_HOST || searchHost,
+    // in ut1 and st1, search api requires basic authentication
     authorization: env.SEARCH_API_AUTHORIZATION || undefined
   };
   const defaultToSearchApi = host => (host ? { host } : searchApi);
@@ -17,10 +18,8 @@ const createConfig = env => {
     datasetApi: defaultToSearchApi(env.DATASET_API_HOST),
     conceptApi: defaultToSearchApi(env.CONCEPT_API_HOST),
     publisherApi: defaultToSearchApi(env.PUBLISHER_API_HOST),
-    registrationApi: {
-      // default configuration runs in cluster through proxy, assuming frontend comes from the same origin
-      host: ''
-    },
+    // default configuration runs in cluster through proxy, assuming frontend comes from the same origin
+    registrationApi: { host: env.REGISTRATION_API_HOST || '' },
     keycloak: {
       realm: 'fdk',
       url: `${env.SSO_HOST}/auth`,
