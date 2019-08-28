4eea3599c70380e997c14eef6a3038f61796a1a5
Stig B. Dørmænen <stigbd@gmail.com>
2019-08-28 08:47:26 +0200

    Remove api-cat module

--- applications/api-cat/src/main/java/no/acat/model/queryresponse/AggregationBucket.java
+++
@@ -1,9 +0,0 @@
-package no.acat.model.queryresponse;
-
-import lombok.Data;
-
-@Data(staticConstructor="of")
-public class AggregationBucket {
-    private final String key;
-    private final long count;
-}
