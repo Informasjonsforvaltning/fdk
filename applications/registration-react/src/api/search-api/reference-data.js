a35340e28f8b6a3f8a64fc23900e3ed0c88d2400
Markko Paas <markko.paas@gmail.com>
2019-08-28 16:03:43 +0200

    refactor(registration-react): Use named parameters on searchApiGet

--- applications/registration-react/src/api/search-api/reference-data.js
+++ applications/registration-react/src/api/search-api/reference-data.js
@@ -1,12 +1,8 @@
-import url from 'url';
 import { getConfig } from '../../config';
 import { searchApiGet } from './host';
 
-const getRootUrl = () =>
-  url.resolve(getConfig().referenceDataApi.host, '/reference-data/');
-const getAuthorization = () => getConfig().referenceDataApi.authorization;
-
-const resolvePath = path => url.resolve(getRootUrl(), path);
-
 export const getReferenceData = path =>
-  searchApiGet(resolvePath(path), getAuthorization());
+  searchApiGet({
+    url: `${getConfig().referenceDataApi.host}/reference-data/${path}`,
+    authorization: getConfig().referenceDataApi.authorization
+  });
