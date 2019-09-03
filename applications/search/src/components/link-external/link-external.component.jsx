2c66158b553473ec19feb47036d51b51911f5ad6
Markko Paas <markko.paas@gmail.com>
2019-09-03 09:16:09 +0200

    sprint-fix(search): #2237 allow prefLabel to be translation object

--- applications/search/src/components/link-external/link-external.component.jsx
+++ applications/search/src/components/link-external/link-external.component.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { getTranslateText } from "../../lib/translateText";
 
 export const LinkExternal = ({ uri, prefLabel }) => (
   <a href={uri}>
-    {prefLabel}
+    {getTranslateText(prefLabel)}
     <i className="fa fa-external-link fdk-fa-right" />
   </a>
 );
 
 LinkExternal.defaultProps = {
   uri: '',
-  prefLabel: ''
+  prefLabel: {}
 };
 
 LinkExternal.propTypes = {
   uri: PropTypes.string,
-  prefLabel: PropTypes.string
+  prefLabel: PropTypes.object
 };
