35414a495b8045fe326ead31834ee3d10a155acf
Markko Paas <markko.paas@gmail.com>
2019-09-09 14:17:35 +0200

    conf(search): Read api search host from environment variable

--- applications/search/src/config.js
+++ applications/search/src/config.js
@@ -1,16 +1,27 @@
 import axios from 'axios';
 import { isNapHostname } from './lib/nap-hostname';
 
-const createConfig = env => ({
-  store: { useLogger: env.REDUX_LOG === 'true' },
-  disqusShortname: env.DISQUS_SHORTNAME,
-  filterTransportDatasets:
-    isNapHostname(window.location.hostname) ||
-    !!localStorage.getItem('filterTransportDatasets'),
-  themeNap:
-    isNapHostname(window.location.hostname) ||
-    !!localStorage.getItem('themeNap')
-});
+const createConfig = env => {
+  const searchApi = {
+    host: env.SEARCH_API_HOST || '',
+    // in ut1 and st1, search api requires basic authentication
+    authorization: env.SEARCH_API_AUTHORIZATION || undefined
+  };
+  const defaultToSearchApi = host => (host ? { host } : searchApi);
+
+  return {
+    store: { useLogger: env.REDUX_LOG === 'true' },
+    disqusShortname: env.DISQUS_SHORTNAME,
+    filterTransportDatasets:
+      isNapHostname(window.location.hostname) ||
+      !!localStorage.getItem('filterTransportDatasets'),
+    themeNap:
+      isNapHostname(window.location.hostname) ||
+      !!localStorage.getItem('themeNap'),
+    apiApi:  defaultToSearchApi(env.API_API_HOST)
+  };
+};
+
 const config = createConfig({});
 
 export const getConfig = () => config;
