243051565e372c87f1fac7281ee5ab6a5d78e4e1
Markko Paas <markko.paas@gmail.com>
2019-09-10 00:00:40 +0200

    conf(search): Read informationmodel search host from environment variable

--- applications/search/src/config.js
+++ applications/search/src/config.js
@@ -21,6 +21,7 @@ const createConfig = env => {
     datasetApi: defaultToSearchApi(env.DATASET_API_HOST),
     apiApi:  defaultToSearchApi(env.API_API_HOST),
     conceptApi: defaultToSearchApi(env.CONCEPT_API_HOST),
+    informationmodelApi: defaultToSearchApi(env.INFORMATIONMODEL_API_HOST)
   };
 };
 
