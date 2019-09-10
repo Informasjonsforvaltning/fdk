1f603ca9ecdb55476f5bb8a82b1194404087a91e
Markko Paas <markko.paas@gmail.com>
2019-09-10 21:43:23 +0200

    refactor(search): Move /catalogs api call into catalog api module.

---
+++ applications/search/src/api/catalogs.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import { getConfig } from '../config';
+
+const catalogsUrlBase = () => `${getConfig().catalogApi.host}/catalogs`;
+
+export const getAllCatalogs = () =>
+  axios.get(
+    catalogsUrlBase(),
+    { headers: { authorization: getConfig().catalogApi.authorization } }
+  )
+    .then(r => r.data);
