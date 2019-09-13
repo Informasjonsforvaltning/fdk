bfd3e156e894413ee306a8dd33c75a6bc134f352
Markko Paas <markko.paas@gmail.com>
2019-09-13 13:59:37 +0200

    chore: Fix precomit setup

--- applications/search/src/api/datasets.js
+++ applications/search/src/api/datasets.js
@@ -7,20 +7,21 @@ export const datasetsUrlBase = () =>
   `${getConfig().datasetApi.host}/api/datasets`;
 
 export const datasetsSearch = params => {
-
   // Filter out NAP data if filterTransportDatasets in conf is true
-  if ( getConfig().filterTransportDatasets ) {
-    Object.assign(params,{accessrights: 'PUBLIC',losTheme : 'trafikk-reiser-og-samferdsel'});
+  if (getConfig().filterTransportDatasets) {
+    Object.assign(params, {
+      accessrights: 'PUBLIC',
+      losTheme: 'trafikk-reiser-og-samferdsel'
+    });
   }
 
   return axios
-  .get(datasetsUrlBase(), {
-    params,
-    headers: { authorization: getConfig().datasetApi.authorization }
-  })
-  .then(r => r.data);
-}
-
+    .get(datasetsUrlBase(), {
+      params,
+      headers: { authorization: getConfig().datasetApi.authorization }
+    })
+    .then(r => r.data);
+};
 
 export const getDataset = id =>
   axios
