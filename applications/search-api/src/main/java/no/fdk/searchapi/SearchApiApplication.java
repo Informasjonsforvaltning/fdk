78c50ac74f018127ca1df28c3d300d2c302f0de1
Markko Paas <markko.paas@gmail.com>
2019-09-18 13:17:48 +0200

    chore(search-api): Remove encoded slash support, assuming it was used to suport url as encoded path variable instead of query parameter.

--- applications/search-api/src/main/java/no/fdk/searchapi/SearchApiApplication.java
+++ applications/search-api/src/main/java/no/fdk/searchapi/SearchApiApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SearchApiApplication {
     public static void main(String[] args) {
-        System.setProperty("org.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH", "true");
         SpringApplication.run(SearchApiApplication.class, args);
     }
-
 }
