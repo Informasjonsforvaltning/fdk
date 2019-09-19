3fa2dd1dad3f49b7092b368b2265d86675705f8a
Markko Paas <markko.paas@gmail.com>
2019-09-19 10:30:25 +0200

    fix(search-api): Enable cors support for all endpoints

---
+++ applications/search-api/src/main/java/no/fdk/searchapi/configuration/SecurityConfigurer.java
@@ -0,0 +1,18 @@
+package no.fdk.searchapi.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(final HttpSecurity http) throws Exception {
+        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+    }
+
+}
