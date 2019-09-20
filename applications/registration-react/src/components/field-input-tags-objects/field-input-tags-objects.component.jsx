6123dda9e5ba0c98baa6e781a5ebd1a107779c36
chlenix <gerpol17@gmail.com>
2019-09-20 09:49:58 +0200

    feat(registration-react): #2261 WIP remaking tags fields

--- applications/registration-react/src/components/field-input-tags-objects/field-input-tags-objects.component.jsx
+++ applications/registration-react/src/components/field-input-tags-objects/field-input-tags-objects.component.jsx
@@ -1,34 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import TagsInput from 'react-tagsinput';
+import cx from 'classnames';
+
 import '../field-input-tags/field-input-tags.scss';
 
-const handleChange = (props, tags) => {
-  const { fieldLabel } = props;
-  let updates = [];
-  updates = tags.map(item => ({ [fieldLabel]: item }));
-  props.input.onChange(updates);
-};
-
-const InputTagsFieldArray = props => {
-  const { input, label, fieldLabel, showLabel } = props;
+const InputTagsFieldArray = ({
+  input,
+  label,
+  fieldLabel,
+  showLabel,
+  language
+}) => {
   let tagNodes = [];
 
   if (input && input.value && input.value.length > 0) {
     tagNodes = input.value.map(item => item[fieldLabel]);
   }
+
+  const handleChange = tags => {
+    debugger;
+
+    // We are getting keyword.nb here instead of keyword. @See: multilingual-field-component
+
+    input.onChange(tags.map(item => ({ [language || fieldLabel]: item })));
+  };
+
   return (
-    <div className="pl-2">
+    <div className={cx('pl-2', { 'multilingual-field': !!language })}>
       <label className="fdk-form-label w-100" htmlFor={input.name}>
         {showLabel ? label : null}
-        <div className="d-flex align-items-center">
-          <TagsInput
-            value={tagNodes}
-            className="fdk-reg-input-tags"
-            inputProps={{ placeholder: '' }}
-            onChange={tags => handleChange(props, tags)}
-          />
-        </div>
+        {language && <span className="language-indicator">{language}</span>}
+        <TagsInput
+          value={tagNodes}
+          className="fdk-reg-input-tags"
+          inputProps={{ placeholder: '' }}
+          onChange={handleChange}
+        />
       </label>
     </div>
   );
@@ -37,14 +45,16 @@ const InputTagsFieldArray = props => {
 InputTagsFieldArray.defaultProps = {
   label: null,
   fieldLabel: null,
-  showLabel: false
+  showLabel: false,
+  language: null
 };
 
 InputTagsFieldArray.propTypes = {
   input: PropTypes.object.isRequired,
   label: PropTypes.string,
   fieldLabel: PropTypes.string,
-  showLabel: PropTypes.bool
+  showLabel: PropTypes.bool,
+  language: PropTypes.string
 };
 
 export default InputTagsFieldArray;
