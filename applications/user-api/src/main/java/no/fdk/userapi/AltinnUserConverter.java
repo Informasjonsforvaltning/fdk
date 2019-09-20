3a5abc5d68fa00e9856632b7522e29e8b34808d1
Markko Paas <markko.paas@gmail.com>
2019-09-20 10:40:22 +0200

    feat(user-api): #2351 Add admin role

--- applications/user-api/src/main/java/no/fdk/userapi/AltinnUserConverter.java
+++ applications/user-api/src/main/java/no/fdk/userapi/AltinnUserConverter.java
@@ -14,12 +14,17 @@ import java.util.stream.Collectors;
 public class AltinnUserConverter {
     private String orgNrWhitelistString;
     private String orgFormWhitelistString;
+    @Value("${application.adminList}")
+    private String adminListString;
+
     private Predicate<Organisation> organisationFilter = (o) -> getOrgNrWhitelist().contains(o.getOrganisationNumber()) || getOrgFormWhitelist().contains(o.getOrganisationForm());
 
     AltinnUserConverter(@Value("${application.orgNrWhitelist}") String orgNrWhitelistString,
-                        @Value("${application.orgFormWhitelist}") String orgFormWhitelistString) {
+                        @Value("${application.orgFormWhitelist}") String orgFormWhitelistString,
+                        @Value("${application.adminList}") String adminListString) {
         this.orgNrWhitelistString = orgNrWhitelistString;
         this.orgFormWhitelistString = orgFormWhitelistString;
+        this.adminListString = adminListString;
     }
 
     private List<String> getOrgNrWhitelist() {
@@ -28,6 +33,10 @@ public class AltinnUserConverter {
 
     private List<String> getOrgFormWhitelist() {
         return Arrays.asList(orgFormWhitelistString.split(","));
+    }
+
+    private List<String> getAdminListString() {
+        return Arrays.asList(adminListString.split(","));
     }
 
     User convert(Person person) {
@@ -62,6 +71,10 @@ public class AltinnUserConverter {
             List<String> privilegesList = person.getOrganisations().stream()
                 .filter(organisationFilter)
                 .map(o -> "publisher:" + o.getOrganisationNumber() + ":admin").collect(Collectors.toList());
+
+            if (getAdminListString().contains(this.getId())) {
+                privilegesList.add("admin");
+            }
             return String.join(",", privilegesList);
         }
     }
