e41bb1427329f93e012df41e0ebc163f5a3d1626
Stig B. Dørmænen <stigbd@gmail.com>
2019-09-23 15:26:00 +0200

    Remove concept-cat application

--- applications/concept-cat/src/main/java/no/ccat/config/ControllerExceptionHandler.java
+++
@@ -1,8 +0,0 @@
-package no.ccat.config;
-
-import no.fdk.webutils.exceptions.GlobalControllerExceptionHandler;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-
-@ControllerAdvice
-public class ControllerExceptionHandler extends GlobalControllerExceptionHandler {
-}
--- applications/concept-cat/src/main/java/no/ccat/controller/Common.java
+++
@@ -1,5 +0,0 @@
-package no.ccat.controller;
-
-public class Common {
-    public static final String MISSING = "MISSING";
-}
