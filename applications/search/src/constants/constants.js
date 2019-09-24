f23f3bb46551afb7be09cb5444febc5cc6b372ec
Øyvind Isaksen <oyvindisaks1@gmail.com>
2019-09-24 09:49:47 +0200

    sprint-fix(search): #2395 add external link to 'About nap'

--- applications/search/src/constants/constants.js
+++ applications/search/src/constants/constants.js
@@ -8,4 +8,4 @@ export const PATHNAME_REPORTS = '/reports';
 export const PATHNAME_ABOUT = '/about';
 export const PATHNAME_ABOUT_REGISTRATION = '/about-registration';
 export const HITS_PER_PAGE = 10;
-export const PATHNAME_ABOUT_NAP = '/';
+export const PATHNAME_ABOUT_NAP = 'http://dataut.vegvesen.no/group/about/nap';
