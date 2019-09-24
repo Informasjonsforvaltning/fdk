e1ac62a9b32bcf3fb7e34a2b350848d634dace6a
Markko Paas <markko.paas@gmail.com>
2019-09-24 08:01:33 +0200

    chore(registration-react): #2336 Remove dev-server from container

--- applications/registration-react/server/server.js
+++ applications/registration-react/server/server.js
@@ -4,17 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const compression = require('compression');
-const webpack = require('webpack');
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
-const config = require('../webpack.dev.config.js');
 
 module.exports = {
   start() {
-    const env = {
-      production: process.env.NODE_ENV === 'production'
-    };
-
     const app = express();
     app.use(compression());
     app.set('view engine', 'ejs');
@@ -44,35 +36,14 @@ module.exports = {
       res.json(envObj);
     });
 
-    if (!env.production) {
-      const compiler = webpack(config);
-
-      app.use(
-        webpackMiddleware(compiler, {
-          publicPath: config.output.publicPath,
-          contentBase: 'src',
-          stats: {
-            colors: true,
-            hash: false,
-            timings: true,
-            chunks: false,
-            chunkModules: false,
-            modules: false
-          }
-        })
-      );
-      app.use(webpackHotMiddleware(compiler));
-    } else {
-      app.use('/static', express.static(path.join(__dirname, '/../dist')));
-    }
+    app.use('/static', express.static(path.join(__dirname, '/../dist')));
 
     app.get('*', (req, res) => {
       res.render('index');
     });
 
     app.listen(app.get('port'), () => {
-      console.log('FDK-registration-react lytter på', app.get('port')); // eslint-disable-line no-console
-      console.log('env:', env.production ? 'production' : 'development'); // eslint-disable-line no-console
+      console.log('registration-react started, port:', app.get('port')); // eslint-disable-line no-console
     });
   }
 };
