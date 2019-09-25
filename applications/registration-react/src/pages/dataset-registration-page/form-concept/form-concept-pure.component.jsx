aeb6704a8df0085a506270431d9962261841f5d4
Markko Paas <markko.paas@gmail.com>
2019-09-25 16:57:15 +0200

    refactor(registration-react): refactor errors in FormConceptPure

--- applications/registration-react/src/pages/dataset-registration-page/form-concept/form-concept-pure.component.jsx
+++ applications/registration-react/src/pages/dataset-registration-page/form-concept/form-concept-pure.component.jsx
@@ -1,55 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Field } from 'redux-form';
-import _ from 'lodash';
 
 import localization from '../../../lib/localization';
 import Helptext from '../../../components/helptext/helptext.component';
 import InputTagsFieldConcepts from './input-tags-concepts/input-tags-concepts.component';
 import InputTagsFieldArray from '../../../components/field-input-tags-objects/field-input-tags-objects.component';
 
-export const FormConceptPure = props => {
-  const { syncErrors } = props;
-  return (
-    <form>
-      <div className="form-group">
-        <Helptext
-          title={localization.schema.concept.helptext.content}
-          term="Dataset_content"
-        />
-        <Field
-          name="concepts"
-          type="text"
-          component={InputTagsFieldConcepts}
-          label={localization.schema.concept.conceptLabel}
-          fieldLabel="no"
-        />
-      </div>
-      <div className="form-group">
-        <Helptext
-          title={localization.schema.concept.helptext.keyword}
-          term="Dataset_keyword"
-        />
-        <Field
-          name="keyword"
-          type="text"
-          component={InputTagsFieldArray}
-          label={localization.schema.concept.keywordLabel}
-          fieldLabel={localization.getLanguage()}
-        />
-        {_.get(syncErrors, 'keyword') && (
-          <div className="alert alert-danger mt-3">
-            {_.get(syncErrors, 'keyword')[localization.getLanguage()]}
-          </div>
-        )}
-      </div>
-    </form>
-  );
-};
+export const FormConceptPure = ({ errors }) => (
+  <form>
+    <div className="form-group">
+      <Helptext
+        title={localization.schema.concept.helptext.content}
+        term="Dataset_content"
+      />
+      <Field
+        name="concepts"
+        type="text"
+        component={InputTagsFieldConcepts}
+        label={localization.schema.concept.conceptLabel}
+        fieldLabel="no"
+      />
+    </div>
+    <div className="form-group">
+      <Helptext
+        title={localization.schema.concept.helptext.keyword}
+        term="Dataset_keyword"
+      />
+      <Field
+        name="keyword"
+        type="text"
+        component={InputTagsFieldArray}
+        label={localization.schema.concept.keywordLabel}
+        fieldLabel={localization.getLanguage()}
+      />
+      {errors && errors.keyword && (
+        <div className="alert alert-danger mt-3">
+          {errors.keyword[localization.getLanguage()]}
+        </div>
+      )}
+    </div>
+  </form>
+);
 
 FormConceptPure.defaultProps = {
-  syncErrors: null
+  errors: null
 };
 FormConceptPure.propTypes = {
-  syncErrors: PropTypes.object
+  errors: PropTypes.object
 };
