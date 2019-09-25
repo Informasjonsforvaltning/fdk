6e0195c8e55b23bf0823092d8d6075794d3aebb1
Markko Paas <markko.paas@gmail.com>
2019-09-25 22:48:24 +0200

    refactor(registration-react): Simplify FieldInputTags

--- applications/registration-react/src/components/field-input-tags-objects/field-input-tags-objects.component.jsx
+++ applications/registration-react/src/components/field-input-tags-objects/field-input-tags-objects.component.jsx
@@ -1,34 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import TagsInput from 'react-tagsinput';
+
 import '../field-input-tags/field-input-tags.scss';
 
-const handleChange = (props, tags) => {
-  const { fieldLabel } = props;
-  let updates = [];
-  updates = tags.map(item => ({ [fieldLabel]: item }));
-  props.input.onChange(updates);
-};
-
-const InputTagsFieldArray = props => {
-  const { input, label, fieldLabel, showLabel } = props;
+const InputTagsFieldArray = ({ input, label, fieldLabel, showLabel }) => {
   let tagNodes = [];
 
   if (input && input.value && input.value.length > 0) {
-    tagNodes = input.value.map(item => item[fieldLabel]);
+    tagNodes = (fieldLabel
+      ? input.value.map(item => item[fieldLabel])
+      : input.value
+    ).filter(Boolean);
   }
+
+  const handleChange = tags =>
+    input.onChange(
+      (fieldLabel ? tags.map(item => ({ [fieldLabel]: item })) : tags).filter(
+        Boolean
+      )
+    );
+
   return (
     <div className="pl-2">
       <label className="fdk-form-label w-100" htmlFor={input.name}>
         {showLabel ? label : null}
-        <div className="d-flex align-items-center">
-          <TagsInput
-            value={tagNodes}
-            className="fdk-reg-input-tags"
-            inputProps={{ placeholder: '' }}
-            onChange={tags => handleChange(props, tags)}
-          />
-        </div>
+        <TagsInput
+          value={tagNodes}
+          className="fdk-reg-input-tags"
+          inputProps={{ placeholder: '' }}
+          onChange={handleChange}
+        />
       </label>
     </div>
   );
