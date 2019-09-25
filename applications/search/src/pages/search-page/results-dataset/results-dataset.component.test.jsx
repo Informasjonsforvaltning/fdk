52524add9610ee6e9ba4aa397a04a3c3fb654293
Øyvind Isaksen <oyvindisaks1@gmail.com>
2019-09-25 14:06:28 +0200

    Revert "feat(search): #2339 hide eu-theme filter if only 'ukjent'"
    This reverts commit 86fc8590

--- applications/search/src/pages/search-page/results-dataset/results-dataset.component.test.jsx
+++ applications/search/src/pages/search-page/results-dataset/results-dataset.component.test.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import {
-  ResultsDatasetPure,
-  showEUThemeFilter
-} from './results-dataset.component';
+import { ResultsDatasetPure } from './results-dataset.component';
 import datasetsResponse from '../__fixtures/datasetsApiResponse.json';
 import {
   extractAggregations,
@@ -24,32 +21,4 @@ test('should render ResultsDataset correctly with hits', () => {
   };
   const result = shallow(<ResultsDatasetPure {...props} />);
   expect(result).toMatchSnapshot();
-});
-
-test('should not show filter EU-theme if only one theme type which is "Ukjent"', () => {
-  const buckets = [
-    {
-      key: 'Ukjent'
-    }
-  ];
-  expect(showEUThemeFilter({ buckets })).toBe(false);
-});
-
-test('should show filter EU-theme if contains theme and at least one of them are not of type "Ukjent"', () => {
-  const bucketsWithNoUkjent = [
-    {
-      key: 'TRAN'
-    }
-  ];
-  expect(showEUThemeFilter({ buckets: bucketsWithNoUkjent })).toBe(true);
-
-  const bucketsWithOneUkjent = [
-    {
-      key: 'Ukjent'
-    },
-    {
-      key: 'TRAN'
-    }
-  ];
-  expect(showEUThemeFilter({ buckets: bucketsWithOneUkjent })).toBe(true);
 });
