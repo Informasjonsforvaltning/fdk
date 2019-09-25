13cf66669390b1eaa5b76a2f44d1ab39ecf2a1e1
Markko Paas <markko.paas@gmail.com>
2019-09-25 11:56:37 +0200

    chore(registration-react): Simplify ConnectedFormTitle

--- applications/registration-react/src/pages/dataset-registration-page/form-title/connected-form-title.component.jsx
+++ applications/registration-react/src/pages/dataset-registration-page/form-title/connected-form-title.component.jsx
@@ -2,19 +2,14 @@ import { getFormSyncErrors } from 'redux-form';
 import { connect } from 'react-redux';
 import _ from 'lodash';
 
-import localization from '../../../lib/localization';
-import { textType, emptyArray } from '../../../schemaTypes';
+import { emptyArray } from '../../../schemaTypes';
 import { ConfiguredFormTitle } from './configured-form-title';
 
 const mapStateToProps = (state, { datasetItem = {} }) => ({
   initialValues: {
     title: datasetItem.title || {},
     description: datasetItem.description || {},
-    objective:
-      _.get(datasetItem, ['objective', localization.getLanguage()], []).length >
-      0
-        ? _.get(datasetItem, 'objective')
-        : textType,
+    objective: datasetItem.objective || {},
     landingPage: _.get(datasetItem, 'landingPage', emptyArray)
   },
   syncErrors: getFormSyncErrors('title')(state)
