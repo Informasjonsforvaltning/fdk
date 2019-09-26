579499e6c3b3f2b3841a399665acf7886abeefdb
Alexander Zakharov <alexander.zakharov@capgemini.com>
2019-09-26 15:42:55 +0200

    refactor(registration-react): move information model render to own component

--- applications/registration-react/src/pages/dataset-registration-page/form-informationmodel/form-informationmodel-pure.test.jsx
+++ applications/registration-react/src/pages/dataset-registration-page/form-informationmodel/form-informationmodel-pure.test.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import {
-  FormInformationModelPure,
-  renderInformationModel
-} from './form-informationmodel-pure.component';
+import { FormInformationModelPure } from './form-informationmodel-pure.component';
 
 let defaultProps;
 let wrapper;
@@ -14,10 +11,5 @@ beforeEach(() => {
 });
 
 test('should render FormInformationModelPure, { renderS correctly', () => {
-  expect(wrapper).toMatchSnapshot();
-});
-
-test('should render renderInformationModel correctly', () => {
-  wrapper = shallow(renderInformationModel(defaultProps));
   expect(wrapper).toMatchSnapshot();
 });
