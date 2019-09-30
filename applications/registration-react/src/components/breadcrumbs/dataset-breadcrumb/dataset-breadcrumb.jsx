da56e8d7ef61ddb0d860f5293156ce964995ac38
Øyvind Isaksen <oyvindisaks1@gmail.com>
2019-09-30 09:46:10 +0200

    feat(registration-react): change breadcrumb texts and update designsystemet v1.4.24

--- applications/registration-react/src/components/breadcrumbs/dataset-breadcrumb/dataset-breadcrumb.jsx
+++ applications/registration-react/src/components/breadcrumbs/dataset-breadcrumb/dataset-breadcrumb.jsx
@@ -5,10 +5,11 @@ import _ from 'lodash';
 
 import { getDatasetItemByDatasetiId } from '../../../redux/modules/datasets';
 import { getTranslateText } from '../../../lib/translateText';
+import localization from "../../../lib/localization";
 
 export const PuredatasetBreadcrumb = props => {
   const { datasetItem } = props;
-  return <span>{getTranslateText(_.get(datasetItem, 'title'))}</span>;
+  return <span>{getTranslateText(_.get(datasetItem, 'title')) || localization.breadcrumbs.datasetRegistration}</span>;
 };
 
 const mapStateToProps = ({ datasets }, ownProps) => {
