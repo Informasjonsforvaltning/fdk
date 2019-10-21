c5340b92e152c7a6e69f209654a5d3837806e45d
Markko Paas <markko.paas@gmail.com>
2019-10-21 11:36:24 +0200

    fix(user-api): #2551 Increase size of response from altinn reportees query

--- libraries/altinn-client/src/main/java/no/fdk/altinn/AltinnClient.java
+++ libraries/altinn-client/src/main/java/no/fdk/altinn/AltinnClient.java
@@ -44,7 +44,7 @@ public class AltinnClient {
     private List<Subject> getReportees(String socialSecurityNumber) {
         RestTemplate restTemplate = new RestTemplate();
 
-        String reporteesUrlTemplate = altinnProxyHost + "/api/serviceowner/reportees?ForceEIAuthentication&subject={subject}&servicecode=4814&serviceedition=1";
+        String reporteesUrlTemplate = altinnProxyHost + "/api/serviceowner/reportees?ForceEIAuthentication&subject={subject}&servicecode=4814&serviceedition=1&$top=1000";
         Map<String, String> params = ImmutableMap.of("subject", socialSecurityNumber);
 
         return restTemplate.exchange(reporteesUrlTemplate, GET, null, new ParameterizedTypeReference<List<Subject>>() {
