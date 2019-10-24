a27d80be4e8453cb485d3c50f826003d6f72b970
Markko Paas <markko.paas@gmail.com>
2019-10-24 08:26:42 +0200

    refactor(user-api): user enums when generating authorities

---
+++ applications/user-api/src/main/java/no/fdk/userapi/ResourceRole.java
@@ -0,0 +1,41 @@
+package no.fdk.userapi;
+
+public class ResourceRole {
+    final static ResourceRole ROOT_ADMIN = new ResourceRole(ResourceType.SYSTEM, "root", Role.ADMIN);
+    private ResourceType resourceType;
+    private String resourceId;
+    private Role role;
+
+    ResourceRole(ResourceType resourceType,
+                 String resourceId,
+                 Role role) {
+        this.resourceType = resourceType;
+        this.resourceId = resourceId;
+        this.role = role;
+    }
+
+    public String toString() {
+        return this.resourceType + ":" + this.resourceId + ":" + this.role;
+    }
+
+    public enum ResourceType {
+        PUBLISHER {
+            public String toString() {
+                return "publisher";
+            }
+        },
+        SYSTEM {
+            public String toString() {
+                return "system";
+            }
+        }
+    }
+
+    public enum Role {
+        ADMIN {
+            public String toString() {
+                return "admin";
+            }
+        }
+    }
+}
