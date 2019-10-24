17b4b52a398d853065768ab0a3c0b6925704cb40
Markko Paas <markko.paas@gmail.com>
2019-10-24 23:32:50 +0200

    refactor(sso): use rest-user-attribute-idp-mapper to fetch user name for idporten users

--- applications/sso/buildDocker.sh
+++ applications/sso/buildDocker.sh
@@ -8,6 +8,9 @@ echo "Start build libraries/keycloak-user-storage-rest";
 (cd ../.. && mvn clean package -B -T 2C --projects libraries/keycloak-user-storage-rest --also-make)
 cp ../../libraries/keycloak-user-storage-rest/target/keycloak-user-storage-rest.jar deployments/
 
+(cd modules/rest-user-mapper && mvn clean package)
+cp modules/rest-user-mapper/target/rest-user-mapper.jar deployments/
+
 echo "SECONDS"
 echo $SECONDS
 
