3a9c93d071e91f374fc38044661b8a7e098a1a20
Alexander Zakharov <alexander.zakharov@capgemini.com>
2019-11-04 10:38:59 +0100

    feat(sso): #2465 add whitelisted email validator to Keycloak registration

--- applications/sso/buildDocker.sh
+++ applications/sso/buildDocker.sh
@@ -8,6 +8,9 @@ echo "Start build libraries/keycloak-user-storage-rest";
 (cd modules/rest-user-mapper && mvn clean package)
 cp modules/rest-user-mapper/target/rest-user-mapper.jar deployments/
 
+(cd modules/whitelisted-email-validator && mvn clean package)
+cp modules/whitelisted-email-validator/target/whitelisted-email-validator.jar deployments/
+
 echo "SECONDS"
 echo $SECONDS
 
