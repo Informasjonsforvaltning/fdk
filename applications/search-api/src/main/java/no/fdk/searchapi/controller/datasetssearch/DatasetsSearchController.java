1cb7613381cfff6abca6997a897e71d0c59253f7
Frode Roxrud Gill <extfrg@brreg.no>
2019-11-12 14:00:03 +0100

    RSS/ATOM proof of concept

--- applications/search-api/src/main/java/no/fdk/searchapi/controller/datasetssearch/DatasetsSearchController.java
+++ applications/search-api/src/main/java/no/fdk/searchapi/controller/datasetssearch/DatasetsSearchController.java
@@ -1,10 +1,19 @@
 package no.fdk.searchapi.controller.datasetssearch;
 
+import com.rometools.rome.feed.atom.Content;
+import com.rometools.rome.feed.atom.Entry;
+import com.rometools.rome.feed.atom.Feed;
+import com.rometools.rome.feed.atom.Person;
+import com.rometools.rome.feed.rss.Channel;
+import com.rometools.rome.feed.rss.Guid;
+import com.rometools.rome.feed.rss.Item;
+import com.rometools.rome.feed.synd.SyndPerson;
 import no.dcat.client.referencedata.ReferenceDataClient;
 import no.fdk.searchapi.service.ElasticsearchService;
 import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.sort.SortBuilder;
 import org.elasticsearch.search.sort.SortBuilders;
 import org.elasticsearch.search.sort.SortOrder;
@@ -83,6 +92,15 @@ public class DatasetsSearchController {
         int from = checkAndAdjustFrom((int) pageable.getOffset());
         int size = checkAndAdjustSize(pageable.getPageSize());
 
+        SearchResponse response = performElasticSearchSearch(params, lang, sortfield, sortdirection, aggregations, from, size);
+
+        return new ResponseEntity<>(response.toString(), HttpStatus.OK);
+    }
+
+    private SearchResponse performElasticSearchSearch(final Map<String, String> params, final String lang,
+                                                      final String sortfield, final String sortdirection,
+                                                      final String aggregations,
+                                                      final int from, final int size) {
         if (hasLOSFilter(params)) {
             //Since the builder is static, it can not request expanded LOS expressions
             //from reference-data, so we do it here instead.
@@ -140,9 +158,7 @@ public class DatasetsSearchController {
         logger.debug("Executing query: {}", searchBuilder.toString());
         SearchResponse response = searchBuilder.execute().actionGet();
         logger.trace("Search response: {}", response.toString());
-
-        // return response
-        return new ResponseEntity<>(response.toString(), HttpStatus.OK);
+        return response;
     }
 
     @RequestMapping(value = "/datasets/search", method = RequestMethod.POST, produces = "application/json")
@@ -174,6 +190,96 @@ public class DatasetsSearchController {
         body.forEach((key, value) -> mergedParams.put(key, value.toString()));
 
         return search(mergedParams, lang, sortfield, sortdirection, aggregations, pageable);
+    }
+
+    /**
+     * RSS
+     *
+     * @return The most recent datasets as RSS
+     */
+    @RequestMapping(value = "/datasets", method = RequestMethod.GET, produces = "application/rss+xml")
+    public Channel rss(@RequestParam Map<String, String> params) {
+        logger.debug("RSS GET /datasets?{}", params);
+
+        Channel channel = new Channel();
+        channel.setFeedType("rss_2.0");
+
+        int from = 0;
+        int size = 50;
+
+        SearchResponse response = performElasticSearchSearch(params, null, "modified", "desc", null, from, size);
+
+        List<Item> items = new ArrayList<>();
+        Iterator<SearchHit> iter = response.getHits().iterator();
+        while (iter.hasNext()) {
+            SearchHit searchHit = iter.next();
+
+            Item item = new Item();
+            item.setUri("https://www.fellesdatakatalog.brreg.no/datasets/"+searchHit.getId());
+
+            Guid guid = new Guid();
+            guid.setValue(searchHit.getId());
+            guid.setPermaLink(true);
+            item.setGuid(guid);
+
+            item.setPubDate(searchHit.getField("harvest.firstHarvested").getValue());
+
+            item.setTitle(searchHit.getField("title.nb").getValue());
+
+            item.setDescription(searchHit.getField("description.nb").getValue());
+
+            item.setAuthor(searchHit.getField("publisher.name").getValue());
+
+            items.add(item);
+        }
+        channel.setItems(items);
+
+        return channel;
+    }
+
+    /**
+     * Atom
+     *
+     * @return The most recent datasets as Atom
+     */
+    @RequestMapping(value = "/datasets", method = RequestMethod.GET, produces = "application/atom+xml")
+    public Feed atom(@RequestParam Map<String, String> params) {
+        logger.debug("ATOM GET /datasets?{}", params);
+
+        Feed feed = new Feed();
+        feed.setFeedType("atom_1.0");
+
+        int from = 0;
+        int size = 50;
+
+        SearchResponse response = performElasticSearchSearch(params, null, "modified", "desc", null, from, size);
+
+        List<Entry> entries = new ArrayList<>();
+        Iterator<SearchHit> iter = response.getHits().iterator();
+        while (iter.hasNext()) {
+            SearchHit searchHit = iter.next();
+
+            Entry entry = new Entry();
+
+            entry.setId(searchHit.getId());
+
+            entry.setPublished(searchHit.getField("harvest.firstHarvested").getValue());
+
+            entry.setTitle(searchHit.getField("title.nb").getValue());
+
+            Content content = new Content();
+            content.setValue(searchHit.getField("description.nb").getValue());
+            entry.setSummary(content);
+
+            SyndPerson author = new Person();
+            author.setName(searchHit.getField("publisher.name").getValue());
+            entry.setAuthors(Collections.singletonList(author));
+
+            entries.add(entry);
+        }
+        feed.setEntries(entries);
+
+        return feed;
     }
 
     private int checkAndAdjustFrom(int from) {
