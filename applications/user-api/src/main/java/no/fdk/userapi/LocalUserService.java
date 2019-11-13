7e973a6e01054a724d6a019f922f58fe1fae17e9
Markko Paas <markko.paas@gmail.com>
2019-11-13 08:42:53 +0100

    chore(user-api): #2625 remove resource type "publisher"

--- applications/user-api/src/main/java/no/fdk/userapi/LocalUserService.java
+++ applications/user-api/src/main/java/no/fdk/userapi/LocalUserService.java
@@ -9,13 +9,11 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static no.fdk.userapi.ResourceRole.ResourceType.organization;
-import static no.fdk.userapi.ResourceRole.ResourceType.publisher;
 import static no.fdk.userapi.ResourceRole.Role.read;
 
 @Service
@@ -35,7 +33,8 @@ public class LocalUserService {
             try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
                 String jsonBody = reader.lines().collect(Collectors.joining(System.lineSeparator()));
                 ObjectMapper mapper = new ObjectMapper();
-                return mapper.readValue(jsonBody, new TypeReference<List<String>>() {});
+                return mapper.readValue(jsonBody, new TypeReference<List<String>>() {
+                });
             }
         } catch (Exception e) {
             return Collections.emptyList();
@@ -47,21 +46,10 @@ public class LocalUserService {
 
         List<String> organizations = getOrganizationsAssociatedWithDomain(domain);
 
-        List<String> resourceRoleTokens = new ArrayList<>();
-
-        resourceRoleTokens.addAll(
-            organizations.stream()
-                .map(o -> new ResourceRole(publisher, o, read))
-                .map(Object::toString)
-                .collect(Collectors.toList())
-        );
-
-        resourceRoleTokens.addAll(
-            organizations.stream()
-                .map(o -> new ResourceRole(organization, o, read))
-                .map(Object::toString)
-                .collect(Collectors.toList())
-        );
+        List<String> resourceRoleTokens = organizations.stream()
+            .map(o -> new ResourceRole(organization, o, read))
+            .map(Object::toString)
+            .collect(Collectors.toList());
 
         return String.join(",", resourceRoleTokens);
     }
