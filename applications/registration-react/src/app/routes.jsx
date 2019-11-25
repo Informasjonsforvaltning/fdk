785cb99160eb8bd2c46e1803b72f9a0d9eaf6741
Markko Paas <markko.paas@gmail.com>
2019-11-26 00:19:03 +0100

    refactor(registration-react): extract catalog routes

--- applications/registration-react/src/app/routes.jsx
+++ applications/registration-react/src/app/routes.jsx
@@ -4,11 +4,7 @@ import { Redirect, Route, Switch } from 'react-router';
 import { LoginPage } from '../pages/login-page/login-page';
 import { ProtectedRoute } from '../app-protected-route/app-protected-route.component';
 import { CatalogsPage } from '../pages/catalogs-page/catalogs-page';
-import { DatasetsListPage } from '../pages/dataset-list-page/dataset-list-page';
-import { DatasetRegistrationPage } from '../pages/dataset-registration-page/dataset-registration-page';
-import { ConnectedAPIListPage } from '../pages/api-list-page/connected-api-list-page';
-import { ConnectedApiImportPage } from '../pages/api-import-page/connected-api-import-page';
-import { ApiRegistrationPage } from '../pages/api-registration-page/api-registration-page';
+import { CatalogRoutes } from './catalog-routes';
 
 export const Routes = () => (
   <Switch>
@@ -20,30 +16,6 @@ export const Routes = () => (
     <Route exact path="/login" render={props => <LoginPage {...props} />} />
     <Redirect exact from="/" to="/catalogs" />
     <ProtectedRoute exact path="/catalogs" component={CatalogsPage} />
-    <ProtectedRoute
-      exact
-      path="/catalogs/:catalogId/datasets"
-      component={DatasetsListPage}
-    />
-    <ProtectedRoute
-      exact
-      path="/catalogs/:catalogId/datasets/:datasetId"
-      component={DatasetRegistrationPage}
-    />
-    <ProtectedRoute
-      exact
-      path="/catalogs/:catalogId/apis"
-      component={ConnectedAPIListPage}
-    />
-    <ProtectedRoute
-      exact
-      path="/catalogs/:catalogId/apis/import"
-      component={ConnectedApiImportPage}
-    />
-    <ProtectedRoute
-      exact
-      path="/catalogs/:catalogId/apis/:apiId"
-      component={ApiRegistrationPage}
-    />
+    <ProtectedRoute path="/catalogs/:catalogId" component={CatalogRoutes} />
   </Switch>
 );
