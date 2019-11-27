29c7974b4069b3809332e2dbf975f9b8f902604f
Gjermund Skobba <gjermund@skobba.net>
2019-11-27 09:52:44 +0100

    feat(registration-react): removed language buttons for readonly user

--- applications/registration-react/src/pages/dataset-registration-page/dataset-registration-page.js
+++ applications/registration-react/src/pages/dataset-registration-page/dataset-registration-page.js
@@ -3,13 +3,19 @@ import { compose, withProps } from 'recompose';
 import { DatasetRegistrationPagePure } from './dataset-registration-page-pure';
 import { datasetRegistrationConnector } from './dataset-registration-connector';
 import { datasetRegistrationnResolver } from './dataset-registration-resolver';
+import { hasOrganizationAdminPermission } from '../../auth/auth-service';
 
 const mapRouteParams = withProps(({ match: { params } }) =>
   _.pick(params, ['catalogId', 'datasetId'])
 );
 
+const withReadOnly = withProps(catalogId => ({
+  isReadOnly: !hasOrganizationAdminPermission(catalogId)
+}));
+
 const enhance = compose(
   mapRouteParams,
+  withReadOnly,
   datasetRegistrationConnector,
   datasetRegistrationnResolver
 );
