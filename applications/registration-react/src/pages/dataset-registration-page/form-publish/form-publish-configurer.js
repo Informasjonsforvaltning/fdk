52f312395943e1236316591b00d96cc6edd3f0d8
Øyvind Isaksen <oyvindisaks1@gmail.com>
2019-11-27 19:18:19 +0100

    refactor(registration-react): dataset-registration - use decorator pattern at FormPublish

--- applications/registration-react/src/pages/dataset-registration-page/form-publish/form-publish-configurer.js
+++ applications/registration-react/src/pages/dataset-registration-page/form-publish/form-publish-configurer.js
@@ -1,11 +1,7 @@
 import { reduxForm } from 'redux-form';
-import { FormPublishPure } from '../../../components/form-publish/form-publish-pure.component';
-
 import { asyncValidateDatasetInvokePatch } from '../formsLib/asyncValidateDatasetInvokePatch';
 
-const config = {
+export const formPublishConfigurer = reduxForm({
   form: 'datasetPublish',
   asyncValidate: asyncValidateDatasetInvokePatch
-};
-
-export const ConfiguredFormPublish = reduxForm(config)(FormPublishPure);
+});
