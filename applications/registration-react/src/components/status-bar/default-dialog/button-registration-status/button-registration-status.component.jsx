fcdc2f8c1b4aea4b91c24156d70d321a4bb7c6b9
Øyvind Isaksen <oyvindisaks1@gmail.com>
2019-11-28 15:44:40 +0100

    refactor(registration-react): use onChange function directly, remove handleChange function

--- applications/registration-react/src/components/status-bar/default-dialog/button-registration-status/button-registration-status.component.jsx
+++ applications/registration-react/src/components/status-bar/default-dialog/button-registration-status/button-registration-status.component.jsx
@@ -4,10 +4,6 @@ import noop from 'lodash/noop';
 import { Button } from 'reactstrap';
 import localization from '../../../../services/localization';
 
-const handleChange = (onChange, registrationStatus) => {
-  onChange(registrationStatus);
-};
-
 const renderToggleShowValidationButton = onShowValidationError => (
   <Button
     id="dataset-setPublish-button"
@@ -31,7 +27,7 @@ export const ButtonRegistrationStatus = ({
         id="dataset-setPublish-button"
         className="fdk-button mr-3"
         color="primary"
-        onClick={() => handleChange(onChange, 'PUBLISH')}
+        onClick={() => onChange('PUBLISH')}
       >
         Publiser
       </Button>
@@ -44,7 +40,7 @@ export const ButtonRegistrationStatus = ({
     <Button
       id="dataset-setDraft-button"
       className="fdk-button shadow-none bg-transparent btn-outline-primary mr-3"
-      onClick={() => handleChange(onChange, 'DRAFT')}
+      onClick={() => onChange('DRAFT')}
     >
       Avpubliser
     </Button>
