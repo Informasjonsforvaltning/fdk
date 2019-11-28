97198be6229f95466429636281b22f1772af5ad8
Øyvind Isaksen <oyvindisaks1@gmail.com>
2019-11-28 15:46:02 +0100

    feat(registration-react): #2757 add 'approve' status to StatusBar

--- applications/registration-react/src/lib/registration-status.js
+++ applications/registration-react/src/lib/registration-status.js
@@ -1,4 +1,12 @@
 const REGISTRATION_STATUS_PUBLISH = 'PUBLISH';
+const REGISTRATION_STATUS_APPROVE = 'APPROVE';
+const REGISTRATION_STATUS_DRAFT = 'DRAFT';
 
 export const isPublished = registrationStatus =>
   !!(registrationStatus === REGISTRATION_STATUS_PUBLISH);
+
+export const isApproved = registrationStatus =>
+  !!(registrationStatus === REGISTRATION_STATUS_APPROVE);
+
+export const isDraft = registrationStatus =>
+  !!(registrationStatus === REGISTRATION_STATUS_DRAFT);
