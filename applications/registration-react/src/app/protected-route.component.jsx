441efab48ae6dd1760c3fd7b517c0a0411136e82
Markko Paas <markko.paas@gmail.com>
2019-11-30 21:36:47 +0100

    refactor(registration-react): perform logout instead of reauthorization on the protected route authorization error, to avoid infinite login loop.

--- applications/registration-react/src/app/protected-route.component.jsx
+++ applications/registration-react/src/app/protected-route.component.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Redirect, Route } from 'react-router-dom';
 import T from 'lodash/fp/T';
-
-import { isAuthenticated, reauthenticateDueToTimeout, reauthenticateDueToUnauthenticated } from '../auth/auth-service';
+import {
+  isAuthenticated,
+  logout,
+  reauthenticateDueToTimeout,
+  reauthenticateDueToUnauthenticated
+} from '../auth/auth-service';
 import { Timeout } from './timeout.component';
 
 const TIMEOUT = 27.5 * 60 * 1000;
@@ -13,8 +17,13 @@ export const ProtectedRoute = ({ check, ...props }) => {
     computedMatch: { params }
   } = props;
 
-  if (!(isAuthenticated() && check(params))) {
+  if (!isAuthenticated()) {
     reauthenticateDueToUnauthenticated();
+    return <Redirect to="/login" />; // render preemptively to reduce flicker
+  }
+
+  if (!check(params)) {
+    logout();
     return <Redirect to="/login" />; // render preemptively to reduce flicker
   }
 
