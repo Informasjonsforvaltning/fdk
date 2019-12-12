c2142ed66c063bc0dbf9d4d138017bbb0b47268b
Gjermund Skobba <gjermund@skobba.net>
2019-12-12 12:24:32 +0100

    feat(registration-react): readonly fields on form-informationmodel

--- applications/registration-react/src/pages/dataset-registration-page/form-informationmodel/information-model/information-model.component.jsx
+++ applications/registration-react/src/pages/dataset-registration-page/form-informationmodel/information-model/information-model.component.jsx
@@ -4,14 +4,22 @@ import { Field } from 'redux-form';
 
 import InputField from '../../../../components/fields/field-input/field-input.component';
 import MultilingualField from '../../../../components/multilingual-field/multilingual-field.component';
+import LinkReadonlyField from '../../../../components/fields/field-link-readonly/field-link-readonly.component';
+import InputFieldReadonly from '../../../../components/fields/field-input-readonly/field-input-readonly.component';
 
-const InformationModel = ({ fields, titleLabel, linkLabel, languages }) =>
+const InformationModel = ({
+  fields,
+  titleLabel,
+  linkLabel,
+  languages,
+  isReadOnly
+}) =>
   fields &&
   fields.map((item, index) => (
     <div className="d-flex flex-column mb-5" key={index}>
       <MultilingualField
         name={`${item}.prefLabel`}
-        component={InputField}
+        component={isReadOnly ? InputFieldReadonly : InputField}
         label={titleLabel}
         showLabel
         languages={languages}
@@ -19,7 +27,7 @@ const InformationModel = ({ fields, titleLabel, linkLabel, languages }) =>
       <div className="mt-2">
         <Field
           name={`${item}.uri`}
-          component={InputField}
+          component={isReadOnly ? LinkReadonlyField : InputField}
           label={linkLabel}
           showLabel
         />
@@ -31,14 +39,16 @@ InformationModel.defaultProps = {
   fields: null,
   titleLabel: '',
   linkLabel: '',
-  languages: []
+  languages: [],
+  isReadOnly: false
 };
 
 InformationModel.propTypes = {
   fields: PropTypes.object,
   titleLabel: PropTypes.string,
   linkLabel: PropTypes.string,
-  languages: PropTypes.array
+  languages: PropTypes.array,
+  isReadOnly: PropTypes.bool
 };
 
 export default InformationModel;
