82b18e31608cbaee28d12546a73bdea2596db492
Markko Paas <markko.paas@gmail.com>
2019-12-12 14:22:56 +0100

    chore(registration-react): default allowDelegatedRegistration in case organization api is down

--- applications/registration-react/src/pages/dataset-registration-page/dataset-registration-resolver.js
+++ applications/registration-react/src/pages/dataset-registration-page/dataset-registration-resolver.js
@@ -2,7 +2,10 @@ import { resolve } from 'react-resolver';
 import { memoizedGetOrganization } from '../../services/api/organization-api/host';
 
 const mapProps = {
-  organization: props => memoizedGetOrganization(props.catalogId)
+  allowDelegatedRegistration: props =>
+    memoizedGetOrganization(props.catalogId)
+      .then(o => o.allowDelegatedRegistration)
+      .catch(() => false)
 };
 
 export const datasetRegistrationnResolver = resolve(mapProps);
