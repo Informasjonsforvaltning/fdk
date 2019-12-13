2415c85cc4df7ef816bb6f015295f1ee4dd2dd66
Gjermund Skobba <gjermund@skobba.net>
2019-12-13 12:59:41 +0100

    feat(registration-react): readonly fields on form-sample

--- applications/registration-react/src/pages/dataset-registration-page/dataset-registration-page-pure.jsx
+++ applications/registration-react/src/pages/dataset-registration-page/dataset-registration-page-pure.jsx
@@ -398,6 +398,7 @@ export function DatasetRegistrationPagePure(props) {
                   catalogId={catalogId}
                   datasetId={datasetId}
                   languages={languages}
+                  isReadOnly={isReadOnly}
                 />
               </FormTemplateWithState>
               {!isReadOnly && (
