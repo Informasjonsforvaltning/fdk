ad1091ac85cefa380b55aa301c09cea3ae85ee55
NilsOveTen <nils.ove.tendenes@acando.no>
2019-12-16 16:10:09 +0100

    chore: remove sso

--- applications/sso/tools/wait_for_http_200.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-url=($1)
-echo "Waiting 200 from " $url
-while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' $url)" != "200" ]];
-    do sleep 5;
-    echo "Retrying " $url;
-done
-echo "Success 200 from " $url
