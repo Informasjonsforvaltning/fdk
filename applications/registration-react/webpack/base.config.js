8757422785438834230ca5d176347aa78c003ea3
Markko Paas <markko.paas@gmail.com>
2019-12-16 20:52:27 +0100

    chore(registration-react): separate common webpack configuration to base file

---
+++ applications/registration-react/webpack/base.config.js
@@ -0,0 +1,78 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+
+export default {
+  entry: ['@babel/polyfill', './src/index.jsx'],
+  output: {
+    path: path.join(__dirname, '../dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'resolve-url-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              sourceMapContents: false
+            }
+          }
+        ]
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.(png|jpg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: 10000 } // Convert images < 10k to base64 strings
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      react: path.resolve('./node_modules/react')
+    },
+    extensions: ['.js', '.jsx', '.webpack.js', '.web.js']
+  },
+  resolveLoader: {
+    modules: [__dirname, 'node_modules']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: true
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css'
+    }),
+    new CopyWebpackPlugin(
+      [{ from: './src/img/*', to: './img', flatten: true }],
+      {
+        copyUnmodified: true
+      }
+    )
+  ]
+};
