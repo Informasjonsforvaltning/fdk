6a9892799d1e643bdc60b4b638c2fd2359e95ef3
Markko Paas <markko.paas@gmail.com>
2019-12-18 20:51:18 +0100

    chore(registration-react): fix accessibility of filter-pills-los.component.jsx

--- applications/registration-react/src/pages/dataset-registration-page/form-los/form-helper.js
+++ applications/registration-react/src/pages/dataset-registration-page/form-los/form-helper.js
@@ -1,11 +1,15 @@
+export const removeValueFromInputValueArray = (input, removedUri) => {
+  const newInputValue = input.value.filter(
+    inputValue => inputValue.uri !== removedUri
+  );
+  input.onChange(newInputValue);
+};
+
 export const handleUpdateField = (input, event) => {
   const selectedItemURI = event.target.value;
   // Skal fjerne fra array
   if (!event.target.checked) {
-    const newInput = input.value.filter(
-      returnableObjects => returnableObjects.uri !== selectedItemURI
-    );
-    input.onChange(newInput);
+    removeValueFromInputValueArray(input, selectedItemURI);
   } else {
     // add object
     let updates = [];
