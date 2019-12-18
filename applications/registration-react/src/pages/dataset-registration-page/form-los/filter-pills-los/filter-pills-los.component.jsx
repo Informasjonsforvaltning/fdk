114a648801d7b26b30d05f8780d3f3b50b5ad1a4
Markko Paas <markko.paas@gmail.com>
2019-12-18 20:51:18 +0100

    chore(registration-react): fix accessibility of filter-pills-los.component.jsx

--- applications/registration-react/src/pages/dataset-registration-page/form-los/filter-pills-los/filter-pills-los.component.jsx
+++ applications/registration-react/src/pages/dataset-registration-page/form-los/filter-pills-los/filter-pills-los.component.jsx
@@ -1,54 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import _ from 'lodash';
 
 import { getTranslateText } from '../../../../services/translateText';
 import { getLosItemsFromInput } from '../field-tree-los/field-tree-los-helper';
-import { handleUpdateField } from '../form-helper';
+import { removeValueFromInputValueArray } from '../form-helper';
 import './filter-pills-los.scss';
 
 const renderFilterPills = (input, losItems) => {
   const chosenLosItems = getLosItemsFromInput(input, losItems);
   return (
     chosenLosItems &&
-    chosenLosItems.map((item, index) => {
-      if (item !== undefined) {
-        let inputRef;
-        return (
-          <div key={`filter-${index}-${_.get(item, 'uri')}`}>
-            <label
-              className="mr-2 mb-1 fdk-badge badge badge-secondary fdk-text-size-15"
-              onKeyPress={() => {
-                inputRef.click();
-              }}
-              tabIndex="0"
-              role="button"
-              htmlFor={_.get(item, 'uri')}
-            >
-              <input
-                className="badge"
-                ref={input => {
-                  inputRef = input;
-                }}
-                type="text"
-                defaultValue={_.get(item, 'uri')}
-                checked={false}
-                readOnly
-                onClick={e => {
-                  handleUpdateField(input, e);
-                }}
-                label={getTranslateText(_.get(item, 'name'))}
-                id={_.get(item, 'uri')}
-              />
-              <span className="fdk-filter-pill">
-                {getTranslateText(_.get(item, 'name'))}
-              </span>
-            </label>
-          </div>
-        );
-      }
-      return null;
-    })
+    chosenLosItems.filter(Boolean).map(({ uri, name }, index) => (
+      <div key={`filter-${index}-${uri}`}>
+        <div
+          role="button"
+          tabIndex="0"
+          className="mr-2 mb-1 fdk-badge badge badge-secondary fdk-text-size-15"
+          onClick={() => removeValueFromInputValueArray(input, uri)}
+          onKeyPress={e => {
+            removeValueFromInputValueArray(input, uri);
+            e.preventDefault();
+          }}
+        >
+          <span className="fdk-filter-pill">{getTranslateText(name)}</span>
+        </div>
+      </div>
+    ))
   );
 };
 
