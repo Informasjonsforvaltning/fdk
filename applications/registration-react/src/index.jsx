81316ed985c7fdc896c5960ae83e1eb6797a3560
Markko Paas <markko.paas@gmail.com>
2019-12-18 12:03:13 +0100

    chore(registration-react): replace @babel/polyfill with core-js, remove whatwg-fetch

--- applications/registration-react/src/index.jsx
+++ applications/registration-react/src/index.jsx
@@ -1,4 +1,6 @@
-import 'whatwg-fetch';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
