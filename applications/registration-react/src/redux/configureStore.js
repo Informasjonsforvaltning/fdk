ac0d10ec209754b555e7599dc82d881452a42f71
Markko Paas <markko.paas@gmail.com>
2019-12-18 16:38:14 +0100

    chore(registration-react): remove unused compose import

--- applications/registration-react/src/redux/configureStore.js
+++ applications/registration-react/src/redux/configureStore.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, compose, createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
