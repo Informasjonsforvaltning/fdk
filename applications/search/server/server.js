4e13bdef6abaa57baab2799a2b7bcc56e4fa9e9a
Markko Paas <markko.paas@gmail.com>
2019-12-20 14:27:09 +0100

    chore(search): static server

--- applications/search/server/server.js
+++ applications/search/server/server.js
@@ -1,28 +1,21 @@
 const path = require('path');
 const express = require('express');
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
-const compression = require('compression');
 
 module.exports = {
   start() {
     const app = express();
-    app.use(compression());
-    app.set('view engine', 'ejs');
-    app.set('views', `${__dirname}/views`);
-    app.use(bodyParser.urlencoded({ extended: false }));
-    app.use(bodyParser.json());
-    app.use(methodOverride());
 
     const port = Number(process.env.PORT || 3000);
     app.set('port', port);
 
-    app.use('/env.json', express.static(path.join(__dirname, 'env.json')));
+    app.get('/env.json', (req, res) => {
+      res.sendFile(path.join(__dirname, '../env.json'));
+    });
 
     app.use('/static', express.static(path.join(__dirname, '/../dist')));
 
     app.get('*', (req, res) => {
-      res.render('index');
+      res.sendFile(path.join(__dirname, '../dist/index.html'));
     });
 
     app.listen(app.get('port'), () => {
