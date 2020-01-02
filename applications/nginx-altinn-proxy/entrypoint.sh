939507a7a3af1dd6a1f6620b1f652bba9d28a49f
NilsOveTen <nils.ove.tendenes@acando.no>
2020-01-02 15:06:21 +0100

    chore: remove nginx-altinn-proxy

--- applications/nginx-altinn-proxy/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-set -eu
-
-envsubst '${ALTINN_HOST} ${ALTINN_API_KEY}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-
-echo "$ALTINN_SSL_BR_CRT_PEM" > /etc/nginx/client.crt.pem
-echo "$ALTINN_SSL_BR_KEY_PEM" > /etc/nginx/client.key.pem
-
-exec "$@"
