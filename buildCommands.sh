719d5658699ef89c59bf419a88cd7eb2238d27cf
NilsOveTen <nils.ove.tendenes@acando.no>
2020-01-02 15:57:36 +0100

    chore: remove nginx-enhetsregisteret-proxy-mock

--- buildCommands.sh
+++ buildCommands.sh
@@ -2,7 +2,7 @@
 
 i=0
 
-docker_apps="e2e search registration-react fuseki nginx-registration nginx-enhetsregisteret-proxy nginx-enhetsregisteret-proxy-mock"
+docker_apps="e2e search registration-react fuseki nginx-registration nginx-enhetsregisteret-proxy"
 
 for docker_app in $docker_apps; do
     i=$((i+1))
