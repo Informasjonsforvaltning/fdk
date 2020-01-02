5fd9da1c53875349f5e99b40153c5e75d5055c01
NilsOveTen <nils.ove.tendenes@acando.no>
2020-01-02 13:29:04 +0100

    chore: remove search-api

--- buildCommands.sh
+++ buildCommands.sh
@@ -10,13 +10,5 @@ for docker_app in $docker_apps; do
     BUILD_CMD[$i]="( cd applications/$docker_app && ./buildDocker.sh )"
 done
 
-maven_apps="search-api"
-
-for maven_app in $maven_apps; do
-    i=$((i+1))
-    BUILD_APPS[$i]=$maven_app
-    BUILD_CMD[$i]="mvn clean install -B -T 2C --projects applications/$maven_app --also-make"
-done
-
 export BUILD_APPS
 export BUILD_CMD
